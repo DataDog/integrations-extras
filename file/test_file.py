b2819cf951d1a3847f0b00db7652ccd8411a1f16
Andreas Fuchs <asf@boinkor.net>
2016-04-07 15:48:33 -0700

    New check to test for a file's existence
    The file.py check allows the agent to test for whether files exist in
    the local file system, and if they do, emit a metric for how long they
    have existed. This is particularly useful for files like Debian's
    "package upgrades require a reboot" marker in /var/run/, or local puppet
    facts meant to be kept around only for a short time.

--- file/test_file.py
+++ file/test_file.py
@@ -1,38 +1,54 @@
-# (C) Datadog, Inc. 2010-2016
-# All rights reserved
-# Licensed under Simplified BSD License (see LICENSE)
-
 # stdlib
-from nose.plugins.attrib import attr
 
 # 3p
 
 # project
-from tests.checks.common import AgentCheckTest
+from tests.checks.common import AgentCheckTest, load_check
+from checks import AgentCheck
 
 
-instance = {
-    'host': 'localhost',
-    'port': 26379,
-    'password': 'datadog-is-devops-best-friend'
-}
-
-
-# NOTE: Feel free to declare multiple test classes if needed
-
-@attr(requires='file')
 class TestFile(AgentCheckTest):
-    """Basic Test for file integration."""
     CHECK_NAME = 'file'
 
-    def test_check(self):
-        """
-        Testing File check.
-        """
-        self.load_check({}, {})
+    def assert_tags(self, expected_tags, present_tags):
+        for tag in expected_tags:
+            self.assertTrue(tag in present_tags)
 
-        # run your actual tests...
+    def test_present_success(self):
+        conf = {
+            'init_config': {},
+            'instances': [
+                {'path': __file__, 'expect': 'present'}
+            ]
+        }
+        self.check = load_check('file', conf, {})
+        self.check.check(conf['instances'][0])
+        metrics = self.check.get_metrics()
+        self.assertTrue(len(metrics) == 1)
+        metric = metrics[0]
+        self.assertTrue(metric[2] > 0)
+        self.assert_tags(['expected_status:present', 'actual_status:present'], metric[3]['tags'])
 
-        self.assertTrue(True)
-        # Raises when COVERAGE=true and coverage < 100%
-        self.coverage_report()
+        service_checks = self.check.get_service_checks()
+        self.assertTrue(service_checks[0]['status'] == AgentCheck.OK)
+        self.assert_tags(['expected_status:present', 'actual_status:present'], service_checks[0]['tags'])
+
+
+    def test_absent_failure(self):
+        conf = {
+            'init_config': {},
+            'instances': [
+                {'path': __file__, 'expect': 'absent'}
+            ]
+        }
+        self.check = load_check('file', conf, {})
+        self.check.check(conf['instances'][0])
+        metrics = self.check.get_metrics()
+        self.assertTrue(len(metrics) == 1)
+        metric = metrics[0]
+        self.assertTrue(metric[2] > 0)
+        self.assert_tags(['expected_status:absent', 'actual_status:present'], metric[3]['tags'])
+
+        service_checks = self.check.get_service_checks()
+        self.assertTrue(service_checks[0]['status'] == AgentCheck.CRITICAL)
+        self.assert_tags(['expected_status:absent', 'actual_status:present'], service_checks[0]['tags'])
