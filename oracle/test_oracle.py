22108c89b690b89f34f617c073bade710685db69
Ryan Handy <ryan.handy@datadoghq.com>
2016-07-18 14:29:44 -0700

    Adding tests

--- oracle/test_oracle.py
+++ oracle/test_oracle.py
@@ -3,36 +3,67 @@
 # Licensed under Simplified BSD License (see LICENSE)
 
 # stdlib
-from nose.plugins.attrib import attr
+import logging
+from types import ListType
+import unittest
 
 # 3p
+from nose.plugins.attrib import attr
 
 # project
-from tests.checks.common import AgentCheckTest
+from tests.checks.common import get_check
 
 
-instance = {
-    'host': 'localhost',
-    'port': 26379,
-    'password': 'datadog-is-devops-best-friend'
-}
+logging.basicConfig()
 
+"""
+Uses Oracle instance running in VM from:
+https://atlas.hashicorp.com/woznial/boxes/centos-6.3-oracle-xe
 
-# NOTE: Feel free to declare multiple test classes if needed
+Include following line in your Vagrantfile:
+
+config.vm.network "forwarded_port", guest: 1521, host: 8521
+
+Using the "system" user as permission granting not available
+for default "system" user
+
+Install oracle instant client in /opt/oracle
+
+Set up Oracle instant client:
+http://jasonstitt.com/cx_oracle_on_os_x
+
+Set:
+export ORACLE_HOME=/opt/oracle/instantclient_12_1/
+export DYLD_LIBRARY_PATH="$ORACLE_HOME:$DYLD_LIBRARY_PATH"
+"""
+
+CONFIG = """
+init_config:
+
+instances:
+    -   server: 127.0.0.1:8521
+        user: system
+        password: manager
+"""
 
 @attr(requires='oracle')
-class TestOracle(AgentCheckTest):
+class TestOracle(unittest.TestCase):
     """Basic Test for oracle integration."""
     CHECK_NAME = 'oracle'
 
-    def test_check(self):
-        """
-        Testing Oracle check.
-        """
-        self.load_check({}, {})
+    def testOracle(self):
+        check, instances = get_check('oracle', CONFIG)
+        check.check(instances[0])
+        metrics = check.get_metrics()
 
-        # run your actual tests...
+        # Make sure the base metrics loaded
+        base_metrics = check.SYS_METRICS.values()
+        ret_metrics = [m[0] for m in metrics]
+        for metric in base_metrics:
+            assert metric in ret_metrics
 
-        self.assertTrue(True)
-        # Raises when COVERAGE=true and coverage < 100%
-        self.coverage_report()
+        service_checks = check.get_service_checks()
+        service_checks_count = len(service_checks)
+        self.assertTrue(isinstance(metrics, ListType))
+        self.assertTrue(service_checks_count > 0)
+        self.assertEquals(len([sc for sc in service_checks if sc['check'] == check.SERVICE_CHECK_NAME]), 1, service_checks)
