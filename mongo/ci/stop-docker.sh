8b21204313a6fcf1a974cf47610a5abd30209ad6
Greg Meyer <gregory.meyer@datadoghq.com>
2016-07-27 18:10:15 +0000

    better stop script

--- mongo/ci/stop-docker.sh
+++ mongo/ci/stop-docker.sh
@@ -2,13 +2,18 @@
 
 set -e
 
-if docker ps | grep dd-test-mongo >/dev/null 2>&1; then
+if docker ps | grep dd-test-mongo >/dev/null; then
   containers=`docker ps --format '{{.Names}}' | grep dd-test-mongo`
 
   echo 'removing containers'
   for container in $containers; do
-    echo "removing $container"
+    echo "stopping $container"
     docker kill $container >/dev/null 2>&1
+  done
+
+  stopped_containers=`docker ps -a --format '{{.Names}}' | grep dd-test-mongo`
+  for container in $stopped_containers; do
+    echo "removing $container"
     docker rm $container >/dev/null 2>&1
   done
 
