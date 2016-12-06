58e62da340b170cf3a7720e6bfc0512a673bce9f
Jean Rouge <jer329@cornell.edu>
2016-12-06 08:12:14 -0800

    Adding a check for filebeat
    Filebeat (https://github.com/elastic/beats) is a deamon tailing and processing
    log files, for example to send them to Logstash.
    
    It maintains a _registry file_, in which it keeps track of which log files it's
    currently tailing, and up to what point it has processed them.
    
    This patch adds an agent check that leverages that registry file to gauge
    filebat's progress; more specifically, it pushes how many bytes are currently
    left to process for filebeat.
    
    This metric would be useful for example to create a monitor alerting when
    filebeat falls too far behind.
    
    Added unit tests on the new check.

--- filebeat/check.py
+++ filebeat/check.py
@@ -5,9 +5,12 @@
 # stdlib
 
 # 3rd party
+import json
+import os
 
 # project
 from checks import AgentCheck
+
 
 EVENT_TYPE = SOURCE_TYPE_NAME = 'filebeat'
 
@@ -18,4 +21,39 @@ class FilebeatCheck(AgentCheck):
         AgentCheck.__init__(self, name, init_config, agentConfig, instances)
 
     def check(self, instance):
-        pass
+        registry_file_path = instance.get('registry_file_path')
+        if registry_file_path is None:
+            raise Exception('An absolute path to a filebeat registry path must be specified')
+
+        registry_contents = self._parse_registry_file(registry_file_path)
+
+        for item in registry_contents.itervalues():
+            self._process_registry_item(item)
+
+    def _parse_registry_file(self, registry_file_path):
+        try:
+            with open(registry_file_path) as registry_file:
+                return json.load(registry_file)
+        except IOError:
+            self.log.warn('No filebeat registry file found at %s' % (registry_file_path, ))
+            return {}
+
+    def _process_registry_item(self, item):
+        source = item['source']
+        offset = item['offset']
+
+        try:
+            stats = os.stat(source)
+
+            if self._is_same_file(stats, item['FileStateOS']):
+                unprocessed_bytes = stats.st_size - offset
+
+                self.gauge('filebeat.registry.unprocessed_bytes', unprocessed_bytes,
+                           tags=["source:{0}".format(source)])
+            else:
+                self.log.debug("Filebeat source %s appears to have changed" % (source, ))
+        except OSError:
+            self.log.debug("Unable to get stats on filebeat source %s" % (source, ))
+
+    def _is_same_file(self, stats, file_state_os):
+        return stats.st_dev == file_state_os['device'] and stats.st_ino == file_state_os['inode']
