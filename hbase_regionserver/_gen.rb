8f42203388774049957c4570d118982c50eceb81
Massimiliano Pippi <mpippi@gmail.com>
2017-10-11 12:30:13 +0200

    bumped rubocop

--- hbase_regionserver/_gen.rb
+++ hbase_regionserver/_gen.rb
@@ -17,74 +17,72 @@ require 'json'
 require 'csv'
 require 'erb'
 
-METRICS_FILENAME='_metrics.yml'
-OUT_CONFIG_FILENAME='_conf.yaml.example'
-OUT_METADATA_FILENAME='_metadata.csv'
-ORIENTATION = "0"
+METRICS_FILENAME = '_metrics.yml'.freeze
+OUT_CONFIG_FILENAME = '_conf.yaml.example'.freeze
+OUT_METADATA_FILENAME = '_metadata.csv'.freeze
+ORIENTATION = '0'.freeze
 
-MANIFEST = JSON.load(File.new('manifest.json',"r"))
-jmx_dump = JSON.load(`curl -s http://localhost:60030/jmx?description=true`)["beans"]
-metrics = YAML.load(File.new(METRICS_FILENAME, "r"))
+MANIFEST = JSON.load(File.new('manifest.json', 'r'))
+jmx_dump = JSON.load(`curl -s http://localhost:60030/jmx?description=true`)['beans']
+metrics = YAML.safe_load(File.new(METRICS_FILENAME, 'r'))
 
 # metrics data decorators
 # complete required fields or optional fields from dumped attribut from hbase
 def decorate_descrption(metric, dumped_attribute)
-  if Hash === dumped_attribute && dumped_attribute.has_key?("description")
-    metric["description"] = dumped_attribute["description"]
-  elsif not metric.has_key?("description")
-    metric["description"] = ""
+  if Hash === dumped_attribute && dumped_attribute.key?('description')
+    metric['description'] = dumped_attribute['description']
+  elsif !metric.key?('description')
+    metric['description'] = ''
   end
 end
 
 def decorate_unit(metric)
-  if !metric.has_key?("unit_name") then
-    if metric["alias"].include?("size") || metric["description"].include?("in bytes") then
-      metric["unit_name"] = "byte"
-    elsif (metric["alias"].include?("percent") || metric["description"].include?("percent")) && !metric["alias"].include?("percentile")
-      metric["unit_name"] = "percent"
-    elsif metric["alias"].include?("time") || metric["description"].include?("time")
-      metric["unit_name"] = "millisecond"
+  unless metric.key?('unit_name')
+    if metric['alias'].include?('size') || metric['description'].include?('in bytes')
+      metric['unit_name'] = 'byte'
+    elsif (metric['alias'].include?('percent') || metric['description'].include?('percent')) && !metric['alias'].include?('percentile')
+      metric['unit_name'] = 'percent'
+    elsif metric['alias'].include?('time') || metric['description'].include?('time')
+      metric['unit_name'] = 'millisecond'
     else
-      metric["unit_name"] = ""
+      metric['unit_name'] = ''
     end
   end
 end
 
 def decorate_required_fields(metric)
-  metric["integration"] = MANIFEST["name"]
-  metric["metric_name"] = metric["alias"]
+  metric['integration'] = MANIFEST['name']
+  metric['metric_name'] = metric['alias']
   # operation will be fixed value
-  metric["orientation"] = ORIENTATION
+  metric['orientation'] = ORIENTATION
   # default metric_type is gauge
-  if !metric.has_key?("metric_type")
-    metric["metric_type"] = "gauge"
-  end
+  metric['metric_type'] = 'gauge' unless metric.key?('metric_type')
 end
 
 def decorate_optional_fields(metric, attribute)
-  decorate_descrption(metric,attribute)
+  decorate_descrption(metric, attribute)
   decorate_unit(metric)
 end
 
-metadata_keys = %w{metric_name metric_type interval unit_name per_unit_name description orientation integration short_name}
-metadata_header = CSV::Row.new(metadata_keys,[],true)
+metadata_keys = %w[metric_name metric_type interval unit_name per_unit_name description orientation integration short_name]
+metadata_header = CSV::Row.new(metadata_keys, [], true)
 metadata = CSV::Table.new([metadata_header])
 metrics.each do |m|
   decorate_required_fields(m)
-  mbean = jmx_dump.find{|o| o["name"] == m["bean"]}
-  if mbean.has_key?(m["attribute"])
-    decorate_optional_fields(m, mbean[m["attribute"]])
+  mbean = jmx_dump.find { |o| o['name'] == m['bean'] }
+  if mbean.key?(m['attribute'])
+    decorate_optional_fields(m, mbean[m['attribute']])
   else
-    decorate_optional_fields(m, "")
+    decorate_optional_fields(m, '')
   end
 
-  r = CSV::Row.new([],[],false)
+  r = CSV::Row.new([], [], false)
   metadata_keys.each do |k|
-    if m.has_key?(k)
-      r << { k => m[k] }
-    else
-      r << { k => "" }
-    end
+    r << if m.key?(k)
+           { k => m[k] }
+         else
+           { k => '' }
+         end
   end
   metadata << r
 end
@@ -127,8 +125,8 @@ init_config:
             values: <%= m["values"] %><% end %><% end %>
 <% end %>
 HEREDOC
-)
+                       )
 
-bean_metrics=metrics.group_by {|m| m["bean"] }
-conf_rendered=conf_template.result(binding)
+bean_metrics = metrics.group_by { |m| m['bean'] }
+conf_rendered = conf_template.result(binding)
 File.open(OUT_CONFIG_FILENAME, 'w').puts conf_rendered
