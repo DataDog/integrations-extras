5ca059b61b98b2e9e0c1deeaf7b4930f5de612af
Cody Lee <cody.lee@datadoghq.com>
2017-12-29 09:40:19 -0600

    fix lint

--- upsc/check.py
+++ upsc/check.py
@@ -31,7 +31,7 @@ class UpscCheck(AgentCheck):
             results = subprocess.check_output(['upsc', '-l'], stderr=self.DEV_NULL)
             return [r.lstrip().rstrip() for r in results.split('\n')]
         except subprocess.CalledProcessError as e:
-            self.log.error("Unable to query devices", e)
+            self.log.error("Unable to query devices: %s", e)
             return []
 
     def query_ups_device(self, name):
@@ -135,4 +135,3 @@ class UpscCheck(AgentCheck):
         self.excluded_devices_re = []
         for excluded_regex in instance.get('excluded_devices_re', []):
             self.excluded_devices_re.append(re.compile(excluded_regex))
-
