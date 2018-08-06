54832175d12cde76720642194803a0390fbe6a40
Ashutosh Vyas <ashutoshvyas@Ashutoshs-MacBook-Pro.local>
2018-08-06 14:34:04 +0530

    instance value checked

--- bind9_check/tests/test_check.py
+++ bind9_check/tests/test_check.py
@@ -1,11 +1,24 @@
 # (C) Datadog, Inc. 2018
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-
+import pytest
 from datadog_checks.bind9_check import bind9_check
 
 
-def test_check(aggregator):
-    c = bind9_check('bind9_check', {}, {}, None)
-    c.check({})
-    aggregator.assert_all_metrics_covered()
+
+@pytest.fixture()
+def aggregator():
+    from datadog_checks.stubs import aggregator
+    aggregator.reset()
+    return aggregator
+
+@pytest.fixture
+def instance():
+    return {
+        'url': 'http://10.10.1.101:8080'
+    }
+
+def test_check(aggregator, instance):
+	c = bind9_check('bind9_check', {}, {}, None)
+	c.check(instance)
+	aggregator.assert_all_metrics_covered()
