f36d46671c9d48ef7e33eaf774295f4d72591e9c
Ashutosh Vyas <ashutoshvyas@Ashutoshs-MacBook-Pro.local>
2018-08-06 15:29:56 +0530

    xml format stats response saved

--- bind9_check/datadog_checks/bind9_check/bind9_check.py
+++ bind9_check/datadog_checks/bind9_check/bind9_check.py
@@ -8,8 +8,22 @@ from datadog_checks.checks import AgentCheck
 class bind9_check(AgentCheck):
 
     def check(self, instance):
-    	url = instance['URL']
-    	if url is None :
+    	dns_url = instance.get('url')
+    	if dns_url is None :
     		raise Exception('The statistic channel URL must be specified in the configuration')
-		    	
+		
+		self.service_check("Status", AgentCheck.OK,
+                           message='Connection to %s was successful' % dns_url)
 
+		xmlStats = self.getStatsFromUrl(dns_url)
+
+	def getStatsFromUrl(self, dns_url) :
+
+		try:
+			xmlStats = urllib.urlopen(url)
+
+		except (URLError, HTTPError) as e:
+			self.service_check("status", message= "not connected due to " + e.reason)
+			raise
+
+		return xmlStats
--- bind9_check/tests/test_check.py
+++ bind9_check/tests/test_check.py
@@ -2,9 +2,12 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 import pytest
+from mock import MagicMock
 from datadog_checks.bind9_check import bind9_check
+import os
 
-
+HERE = os.path.dirname(os.path.abspath(__file__))
+URL = 'http://10.10.1.101:8080'
 
 @pytest.fixture()
 def aggregator():
@@ -15,10 +18,15 @@ def aggregator():
 @pytest.fixture
 def instance():
     return {
-        'url': 'http://10.10.1.101:8080'
+        'url': URL
     }
 
 def test_check(aggregator, instance):
 	c = bind9_check('bind9_check', {}, {}, None)
 	c.check(instance)
+
+	with open(os.path.join(HERE,'sample_stats.xml'), 'r') as file :
+		c.getStatsFromUrl = MagicMock(return_value=file)
+	stats = c.getStatsFromUrl(c, URL)
+	c.getStatsFromUrl.assert_called_with(c, URL )
 	aggregator.assert_all_metrics_covered()
