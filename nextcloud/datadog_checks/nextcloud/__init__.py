5df14ce44d1238818372559c43ad656d60011103
Emeric Planet <emeric.planet@datadoghq.com>
2019-02-17 21:18:24 +0100

    Add Nextcloud integration (#341)
    * Add Nextcloud integration
    
    * Update nextcloud/tox.ini
    
    Co-Authored-By: eplanet <emeric.planet@datadoghq.com>
    
    * Update nextcloud/metadata.csv
    
    Co-Authored-By: eplanet <emeric.planet@datadoghq.com>
    
    * Update nextcloud/metadata.csv
    
    Co-Authored-By: eplanet <emeric.planet@datadoghq.com>
    
    * Update manifest
    
    * Update nextcloud/datadog_checks/nextcloud/nextcloud.py
    
    Co-Authored-By: eplanet <emeric.planet@datadoghq.com>
    
    * Address PR comments
    
    * Add tags and more metrics
    
    * fix link
    
    * remove print
    
    * Update .travis.yml

---
+++ nextcloud/datadog_checks/nextcloud/__init__.py
@@ -0,0 +1,7 @@
+from .__about__ import __version__
+from .nextcloud import NextcloudCheck
+
+__all__ = [
+    '__version__',
+    'NextcloudCheck'
+]
---
+++ nextcloud/tests/test_nextcloud.py
@@ -0,0 +1,30 @@
+from datadog_checks.nextcloud import NextcloudCheck
+
+import pytest
+
+
+def test_empty_url(aggregator, empty_url_instance):
+    check = NextcloudCheck('nextcloud', {}, {})
+    check.check(empty_url_instance)
+    aggregator.assert_service_check(NextcloudCheck.STATUS_CHECK, check.CRITICAL)
+
+
+@pytest.mark.usefixtures('dd_environment')
+@pytest.mark.integration
+def test_invalid_url(aggregator, invalid_url_instance):
+    check = NextcloudCheck('nextcloud', {}, {})
+    check.check(invalid_url_instance)
+    aggregator.assert_service_check(NextcloudCheck.STATUS_CHECK, check.CRITICAL)
+
+
+@pytest.mark.usefixtures('dd_environment')
+@pytest.mark.integration
+def test_valid_check(aggregator, instance):
+    check = NextcloudCheck('nextcloud', {}, {})
+    check.check(instance)
+    aggregator.assert_service_check(NextcloudCheck.STATUS_CHECK, check.OK)
+
+    for gauge in NextcloudCheck.METRICS_GAUGES:
+        aggregator.assert_metric(check.get_metric_display_name(gauge), tags=check.tags)
+
+    aggregator.assert_all_metrics_covered()
