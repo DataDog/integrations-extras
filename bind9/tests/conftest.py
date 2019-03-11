5a7c5b9b84178b991c71ff11f0b081ab1e7eefea
ashuvyas45 <41504937+ashuvyas45@users.noreply.github.com>
2019-03-11 10:11:27 +0530

    Added Bind9 dns integration to collect dns server metrics. (#186)
    * basic files created for bind9 integration
    
    * URL exception handled
    
    * instance value checked
    
    * xml format stats response saved
    
    * All metrics sent and asserted
    
    * exception case handled
    
    * added images
    
    * Update service_checks.json
    
    service_checks.json updated
    
    * Update README.md
    
    README.md updated
    
    * metadata.csv updated
    
    * metadata updated
    
    * Skips the metric if ValueError occurs
    
    KernelVersion and StoreId may contain letters and fails during  conversion to float/int
    
    * 4-space indentation
    
    * fixing non ASCII character (#184)
    
    * update datadog-checks-base
    
    * Changed the directory name from bind9_check to bind9
    
    * Refactored indentation to 4 spaces
    
    * Update requirements-dev.txt
    
    * [Ashu] Mock the request to test the function
    
    * [Ashu] Update the manifest file
    
    * [Ashu] Update manifest with all mandatory fields
    
    * [Ashu] Fix the error in manifest file
    
    * fix the creates_events key and public title value
    
    * clean up
    
    * fix
    
    * more fixes
    
    * oops
    
    * root
    
    * fix metric names
    
    * fix tests
    
    * final
    
    * add to travis

---
+++ bind9/tests/conftest.py
@@ -0,0 +1,23 @@
+# (C) Datadog, Inc. 2018
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+import os
+
+import mock
+import pytest
+
+from .common import HERE, URL
+
+
+@pytest.fixture(scope='session', autouse=True)
+def mock_requests():
+    with open(os.path.join(HERE, 'fixtures', 'sample_stats.xml'), 'r') as f:
+        mock_data = f.read()
+
+    with mock.patch('requests.get', return_value=mock.MagicMock(text=mock_data, status_code=200)):
+        yield
+
+
+@pytest.fixture
+def instance():
+    return {'url': URL}
