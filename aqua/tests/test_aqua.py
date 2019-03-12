936319dbef0286d1d592ccb2d65e78f59c3e1c4c
Ofek Lev <ofekmeister@gmail.com>
2019-03-11 23:11:49 -0400

    Support Python 3

--- aqua/tests/test_aqua.py
+++ aqua/tests/test_aqua.py
@@ -1,33 +1,14 @@
 # (C) Datadog, Inc. 2018
 # All rights reserved
 # Licensed under Simplified BSD License (see LICENSE)
-import os
 import json
+import os
 
-import pytest
 from mock import MagicMock
 
 from datadog_checks.aqua import AquaCheck
 
-
-HERE = os.path.dirname(os.path.abspath(__file__))
-
-
-@pytest.fixture
-def instance():
-    return {
-        'url': 'http://localhost:8080',
-        'api_user': 'foo',
-        'password': 'bar',
-        'tags': ['foo:bar']
-    }
-
-
-@pytest.fixture()
-def aggregator():
-    from datadog_checks.stubs import aggregator
-    aggregator.reset()
-    return aggregator
+from .common import HERE
 
 
 EXPECTED_VALUES = (
@@ -43,6 +24,7 @@ EXPECTED_VALUES = (
 def test_check(aggregator, instance):
     """
     Testing Aqua check.
+    Add e2e eventually https://github.com/aquasecurity/microscanner
     """
     check = AquaCheck('aqua', {}, {})
     check.validate_instance = MagicMock(return_value=None)
@@ -50,16 +32,16 @@ def test_check(aggregator, instance):
 
     def mock_perform(inst, url, token):
         if url == '/api/v1/dashboard':
-            with open(os.path.join(HERE, 'aqua_base_metrics.json'), 'r') as f:
+            with open(os.path.join(HERE, 'fixtures', 'aqua_base_metrics.json'), 'r') as f:
                 return json.load(f)
         elif url == '/api/v1/hosts':
-            with open(os.path.join(HERE, 'aqua_hosts_metrics.json'), 'r') as f:
+            with open(os.path.join(HERE, 'fixtures', 'aqua_hosts_metrics.json'), 'r') as f:
                 return json.load(f)
         elif url == '/api/v1/audit/access_totals?alert=-1&limit=100&time=hour&type=all':
-            with open(os.path.join(HERE, 'aqua_audit_metrics.json'), 'r') as f:
+            with open(os.path.join(HERE, 'fixtures', 'aqua_audit_metrics.json'), 'r') as f:
                 return json.load(f)
         elif url == '/api/v1/scanqueue/summary':
-            with open(os.path.join(HERE, 'aqua_scan_queues_metrics.json'), 'r') as f:
+            with open(os.path.join(HERE, 'fixtures', 'aqua_scan_queues_metrics.json'), 'r') as f:
                 return json.load(f)
     check._perform_query = MagicMock(side_effect=mock_perform)
 
@@ -68,5 +50,4 @@ def test_check(aggregator, instance):
         aggregator.assert_metric(metric, value=value)
 
     aggregator.assert_service_check(check.SERVICE_CHECK_NAME)
-    # Raises when COVERAGE=true and coverage < 100%
     aggregator.assert_all_metrics_covered()
