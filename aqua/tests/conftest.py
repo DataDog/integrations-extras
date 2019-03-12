1fe608b64971d58f7ff7a1e0ff2da9afd6a34d60
Ofek Lev <ofekmeister@gmail.com>
2019-03-12 10:37:59 -0400

    Support Python 3 (#357)
    * Support Python 3
    
    * Update README.md
    
    * Update README.md

--- aqua/tests/conftest.py
+++ aqua/tests/conftest.py
@@ -1,6 +1,13 @@
 import pytest
 
+from .common import HOST, PORT
+
 
 @pytest.fixture
 def instance():
-    return {}
+    return {
+        'url': 'http://{}:{}'.format(HOST, PORT),
+        'api_user': 'foo',
+        'password': 'bar',
+        'tags': ['foo:bar']
+    }
