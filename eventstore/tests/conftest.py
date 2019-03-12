7a0a1c0bcedb52e983b8257930d1178542fd0a6d
Ofek Lev <ofekmeister@gmail.com>
2019-03-12 17:17:34 -0400

    Support Python 3 (#362)
    * Support Python 3
    
    * Update appveyor.yml
    
    * catch more

--- eventstore/tests/conftest.py
+++ eventstore/tests/conftest.py
@@ -7,13 +7,29 @@ import os
 
 from datadog_checks.dev import docker_run
 
-from .common import HERE
+from .common import HERE, HOST, PORT
 
 
 @pytest.fixture(scope="session")
-def eventstore_server():
+def dd_environment():
     with docker_run(
         compose_file=os.path.join(HERE, "compose", "docker-compose.yml"),
         sleep=10
     ):
         yield
+
+
+@pytest.fixture
+def instance():
+    return {
+        'default_timeout': 5,
+        'tag_by_url': True,
+        'url': 'http://{}:{}/stats'.format(HOST, PORT),
+        'name': 'testInstance',
+        'json_path': [
+            '*',
+            '*.*',
+            '*.*.*',
+            '*.*.*.*'
+        ]
+    }
