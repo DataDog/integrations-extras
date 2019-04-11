f9f21710c3ea4c2917b2f6f58fb53c1d3c2dcf0a
Ofek Lev <ofekmeister@gmail.com>
2019-04-10 23:37:58 -0400

    Adhere to code style (#397)

--- bind9/datadog_checks/bind9/__init__.py
+++ bind9/datadog_checks/bind9/__init__.py
@@ -2,10 +2,7 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
-from .bind9 import Bind9Check
 from .__about__ import __version__
+from .bind9 import Bind9Check
 
-__all__ = [
-    '__version__',
-    'Bind9Check'
-]
+__all__ = ['__version__', 'Bind9Check']
