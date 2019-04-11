07dda8f4fd96b1374e33d670fb52cbc84cc3b4f3
Thomas Hervé <thomas.herve@datadoghq.com>
2019-04-11 16:42:46 +0200

    Add more log patterns

--- riak_repl/tests/conftest.py
+++ riak_repl/tests/conftest.py
@@ -10,9 +10,10 @@ from .common import HERE, URL
 @pytest.fixture(scope="session")
 def dd_environment():
     compose_file = os.path.join(HERE, "docker", "docker-compose.yml")
-    log_patterns = 'Full-sync with site "riak-west-1" completed'
+    log_patterns = ['Full-sync with site "riak-west-1" completed',
+                    'Full-sync with site "riak-east-1" completed']
     with docker_run(
         compose_file=compose_file,
-        conditions=[CheckEndpoints(URL, wait=5), CheckDockerLogs(compose_file, log_patterns)],
+        conditions=[CheckEndpoints(URL, wait=5), CheckDockerLogs(compose_file, log_patterns, matches='all')],
     ):
         yield
