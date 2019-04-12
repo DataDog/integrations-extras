0c0d1fbd4ad6c1871a19e571af1b493eb55ab791
Ofek Lev <ofekmeister@gmail.com>
2019-04-12 16:39:56 -0400

    Adhere to code style (#404)
    * Adhere to code style
    
    * fix
    
    * better
    
    * .

--- riak_repl/tests/conftest.py
+++ riak_repl/tests/conftest.py
@@ -1,5 +1,6 @@
-import pytest
 import os
+
+import pytest
 
 from datadog_checks.dev.conditions import CheckDockerLogs, CheckEndpoints
 from datadog_checks.dev.docker import docker_run
@@ -7,11 +8,11 @@ from datadog_checks.dev.docker import docker_run
 from .common import HERE, URL
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope='session')
 def dd_environment():
-    compose_file = os.path.join(HERE, "docker", "docker-compose.yml")
-    log_patterns = ['Full-sync with site "riak-west-1" completed',
-                    'Full-sync with site "riak-east-1" completed']
+    compose_file = os.path.join(HERE, 'docker', 'docker-compose.yml')
+    log_patterns = ['Full-sync with site "riak-west-1" completed', 'Full-sync with site "riak-east-1" completed']
+
     with docker_run(
         compose_file=compose_file,
         conditions=[CheckEndpoints(URL, wait=5), CheckDockerLogs(compose_file, log_patterns, matches='all')],
