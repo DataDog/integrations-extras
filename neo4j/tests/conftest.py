c0a5af79051d8ff4d65650d4ea4b2d828b339b54
Ofek Lev <ofekmeister@gmail.com>
2019-04-12 11:08:47 -0400

    Adhere to code style (#400)

--- neo4j/tests/conftest.py
+++ neo4j/tests/conftest.py
@@ -5,6 +5,7 @@ import requests
 
 from datadog_checks.dev.conditions import WaitFor
 from datadog_checks.dev.docker import docker_run
+
 from .common import NEO4J_MINIMAL_CONFIG
 
 HERE = os.path.dirname(os.path.abspath(__file__))
@@ -14,10 +15,7 @@ DOCKER_DIR = os.path.join(HERE, 'docker')
 def init_user():
     instance = NEO4J_MINIMAL_CONFIG
     url = '{}:{}/user/{}/password'.format(instance['neo4j_url'], instance['port'], instance['user'])
-    r = requests.post(
-        url,
-        json={'password': instance['password']},
-        auth=(instance['user'], 'neo4j'))
+    r = requests.post(url, json={'password': instance['password']}, auth=(instance['user'], 'neo4j'))
     r.raise_for_status()
 
 
@@ -27,6 +25,6 @@ def dd_environment():
     with docker_run(
         os.path.join(DOCKER_DIR, 'docker-compose.yaml'),
         log_patterns='Remote interface available at',
-        conditions=[WaitFor(init_user)]
+        conditions=[WaitFor(init_user)],
     ):
         yield instance
