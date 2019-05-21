23914980fd8561ab91270a717368aa1c87a93bc5
jay.dave <jay.dave@comp10172.lan>
2019-05-21 18:03:43 -0500

    ready for code review, looking for guidence around docker event body and required files to sc

--- windows_docker/src/win_docker.py
+++ windows_docker/src/win_docker.py
@@ -13,51 +13,129 @@ class UTC(tzinfo):
   def dst(self, dt):
     return timedelta(0)
 
-class WinDocker(AgentCheck):
+class DockerConstant:
+# Metric Name Schema
+	NET_TMPL = "docker.networks.{}"
+	BLKIO_STATS_TMPL = "docker.blkio_stats.{}"
+	STORAGE_STATS_TMPL = "docker.storage_stats.{}"
+	CPU_STATS_TMPL = "docker.cpu_stats.cpu_usage.{}"
+	THROTTLING_DATA_TMPL = "docker.cpu_stats.throttling_data.{}"
+	PRECPU_STATS_CPU_USAGE_TMPL = "docker.precpu_stats.cpu_usage.{}"
+	PRECPU_STATS_THROTTLING_DATA_TMPL = "docker.precpu_stats.throttling_data.{}"
+	MEMORY_STATS_TMPL = "docker.memory_stats.{}"
+	CONTAINER_RUN_TMPL = "docker.containers.running"
+	NUM_PROCESS_TMPL = "docker.num_proces"
+#Tags
+	WINDOWS_TAG = "os:windows"	
+	DOCKER_WIN_TAG = "sources:docker_win"
+#Other
+	DOCKER_LIST_ALL_CONT = "/containers/json"
+	DOCKER_CONTAINER = "/containers/"
+	DOCKER_STATS = "/stats"
+	DOCKER_IMAGE_KEY = "docker_image:"
+	DOCKER_STATE_KEY = "state:"
+	DOCKER_STATUS_KEY = "status:"
+	DOCKER_CONTAINER_NAME_KEY = "container_name:"
+	DOCKER_CONTAINER_ID_KEY = "container_id:"
+	DOCKER_GUID_KEY = "guid:"
+	DOCKER_TYPE = "docker"
+#Docker Metric Mapping
+	DOCKER_NETWORK = "networks"
+	DOCKER_NETWORK_RX_BYTES = "rx_bytes"
+	DOCKER_NETWORK_RX_PACKETS = "rx_packets" 
+	DOCKER_NETWORK_RX_ERRORS = "rx_errors" 
+	DOCKER_NETWORK_RX_DROPPED = "rx_dropped" 
+	DOCKER_NETWORK_TX_BYTES = "tx_bytes"
+	DOCKER_NETWORK_TX_PACKETS = "tx_packets" 
+	DOCKER_NETWORK_TX_ERRORS = "tx_errors" 
+	DOCKER_NETWORK_TX_DROPPED = "tx_dropped"
+	DOCKER_MEMORY_STATS= "memory_stats"
+	DOCKER_MEMORY_COMMIBYTES = "commitbytes"
+	DOCKER_MEMORY_COMMITPEAKBYTES = "commitpeakbytes" 
+	DOCKER_MEMORY_PRIATEWORKINGSET = "privateworkingset"
+	DOCKER_PRECPU_STATS = "precpu_stats"
+	DOCKER_THROTTLING_DATA = "throttling_data"
+	DOCKER_THROTTLING_DATA_PERIODS = "periods"
+	DOCKER_THROTTLING_DATA_THROTTLED_PERIODS = "throttled_periods"
+	DOCKER_THROTTLING_DATA_THROTTLED_TIME = "throttled_time"
+	DOCKER_CPU_USAGE = "cpu_usage"
+	DOCKER_CPU_USAGE_TOTAL_USAGE = "total_usage"
+	DOCKER_CPU_USAGE_USAGE_IN_KERNELMODE = "usage_in_kernelmode"
+	DOCKER_CPU_USAGE_USAGE_IN_USERMODE = "usage_in_usermode"
+	DOCKER_CPU_STATS = "cpu_stats"
+	DOCKER_STORAGE_STATS ="storage_stats"
+	DOCKER_STORAGE_STATS_READ_COUNT_NORMALIZED = "read_count_normalized"
+	DOCKER_STORAGE_STATS_READ_SIZE_BYTES = "read_size_bytes"
+	DOCKER_STORAGE_STATS_READ_WRITE_COUNT_NORMALIZED = "write_count_normalized"
+	DOCKER_STORAGE_STATS_READ_WRITE_SIZE_BYTES = "write_size_bytes"
+	DOCKER_BLKIO_STATS = "blkio_stats"
+	DOCKER_BLKIO_STATS_IO_SERVICE_BYTES_RECURSIVE = "io_service_bytes_recursive"
+	DOCKER_BLKIO_STATS_IO_SERVICE_RECURSIVE = "io_serviced_recursive"
+	DOCKER_BLKIO_STATS_IO_QUEUE_RECURSIVE = "io_queue_recursive"
+	DOCKER_BLKIO_STATS_IO_SERVICE_TIME_RECURSIVE = "io_service_time_recursive"
+	DOCKER_BLKIO_STATS_IO_WAIT_TIME_RECURSIVE = "io_wait_time_recursive"
+	DOCKER_BLKIO_STATS_IO_MERGED_RECURSIVE = "io_merged_recursive"
+	DOCKER_BLKIO_STATS_IO_TIME_RECURSIVE = "io_time_recursive"
+	DOCKER_BLKIO_STATS_SECTORS_RECURSIVE = "sectors_recursive"
+	DOCKER_NUM_PROCS = "num_procs"
+
+class WinDocker(AgentCheck, DockerConstant):
+	def _generate_instance_key(self, instance):
+		return instance.get('url')
+
+	def _generate_request_session(self, instance):
+		sess = requests.Session()
+		if instance.get("tls_client_cert"):
+			sess.cert = instance.get("tls_client_cert")
+		if instance.get("tls_cacert"):
+			sess.verify = instance.get("tls_cacert")
+		elif instance.get("tls_verify"):
+			sess.verify = True
+		return sess
+
 	def check(self, instance):
 		container_full_ids = []
 		container_ids = []
 		container_urls = []
-		id_tag = ""
-		image_tag = ""
-		state_tag = ""
-		status_tag = ""
-		name_tag = ""
-		net_tmpl = "docker.windows.networks.{}"
-		blkio_stats_tmpl = "docker.windows.blkio_stats.{}"
-		storage_stats_tmpl = "docker.windows.storage_stats.{}"
-		cpu_stats_tmpl = "docker.windows.cpu_stats.cpu_usage.{}"
-		throttling_data_tmpl = "docker.windows.cpu_stats.throttling_data.{}"
-		precpu_stats_cpu_usage_tmpl = "docker.windows.precpu_stats.cpu_usage.{}"
-		precpu_stats_throttling_data_tmpl = "docker.windows.precpu_stats.throttling_data.{}"
-		memory_stats_tmpl = "docker.windows.memory_stats.{}"
 
-		container_url="http://localhost:2375/containers/json"
-		containers_response = requests.get(container_url, timeout=1).json()
+		url = self._generate_instance_key(instance)
+		list_all_containers_url=url+self.DOCKER_LIST_ALL_CONT
 
+		session = self._generate_request_session(instance) 
+		containers_response = session.get(list_all_containers_url, timeout=1).json()
+		container_count = len(containers_response)
 
-		for value in containers_response:
-			container_full_ids.append(value["Id"])
-			container_count = len(containers_response)
+		for container in containers_response:
+			container_full_ids.append(container["Id"])
 			
-			if value:
-				id_tag = "sources:docker_win"
-				image_tag = "docker_image:" + value["Image"]
-				state_tag = "state:" + value["State"]
-				status_tag = "status:" + value["Status"]
-				name_tag = "container_name:" + value["Names"][0]
-				self.gauge("docker.windows.created", value["Created"], tags=[id_tag, image_tag, name_tag, status_tag, state_tag ])
+			id_tag = self.DOCKER_WIN_TAG
+			image_tag = self.DOCKER_IMAGE_KEY + container["Image"].lower()
+			state_tag = self.DOCKER_STATE_KEY + container["State"].lower()
+			status_tag = self.DOCKER_STATUS_KEY + container["Status"].lower()
+			name_tag = self.DOCKER_CONTAINER_NAME_KEY + container["Names"][0].lower()
+
+			self.event({
+				"event_type": self.DOCKER_TYPE,
+				"msg_title": 'Could use review from team.',
+				"alert_type": 'info',
+				"source_type_name": self.DOCKER_TYPE,
+				"tags": [self.DOCKER_WIN_TAG],
+				"msg_text": "Docker"
+			})
+
+				#self.gauge("docker.created", container["Created"], tags=[id_tag, image_tag, name_tag, status_tag, state_tag, self.WINDOWS_TAG])
+
 	# running containers:
-		self.gauge("docker.windows.containers.running", container_count, tags=[id_tag, image_tag, name_tag, status_tag, state_tag])
+		self.gauge(self.CONTAINER_RUN_TMPL, container_count, tags=[self.WINDOWS_TAG])
 
 		for x in container_full_ids:
-			container_urls.append("http://localhost:2375/containers/" + x + "/stats")
+			container_urls.append(url+self.DOCKER_CONTAINER+x+self.DOCKER_STATS)
 			container_ids.append(x[:12])
 
 		for index, value in enumerate(container_urls):
-			cont_health_response = requests.get(value, stream=True)
+			cont_health_response = session.get(value, stream=True)
 			for raw_stream in cont_health_response.iter_lines():
-				short_container_id = "container_id:"+ container_ids[index]
+				short_container_id = self.DOCKER_CONTAINER_ID_KEY+container_ids[index]
 				if raw_stream:
 					#main stream
 					try:
@@ -71,50 +149,50 @@ class WinDocker(AgentCheck):
 					microseconds = int(int(read_date_parts[1].rstrip('Z'))/1000.0)
 					d = d.replace(microsecond=microseconds, tzinfo=UTC())
 					d = calendar.timegm(d.timetuple())
-					self.gauge('docker.windows.read_at',d,tags=[short_container_id,id_tag, image_tag, name_tag, status_tag, state_tag])
+					self.gauge('docker.read_at',d,tags=[short_container_id,self.WINDOWS_TAG])
 
 				#num_proces:
-					num_procs=rs['num_procs']
-					self.gauge('docker.windows.num_proces',num_procs, tags=[short_container_id,id_tag, image_tag, name_tag, status_tag, state_tag])
+					num_procs=rs[self.DOCKER_NUM_PROCS]
+					self.gauge(self.NUM_PROCESS_TMPL, num_procs, tags=[short_container_id,self.WINDOWS_TAG])
 
 				#blkio_stats:
-					blkio_stats=rs['blkio_stats']
-					for blkio_stats_prop in ["io_service_bytes_recursive", "io_serviced_recursive", "io_queue_recursive", "io_service_time_recursive", "io_wait_time_recursive","io_merged_recursive", "io_time_recursive", "sectors_recursive"]:
-						self.gauge(blkio_stats_tmpl.format(blkio_stats_prop), blkio_stats[blkio_stats_prop], tags=[short_container_id,id_tag, image_tag, name_tag, status_tag, state_tag])
+					blkio_stats=rs[self.DOCKER_BLKIO_STATS]
+					for blkio_stats_prop in [self.DOCKER_BLKIO_STATS_IO_SERVICE_BYTES_RECURSIVE,self.DOCKER_BLKIO_STATS_IO_SERVICE_RECURSIVE,self.DOCKER_BLKIO_STATS_IO_QUEUE_RECURSIVE,self.DOCKER_BLKIO_STATS_IO_SERVICE_TIME_RECURSIVE,self.DOCKER_BLKIO_STATS_IO_WAIT_TIME_RECURSIVE,self.DOCKER_BLKIO_STATS_IO_MERGED_RECURSIVE,self.DOCKER_BLKIO_STATS_IO_TIME_RECURSIVE,self.DOCKER_BLKIO_STATS_SECTORS_RECURSIVE]:
+						self.gauge(self.BLKIO_STATS_TMPL.format(blkio_stats_prop), blkio_stats[blkio_stats_prop], tags=[short_container_id,self.WINDOWS_TAG])
 
 				#storage_stats:
-					storage_stats=rs['storage_stats']
-					for storage_stats_prop in ["read_count_normalized","read_size_bytes","write_count_normalized","write_size_bytes"]:
-						self.gauge(storage_stats_tmpl.format(storage_stats_prop), storage_stats[storage_stats_prop], tags=[short_container_id,id_tag, image_tag, name_tag, status_tag, state_tag])
+					storage_stats=rs[self.DOCKER_STORAGE_STATS]
+					for storage_stats_prop in [self.DOCKER_STORAGE_STATS_READ_COUNT_NORMALIZED,self.DOCKER_STORAGE_STATS_READ_SIZE_BYTES,self.DOCKER_STORAGE_STATS_READ_WRITE_COUNT_NORMALIZED,self.DOCKER_STORAGE_STATS_READ_WRITE_SIZE_BYTES]:
+						self.gauge(self.STORAGE_STATS_TMPL.format(storage_stats_prop), storage_stats[storage_stats_prop], tags=[short_container_id,self.WINDOWS_TAG])
 
 				#cpu_stats:
-					cpu_usage=rs['cpu_stats']['cpu_usage']
-					for cpu_stat_prop in ["total_usage", "usage_in_kernelmode", "usage_in_usermode"]:
-						self.gauge(cpu_stats_tmpl.format(cpu_stat_prop), cpu_usage[cpu_stat_prop], tags=[short_container_id,id_tag, image_tag, name_tag, status_tag, state_tag])
+					cpu_usage=rs[self.DOCKER_CPU_STATS][self.DOCKER_CPU_USAGE]
+					for cpu_stat_prop in [self.DOCKER_CPU_USAGE_TOTAL_USAGE,self.DOCKER_CPU_USAGE_USAGE_IN_KERNELMODE,self.DOCKER_CPU_USAGE_USAGE_IN_USERMODE]:
+						self.gauge(self.CPU_STATS_TMPL.format(cpu_stat_prop), cpu_usage[cpu_stat_prop], tags=[short_container_id,self.WINDOWS_TAG])
 
-					throttling_data=rs['cpu_stats']['throttling_data']
-					for throttling_data_prop in ["periods","throttled_periods","throttled_time"]:
-						self.gauge(throttling_data_tmpl.format(throttling_data_prop), throttling_data[throttling_data_prop], tags=[short_container_id,id_tag, image_tag, name_tag, status_tag, state_tag])
+					throttling_data=rs[self.DOCKER_CPU_STATS][self.DOCKER_THROTTLING_DATA]
+					for throttling_data_prop in [self.DOCKER_THROTTLING_DATA_PERIODS,self.DOCKER_THROTTLING_DATA_THROTTLED_PERIODS,self.DOCKER_THROTTLING_DATA_THROTTLED_TIME]:
+						self.gauge(self.THROTTLING_DATA_TMPL.format(throttling_data_prop), throttling_data[throttling_data_prop], tags=[short_container_id,self.WINDOWS_TAG])
 
 				#precpu_stats:
-					precpu_stats=rs['precpu_stats']['cpu_usage']
-					for precpu_prop in ["total_usage", "usage_in_kernelmode", "usage_in_usermode"]:
-						self.gauge(precpu_stats_cpu_usage_tmpl.format(precpu_prop), precpu_stats[precpu_prop], tags=[short_container_id,id_tag, image_tag, name_tag, status_tag, state_tag])
+					precpu_stats=rs[self.DOCKER_PRECPU_STATS][self.DOCKER_CPU_USAGE]
+					for precpu_prop in [self.DOCKER_CPU_USAGE_TOTAL_USAGE,self.DOCKER_CPU_USAGE_USAGE_IN_KERNELMODE,self.DOCKER_CPU_USAGE_USAGE_IN_USERMODE]:
+						self.gauge(self.PRECPU_STATS_CPU_USAGE_TMPL.format(precpu_prop), precpu_stats[precpu_prop], tags=[short_container_id,self.WINDOWS_TAG])
 					
-					precpu_throttling_data=rs['precpu_stats']['throttling_data']
-					for precpu_throttling_prop in ["periods", "throttled_periods", "throttled_time"]:
-						self.gauge(precpu_stats_throttling_data_tmpl.format(precpu_throttling_prop), precpu_throttling_data[precpu_throttling_prop], tags=[short_container_id,id_tag, image_tag, name_tag, status_tag, state_tag])
+					precpu_throttling_data=rs[self.DOCKER_PRECPU_STATS][self.DOCKER_THROTTLING_DATA]
+					for precpu_throttling_prop in [self.DOCKER_THROTTLING_DATA_PERIODS,self.DOCKER_THROTTLING_DATA_THROTTLED_PERIODS,self.DOCKER_THROTTLING_DATA_THROTTLED_TIME]:
+						self.gauge(self.PRECPU_STATS_THROTTLING_DATA_TMPL.format(precpu_throttling_prop), precpu_throttling_data[precpu_throttling_prop], tags=[short_container_id,self.WINDOWS_TAG])
 
 				#memory_stats:
-					memory_stats=rs['memory_stats']
-					for memory_stat_prop in ["commitbytes", "commitpeakbytes", "privateworkingset"]:
-						self.gauge(memory_stats_tmpl.format(memory_stat_prop), memory_stats[memory_stat_prop], tags=[short_container_id,id_tag, image_tag, name_tag, status_tag, state_tag])
+					memory_stats=rs[self.DOCKER_MEMORY_STATS]
+					for memory_stat_prop in [self.DOCKER_MEMORY_COMMIBYTES, self.DOCKER_MEMORY_COMMITPEAKBYTES, self.DOCKER_MEMORY_PRIATEWORKINGSET]:
+						self.gauge(self.MEMORY_STATS_TMPL.format(memory_stat_prop), memory_stats[memory_stat_prop], tags=[short_container_id,self.WINDOWS_TAG])
 
 				#networks:
-					networks=rs['networks']
+					networks=rs[self.DOCKER_NETWORK]
 					for guid, data in networks.iteritems():
-						guid_tag = "guid:" + guid
-						for prop in ["rx_bytes", "rx_packets", "rx_errors", "rx_dropped", "tx_bytes", "tx_packets", "tx_errors", "tx_dropped"]:
-							self.gauge(net_tmpl.format(prop), data[prop], tags=[short_container_id, guid_tag,id_tag, image_tag, name_tag, status_tag, state_tag])
+						guid_tag = self.DOCKER_GUID_KEY+guid
+						for prop in [self.DOCKER_NETWORK_RX_BYTES,self.DOCKER_NETWORK_RX_PACKETS,self.DOCKER_NETWORK_RX_ERRORS,self.DOCKER_NETWORK_RX_DROPPED,self.DOCKER_NETWORK_TX_BYTES,self.DOCKER_NETWORK_TX_PACKETS,self.DOCKER_NETWORK_TX_ERRORS,self.DOCKER_NETWORK_TX_DROPPED]:
+							self.gauge(self.NET_TMPL.format(prop), data[prop], tags=[short_container_id, guid_tag,self.WINDOWS_TAG])
 
 				break
