48783c64d5ed7c83a53db867d66976c147ccae6e
tsein-bc <tsein@brightcove.com>
2019-07-09 14:00:05 -0700

    Gotta have 100% code coverage

--- aws_pricing/tests/conftest.py
+++ aws_pricing/tests/conftest.py
@@ -3,11 +3,6 @@ import pytest
 from tests.pricing_client_stubber import PricingClientStubber
 
 
-@pytest.fixture(scope='session')
-def dd_environment():
-    yield
-
-
 @pytest.fixture()
 def pricing_client():
     pricing_client = boto3.client('pricing', region_name='us-east-1')
