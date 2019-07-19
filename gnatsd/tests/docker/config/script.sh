f9e6de1f749fcf09b9590c29dce44f5176233dfe
florian.veaux <florian.veaux@datadoghq.com>
2019-07-19 10:15:19 -0400

    [ci run all] fixing tests for integrations-extras

--- gnatsd/tests/docker/config/script.sh
+++ gnatsd/tests/docker/config/script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-NATS_CONNECTION_NAME=foo-sub /usr/local/bundle/bin/nats-sub foo -s http://$NATS_SERVER:4222 &
-/usr/local/bundle/bin/nats-sub bar -s http://$NATS_SERVER:4222 &
-/usr/local/bundle/bin/nats-pub foo "foo1" -s http://$NATS_SERVER:4222
-/usr/local/bundle/bin/nats-pub foo "foo2" -s http://$NATS_SERVER:4222
-/usr/local/bundle/bin/nats-pub bar "bar1" -s http://$NATS_SERVER:4222
+apt-get update && apt-get install host -y
+NATS_SERVER_IP=`host $NATS_SERVER | awk '{ print $4 }'`
+NATS_CONNECTION_NAME=foo-sub /usr/local/bundle/bin/nats-sub foo -s http://$NATS_SERVER_IP:4222 &
+/usr/local/bundle/bin/nats-sub bar -s http://$NATS_SERVER_IP:4222 &
+/usr/local/bundle/bin/nats-pub foo "foo1" -s http://$NATS_SERVER_IP:4222
+/usr/local/bundle/bin/nats-pub foo "foo2" -s http://$NATS_SERVER_IP:4222
+/usr/local/bundle/bin/nats-pub bar "bar1" -s http://$NATS_SERVER_IP:4222
 sleep infinity
