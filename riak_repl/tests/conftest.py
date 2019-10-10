6dbcf133cc8dbd516473687e194d0fc4dfeef5d6
Britt Treece <britt.treece@gmail.com>
2019-10-09 22:42:50 -0500

    Update log_pattern and remove sleep from docker_run

--- riak_repl/tests/conftest.py
+++ riak_repl/tests/conftest.py
@@ -11,11 +11,10 @@ from .common import HERE, URL
 @pytest.fixture(scope='session')
 def dd_environment():
     compose_file = os.path.join(HERE, 'docker', 'docker-compose.yml')
-    log_patterns = ['Full-sync with site "riak-west-1" completed', 'Full-sync with site "riak-east-1" completed']
+    log_patterns = 'Fullsync complete from riak-west-1 to riak-east-1'
 
     with docker_run(
         compose_file=compose_file,
         conditions=[CheckEndpoints(URL, wait=5), CheckDockerLogs(compose_file, log_patterns, matches='all', wait=5)],
-        sleep=240,
     ):
         yield
