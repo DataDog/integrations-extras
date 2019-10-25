d1927a8181d99ed50467512d9910922633ce937f
Alexandre Yang <alexandre.yang@datadoghq.com>
2019-10-25 17:00:49 +0200

    change

--- aqua/datadog_checks/aqua/aqua.py
+++ aqua/datadog_checks/aqua/aqua.py
@@ -1,6 +1,7 @@
 # (C) Datadog, Inc. 2018
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+# CHANGE
 import requests
 import simplejson as json
 from six.moves.urllib.parse import urljoin
