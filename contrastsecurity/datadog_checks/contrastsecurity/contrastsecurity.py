aeb960f02820c294274bdb0199d979b6a48a90fb
klm207 <kikilmitchell@yahoo.com>
2019-11-25 09:43:52 -0500

    more pr changes, deleted python files, formatted maintainer name, edited json

--- contrastsecurity/datadog_checks/contrastsecurity/contrastsecurity.py
+++
@@ -1,6 +0,0 @@
-from datadog_checks.base import AgentCheck
-
-
-class ContrastsecurityCheck(AgentCheck):
-    def check(self, instance):
-        pass
--- contrastsecurity/tests/test_contrastsecurity.py
+++
@@ -1,8 +0,0 @@
-from datadog_checks.contrastsecurity import ContrastsecurityCheck
-
-
-def test_check(aggregator, instance):
-    check = ContrastsecurityCheck('contrastsecurity', {}, {})
-    check.check(instance)
-
-    aggregator.assert_all_metrics_covered()
