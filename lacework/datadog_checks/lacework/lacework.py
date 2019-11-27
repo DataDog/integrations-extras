6ff41f645a93b250674d3690a12c9f910bdea8f5
anshum.garg <anshum.garg@datadoghq.com>
2019-11-27 16:01:53 -0500

    Removed most files unnecessary for tile

--- lacework/datadog_checks/lacework/lacework.py
+++
@@ -1,9 +0,0 @@
-# (C) Datadog, Inc. 2019
-# All rights reserved
-# Licensed under a 3-clause BSD style license (see LICENSE)
-from datadog_checks.base import AgentCheck
-
-
-class LaceworkCheck(AgentCheck):
-    def check(self, instance):
-        pass
