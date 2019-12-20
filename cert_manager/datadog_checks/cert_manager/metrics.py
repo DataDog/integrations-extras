87ce2c43980d8406f2fa6129e64e54991bee269e
Ara Pulido <ara.pulido@datadoghq.com>
2019-12-20 12:23:44 +0100

    Added hierarchy to the metrics

--- cert_manager/datadog_checks/cert_manager/metrics.py
+++ cert_manager/datadog_checks/cert_manager/metrics.py
@@ -3,9 +3,9 @@
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
 METRIC_MAP = {
-    'certmanager_certificate_ready_status': 'certificate_ready_status',
-    'certmanager_certificate_expiration_timestamp_seconds': 'certificate_expiration_timestamp',
-    'certmanager_acme_client_request_count': 'acme_client_request_count',
-    'certmanager_acme_client_request_duration_seconds': 'acme_client_request_duration',
-    'certmanager_controller_sync_call_count': 'controller_sync_call_count',
+    'certmanager_certificate_ready_status': 'certificate.ready_status',
+    'certmanager_certificate_expiration_timestamp_seconds': 'certificate.expiration_timestamp',
+    'certmanager_acme_client_request_count': 'acme_client.request.count',
+    'certmanager_acme_client_request_duration_seconds': 'acme_client.request.duration',
+    'certmanager_controller_sync_call_count': 'controller.sync_call.count',
 }
--- cert_manager/tests/common.py
+++ cert_manager/tests/common.py
@@ -4,13 +4,13 @@
 from datadog_checks.stubs import aggregator
 
 EXPECTED_METRICS = {
-    'cert_manager.certificate_ready_status': aggregator.GAUGE,
-    'cert_manager.certificate_expiration_timestamp': aggregator.GAUGE,
-    'cert_manager.acme_client_request_count': aggregator.MONOTONIC_COUNT,
-    'cert_manager.acme_client_request_duration.sum': aggregator.GAUGE,
-    'cert_manager.acme_client_request_duration.count': aggregator.GAUGE,
-    'cert_manager.acme_client_request_duration.quantile': aggregator.GAUGE,
-    'cert_manager.controller_sync_call_count': aggregator.MONOTONIC_COUNT,
+    'cert_manager.certificate.ready_status': aggregator.GAUGE,
+    'cert_manager.certificate.expiration_timestamp': aggregator.GAUGE,
+    'cert_manager.acme_client.request.count': aggregator.MONOTONIC_COUNT,
+    'cert_manager.acme_client.request.duration.sum': aggregator.GAUGE,
+    'cert_manager.acme_client.request.duration.count': aggregator.GAUGE,
+    'cert_manager.acme_client.request.duration.quantile': aggregator.GAUGE,
+    'cert_manager.controller.sync_call.count': aggregator.MONOTONIC_COUNT,
     'cert_manager.prometheus.health': aggregator.GAUGE,
 }
 
