f8ea6a39629459c921a01f82596453cc590781d6
monganai <monganai@tcd.ie>
2020-03-07 21:18:20 +0000

    further tests added

--- pihole/tests/test_pihole.py
+++ pihole/tests/test_pihole.py
@@ -18,14 +18,14 @@ def test_config():
 def test_invalid_config(aggregator):
     instance = {"host": "www.google.com"}
 
-    # Invalid should fail
+    # Invalid host parameter - should fail
     with pytest.raises(Exception):
         c = PiholeCheck('pihole', {}, [instance])
         c.check(instance)
         aggregator.assert_service_check('pihole.running', PiholeCheck.CRITICAL)
 
 
-# Testing integration status check using docker
+# Testing integration status check using docker (200 endpoint)
 @pytest.mark.integrations
 @pytest.mark.usefixtures('dd_environment_pass')
 def test_service_check(aggregator, instance_pass):
@@ -35,10 +35,41 @@ def test_service_check(aggregator, instance_pass):
     aggregator.assert_service_check('pihole.running', PiholeCheck.OK)
 
 
-# Testing known metric value using docker
+# Testing integration status check using docker (404 endpoint) - no metrics returned
 @pytest.mark.integrations
 @pytest.mark.usefixtures('dd_environment_pass')
-def test_positive_response(aggregator, instance_pass):
+def test_bad_response(aggregator):
+    instance = {"host": "localhost:8888/fail"}
+    c = PiholeCheck('pihole', {}, [instance])
+    with pytest.raises(Exception):
+        c.check(instance)
+        aggregator.assert_service_check('pihole.running', PiholeCheck.CRITICAL)
+
+        METRICS = {
+            "pihole.domains_being_blocked": 0,
+            "pihole.ads_percent_blocked": 0,
+            "pihole.ads_blocked_today": 0,
+            "pihole.dns_queries_today": 0,
+            "pihole.unique_domains": 0,
+            "pihole.reply_nxdomain": 0,
+            "pihole.queries_cached": 0,
+            "pihole.queries_forwarded": 0,
+            "pihole.clients_ever_seen": 0,
+            "pihole.unique_clients": 0,
+            "pihole.reply_cname": 0,
+            "pihole.reply_nodata": 0,
+            "pihole.reply_ip": 0,
+        }
+        for metric, value in METRICS.items():
+            aggregator.assert_metric(name=metric, value=value)
+
+        aggregator.assert_all_metrics_covered()
+
+
+# Testing known metric value using docker - valid response
+@pytest.mark.integrations
+@pytest.mark.usefixtures('dd_environment_pass')
+def test_good_response(aggregator, instance_pass):
     c = PiholeCheck('pihole', {}, [instance_pass])
 
     c.check(instance_pass)
