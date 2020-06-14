fe41bd869ee9a32c916432ecc6aa164300a36588
Cody Lee <cody.lee@datadoghq.com>
2020-06-14 17:07:06 -0500

    [feature] add speedtest-cli check
    this runs speedtest-cli in a subprocess and reports the metrics from this tool
    
    it is expected that you have installed the speedtest-cli tool from https://www.speedtest.net/apps/cli
    
    make ddev test compatible with duplicate old name of check.py
    
    ddev validate ci --fix
    
    Co-authored-by: sarina-dd <57639676+sarina-dd@users.noreply.github.com>
    
    update spec.yaml through trial and error
    
    not all unit validation is correct, so just drop this
    
    attempt to address CI import issue and add codecov
    
    normalize for common install, also add helper language for note to accept license.
    
    add dashboard
    
    remove dashboards, it mentions using deprecated APIs so not worth it

---
+++ speedtest/CHANGELOG.md
@@ -0,0 +1,8 @@
+# CHANGELOG - speedtest
+
+1.0.0/ Released
+=================
+
+### Changes
+
+* [FEATURE] adds speedtest integration.
---
+++ speedtest/datadog_checks/speedtest/__init__.py
@@ -0,0 +1,4 @@
+from .__about__ import __version__
+from .check import MEGABYTE_TO_MEBIBYTE, SpeedtestCheck
+
+__all__ = ['__version__', 'MEGABYTE_TO_MEBIBYTE', 'SpeedtestCheck']
