b9108b8420e85ea5448c8f252940b671c54ab68e
Christine Chen <ChristineTChen@users.noreply.github.com>
2020-06-15 15:10:34 -0400

    import is_affirmative

--- filebeat/datadog_checks/filebeat/filebeat.py
+++ filebeat/datadog_checks/filebeat/filebeat.py
@@ -14,7 +14,7 @@ import sre_constants
 import requests
 from six import iteritems
 
-from datadog_checks.base import AgentCheck
+from datadog_checks.base import AgentCheck, is_affirmative
 from datadog_checks.base.utils.containers import hash_mutable
 
 EVENT_TYPE = SOURCE_TYPE_NAME = "filebeat"
