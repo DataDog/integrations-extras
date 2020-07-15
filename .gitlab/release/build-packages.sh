bc9ab106ce166572e89931e6c4a944d121361274
florian.veaux <florian.veaux@datadoghq.com>
2020-07-15 09:51:42 +0200

    Use the post-signature commit for building wheels

--- .gitlab/release/build-packages.sh
+++ .gitlab/release/build-packages.sh
@@ -2,8 +2,9 @@
 # http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -euxo pipefail
 IFS=$'\n\t'
+CURRENT_COMMIT=$(git rev-parse HEAD)
 curl --request POST --form "token=$CI_JOB_TOKEN" --form ref=master \
-    --form variables[ORIG_CI_BUILD_REF]=$CI_BUILD_REF \
+    --form variables[ORIG_CI_BUILD_REF]=$CURRENT_COMMIT \
     --form variables[ORIG_CI_BUILD_REF_NAME]=$CI_BUILD_REF_NAME \
     --form variables[ROOT_LAYOUT_TYPE]=extras \
     --form variables[REPO_NAME]=integrations-extras \
