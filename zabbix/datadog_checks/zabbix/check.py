1e2008db375bc5d59e4bc8134f0ceae32d9991af
KosukeKamiya <kosuke.kamiya@datadoghq.com>
2020-07-21 20:21:32 +0900

    Reformat

--- zabbix/datadog_checks/zabbix/check.py
+++ zabbix/datadog_checks/zabbix/check.py
@@ -1,10 +1,10 @@
-from datadog_checks.base import AgentCheck
-import urllib.request
 import json
+import urllib.request
+
+from datadog_checks.base import AgentCheck
 
 
 class ZabbixCheck(AgentCheck):
-
     def request(self, zabbix_api, req_data):
         req_header = {
             'Content-Type': 'application/json-rpc',
@@ -20,124 +20,96 @@ class ZabbixCheck(AgentCheck):
             return "Error"
 
     def login(self, zabbix_user, zabbix_pass, zabbix_api):
-        req_data = json.dumps({
-            'jsonrpc': '2.0',
-            'method': 'user.login',
-            'params': {
-                'user': zabbix_user,
-                'password': zabbix_pass
-            },
-            'id': 1
-        })
+        req_data = json.dumps(
+            {
+                'jsonrpc': '2.0',
+                'method': 'user.login',
+                'params': {'user': zabbix_user, 'password': zabbix_pass},
+                'id': 1,
+            }
+        )
         response = self.request(zabbix_api, req_data)
 
         token = response.get('result')
         return token
 
     def logout(self, token, zabbix_api):
-        req_data = json.dumps({
-            'jsonrpc': '2.0',
-            'method': 'user.logout',
-            'params': {},
-            'auth': token,
-            'id': 1
-        })
+        req_data = json.dumps({'jsonrpc': '2.0', 'method': 'user.logout', 'params': {}, 'auth': token, 'id': 1})
         response = self.request(zabbix_api, req_data)
         return response
 
     def get_hosts(self, token, zabbix_api, hosts=None):
         if hosts is not None:
-            req_data = json.dumps({
-                'jsonrpc': '2.0',
-                'method': 'host.get',
-                'params': {
-                    'filter': {
-                        'host': hosts
-                    },
-                    'output': [
-                        'hostid',
-                        'host'
-                    ]
-                },
-                'auth': token,
-                'id': 1
-            })
+            req_data = json.dumps(
+                {
+                    'jsonrpc': '2.0',
+                    'method': 'host.get',
+                    'params': {'filter': {'host': hosts}, 'output': ['hostid', 'host']},
+                    'auth': token,
+                    'id': 1,
+                }
+            )
         else:
-            req_data = json.dumps({
-                'jsonrpc': '2.0',
-                'method': 'host.get',
-                'params': {
-                    'output': [
-                        'hostid',
-                        'host'
-                    ]
-                },
-                'auth': token,
-                'id': 1
-            })
+            req_data = json.dumps(
+                {
+                    'jsonrpc': '2.0',
+                    'method': 'host.get',
+                    'params': {'output': ['hostid', 'host']},
+                    'auth': token,
+                    'id': 1,
+                }
+            )
 
         response = self.request(zabbix_api, req_data)
         return response.get('result')
 
     def get_items(self, token, hostids, zabbix_api, items=None):
         if items is not None:
-            req_data = json.dumps({
-                'jsonrpc': '2.0',
-                'method': 'item.get',
-                'params': {
-                    'hostids': hostids,
-                    'filter': {
-                        'name': items
+            req_data = json.dumps(
+                {
+                    'jsonrpc': '2.0',
+                    'method': 'item.get',
+                    'params': {
+                        'hostids': hostids,
+                        'filter': {'name': items},
+                        'output': ['itemid', 'name', 'hostid', 'value_type'],
                     },
-                    'output': [
-                        'itemid',
-                        'name',
-                        'hostid',
-                        'value_type'
-                    ]
-                },
-                'auth': token,
-                'id': 1
-            })
+                    'auth': token,
+                    'id': 1,
+                }
+            )
         else:
-            req_data = json.dumps({
-                'jsonrpc': '2.0',
-                'method': 'item.get',
-                'params': {
-                    'hostids': hostids,
-                    'output': [
-                        'itemid',
-                        'name',
-                        'hostid',
-                        'value_type'
-                    ]
-                },
-                'auth': token,
-                'id': 1
-            })
+            req_data = json.dumps(
+                {
+                    'jsonrpc': '2.0',
+                    'method': 'item.get',
+                    'params': {'hostids': hostids, 'output': ['itemid', 'name', 'hostid', 'value_type']},
+                    'auth': token,
+                    'id': 1,
+                }
+            )
 
         response = self.request(zabbix_api, req_data)
         return response.get('result')
 
     def get_history(self, token, hostid, itemid, value_type, zabbix_api):
-        req_data = json.dumps({
-            'jsonrpc': '2.0',
-            'method': 'history.get',
-            'params': {
-                'hostids': hostid,
-                'itemids': itemid,
-                'output': [
-                    'itemid',
-                    'value'
-                ],
-                'history': value_type,
-                'sortfield': 'clock',
-                'sortorder': 'DESC',
-                'limit': 1
-            },
-            'auth': token,
-            'id': 1
-        })
+        req_data = json.dumps(
+            {
+                'jsonrpc': '2.0',
+                'method': 'history.get',
+                'params': {
+                    'hostids': hostid,
+                    'itemids': itemid,
+                    'output': ['itemid', 'value'],
+                    'history': value_type,
+                    'sortfield': 'clock',
+                    'sortorder': 'DESC',
+                    'limit': 1,
+                },
+                'auth': token,
+                'id': 1,
+            }
+        )
 
         response = self.request(zabbix_api, req_data)
         result = response.get('result')
