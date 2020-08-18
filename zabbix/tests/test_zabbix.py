367aa69450fed83bd11ef733f9d75a5f519f939b
KosukeKamiya <kosuke.kamiya@datadoghq.com>
2020-08-18 06:03:57 +0000

    Add unit test.

--- zabbix/tests/test_zabbix.py
+++ zabbix/tests/test_zabbix.py
@@ -1,14 +1,12 @@
-from typing import Any, Dict
+import pytest
 
-from datadog_checks.base.stubs.aggregator import AggregatorStub
-from datadog_checks.dev.utils import get_metadata_metrics
+from datadog_checks.base import ConfigurationError
 from datadog_checks.zabbix import ZabbixCheck
 
 
 def test_check(aggregator, instance):
-    # type: (AggregatorStub, Dict[str, Any]) -> None
+    instance = {}
     check = ZabbixCheck('zabbix', {}, [instance])
-    check.check(instance)
 
-    aggregator.assert_all_metrics_covered()
-    aggregator.assert_metrics_using_metadata(get_metadata_metrics())
+    with pytest.raises(ConfigurationError):
+        check.check(instance)
