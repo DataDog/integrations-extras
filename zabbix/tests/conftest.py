85464132f88ad8b6fbb9745f901348c0b21f5ed8
KosukeKamiya <kosuke.kamiya@datadoghq.com>
2020-09-30 09:18:05 +0000

    Add test code

--- zabbix/tests/conftest.py
+++ zabbix/tests/conftest.py
@@ -1,11 +1,19 @@
 import pytest
 
 
-@pytest.fixture(scope='session')
-def dd_environment():
-    yield
+@pytest.fixture(scope="session")
+def instance_empty():
+    instance = {}
+    return instance
 
 
-@pytest.fixture
-def instance():
-    return {}
+@pytest.fixture(scope="session")
+def instance_missing_pass():
+    instance = {"zabbix_user": "zabbix"}
+    return instance
+
+
+@pytest.fixture(scope="session")
+def instance_missing_url():
+    instance = {"zabbix_user": "zabbix", "zabbix_password": "zabbix"}
+    return instance
