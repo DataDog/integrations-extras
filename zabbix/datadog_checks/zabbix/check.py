6483be581ae174faca523aa965f580faf0f890a9
ChristineTChen <christine.chen@datadoghq.com>
2020-10-15 15:12:30 -0400

    Add log lines and safely make req

--- zabbix/datadog_checks/zabbix/check.py
+++ zabbix/datadog_checks/zabbix/check.py
@@ -4,11 +4,20 @@ from datadog_checks.base import AgentCheck, ConfigurationError
 
 
 class ZabbixCheck(AgentCheck):
+
+    SERVICE_CHECK_NAME = "zabbix.can_connect"
+
     def request(self, zabbix_api, req_data):
         req_header = {
             'Content-Type': 'application/json-rpc',
         }
-        res = self.http.post(zabbix_api, data=req_data.encode(), headers=req_header)
+
+        try:
+            res = self.http.post(zabbix_api, data=req_data.encode(), headers=req_header)
+        except Exception as e:
+            self.log.debug("Unable to get make request to api=%s with req_data=%s", zabbix_api, req_data)
+            self.warning("Request failed: %s", str(e))
+            raise
         return res.json()
 
     def login(self, zabbix_user, zabbix_pass, zabbix_api):
@@ -20,14 +29,17 @@ class ZabbixCheck(AgentCheck):
                 'id': 1,
             }
         )
+        self.log.debug("Logging in with params user=%s api=%s", zabbix_user, zabbix_api)
+    
         response = self.request(zabbix_api, req_data)
-
         token = response.get('result')
+        self.service_check()
         return token
 
     def logout(self, token, zabbix_api):
         req_data = json.dumps({'jsonrpc': '2.0', 'method': 'user.logout', 'params': {}, 'auth': token, 'id': 1})
         response = self.request(zabbix_api, req_data)
+        self.log.debug("Logging out: %s", response)
         return response
 
     def get_hosts(self, token, zabbix_api, hosts=None):
@@ -51,9 +63,11 @@ class ZabbixCheck(AgentCheck):
                     'id': 1,
                 }
             )
-
         response = self.request(zabbix_api, req_data)
-        return response.get('result')
+            
+        result = response.get('result')
+        self.log.debug("Getting zabbix items: %s", result)
+        return result
 
     def get_items(self, token, hostids, zabbix_api, items=None):
         if items is not None:
@@ -80,7 +94,6 @@ class ZabbixCheck(AgentCheck):
                     'id': 1,
                 }
             )
-
         response = self.request(zabbix_api, req_data)
         return response.get('result')
 
@@ -112,6 +125,7 @@ class ZabbixCheck(AgentCheck):
         zabbix_user = instance.get('zabbix_user')
         zabbix_pass = instance.get('zabbix_password')
         zabbix_api = instance.get('zabbix_api')
+        tags = instance.get('tags', [])
 
         if not zabbix_user:
             raise ConfigurationError('Configuration error, please specify zabbix_user.')
@@ -154,13 +168,18 @@ class ZabbixCheck(AgentCheck):
             hostid = item['hostid']
             itemid = item['itemid']
             value_type = item['value_type']
+
+            
             history = self.get_history(token, hostid, itemid, value_type, zabbix_api)
-
-            dd_metricname = 'zabbix.' + item['name'].replace(' ', '_')
-            dd_metricvalue = history[0]['value']
-            dd_hostname = hostdic[hostid].replace(' ', '_')
-
-            self.gauge(dd_metricname, dd_metricvalue, tags=None, hostname=dd_hostname, device_name=None)
+            self.log.debug("Getting history for item %s: %s", itemid, history)
+            try:
+                dd_metricname = 'zabbix.' + item['name'].replace(' ', '_')
+                dd_metricvalue = history[0]['value']
+                dd_hostname = hostdic[hostid].replace(' ', '_')
+            except Exception as e:
+                self.log.debug("Unable to get metric")
+            else:
+                self.gauge(dd_metricname, dd_metricvalue, tags=tags, hostname=dd_hostname, device_name=None)
 
         # Revoke token
         result = self.logout(token, zabbix_api)
