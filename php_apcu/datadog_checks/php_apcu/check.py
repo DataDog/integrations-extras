546e86b8fc1173d2b0a8ecbda7ae3179c48e2d8a
withgod <noname@withgod.jp>
2020-11-16 05:08:28 +0900

    first implementation of apcu

--- php_apcu/datadog_checks/php_apcu/check.py
+++ php_apcu/datadog_checks/php_apcu/check.py
@@ -1,11 +1,29 @@
+import requests
 
-from typing import Any
-
-from datadog_checks.base import AgentCheck
+from datadog_checks.base import AgentCheck, ConfigurationError
 
 
 class PhpApcuCheck(AgentCheck):
-    def check(self, _):
-        # type: (Any) -> None
-        # Use self.instance to read the check configuration
-        pass
+    def check(self, instance):
+        url = instance.get('url')
+        if not url:
+            raise ConfigurationError('require url.')
+
+        self.log.debug('apcu check url[%s]', url)
+
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+        except Exception as e:
+            self.service_check('php_apcu.can_collect', self.CRITICAL, message=str(e))
+        else:
+            self.service_check('php_apcu.can_collect', self.OK)
+            for line in response.iter_lines(decode_unicode=True):
+                values = line.split(' ')
+                if len(values) == 2:
+                    metric, value = values
+                    try:
+                        value = float(value)
+                    except ValueError:
+                        continue
+                    self.gauge(metric, value)
--- php_apcu/tests/test_php_apcu.py
+++ php_apcu/tests/test_php_apcu.py
@@ -1,15 +1,28 @@
+import pytest
 
-from typing import Any, Dict
-
-from datadog_checks.base.stubs.aggregator import AggregatorStub
-from datadog_checks.dev.utils import get_metadata_metrics
+from datadog_checks.base import ConfigurationError
 from datadog_checks.php_apcu import PhpApcuCheck
 
 
-def test_check(aggregator, instance):
-    # type: (AggregatorStub, Dict[str, Any]) -> None
-    check = PhpApcuCheck('php_apcu', {}, [instance])
-    check.check(instance)
+@pytest.mark.unit
+def test_config():
+    instance = {}
+    c = PhpApcuCheck('php_apcu', {}, [instance])
 
-    aggregator.assert_all_metrics_covered()
-    aggregator.assert_metrics_using_metadata(get_metadata_metrics())
+    with pytest.raises(ConfigurationError):
+        c.check(instance)
+
+    c.check({'url': 'http://foobar'})
+
+
+@pytest.mark.integration
+@pytest.mark.usefixtures('dd_environment')
+def test_service_check(aggregator, instance):
+    c = PhpApcuCheck('php_apcu', {}, [instance])
+
+    c.check(instance)
+    aggregator.assert_service_check('php_apcu.can_collect', PhpApcuCheck.OK)
+
+    instance['url'] = instance['url'].replace('.php', '')
+    c.check(instance)
+    aggregator.assert_service_check('php_apcu.can_collect', PhpApcuCheck.CRITICAL)
