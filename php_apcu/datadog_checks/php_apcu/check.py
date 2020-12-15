5701f111b1ac5fa3a29caac2617e2cac0b2c0346
takumi <noname@withgod.jp>
2020-12-16 02:02:44 +0900

    php APCu Integration. (#766)
    * fist commit for apcu
    
    * agent7 only
    
    * fixed stub for black
    
    * first implementation of apcu
    
    * added docker files.
    
    * update conf.yml.example for php_apcu
    
    * update metadata.csv for php_apcu
    
    * added config description.(temporary url)
    
    * rename can_collect to can_connect.
    
    * added assets/monitors. idk how to check...
    
    * modified apcu-dd-handler.php file path.
    
    * update apcu-dd-handler url to local path.
    
    * added link to apcu manual
    
    * modified  requests.get to self.http.get
    
    * added metric_to_check section
    
    * fixed directory name.
    
    * fixed for CI.
    squashed it because I can't check it without push.
    
    * modified document.
    fixed ddev validate ci
    
    * Update php_apcu/tox.ini
    
    added docker environment.
    
    Co-authored-by: Christine Chen <ChristineTChen@users.noreply.github.com>
    
    * Apply suggestions from code review for README
    
    Co-authored-by: Kari Halsted <12926135+kayayarai@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Christine Chen <ChristineTChen@users.noreply.github.com>
    
    * added test for metrcis.
    
    * fixed test annotation.
    
    * Update php_apcu/README.md
    
    change to a good description.
    
    Co-authored-by: Christine Chen <ChristineTChen@users.noreply.github.com>
    
    * Revert "Update php_apcu/tox.ini
    "
    
    This reverts commit f75f0580a26434bfe8d48f814977158419404f31.
    
    * Apply suggestions from code review
    
    thanks @kayayarai and @ChristineTChen
    
    Co-authored-by: Kari Halsted <12926135+kayayarai@users.noreply.github.com>
    Co-authored-by: Christine Chen <ChristineTChen@users.noreply.github.com>
    
    Co-authored-by: Christine Chen <ChristineTChen@users.noreply.github.com>
    Co-authored-by: Kari Halsted <12926135+kayayarai@users.noreply.github.com>

---
+++ php_apcu/datadog_checks/php_apcu/check.py
@@ -0,0 +1,29 @@
+from datadog_checks.base import AgentCheck, ConfigurationError
+
+
+class PhpApcuCheck(AgentCheck):
+    def check(self, instance):
+        url = instance.get('url')
+        if not url:
+            raise ConfigurationError('The `url` must be specified')
+
+        self.log.debug('apcu check url[%s]', url)
+
+        try:
+            response = self.http.get(url)
+            response.raise_for_status()
+        except Exception as e:
+            self.service_check('php_apcu.can_connect', self.CRITICAL, message=str(e))
+        else:
+            self.service_check('php_apcu.can_connect', self.OK)
+            for line in response.iter_lines(decode_unicode=True):
+                values = line.split(' ')
+                if len(values) == 2:
+                    metric, value = values
+                    try:
+                        value = float(value)
+                    except ValueError:
+                        continue
+                    self.gauge(metric, value)
+                else:
+                    self.log.debug("Unexpected response: %s", values)
---
+++ php_apcu/tests/test_php_apcu.py
@@ -0,0 +1,42 @@
+import pytest
+
+from datadog_checks.base import ConfigurationError
+from datadog_checks.php_apcu import PhpApcuCheck
+
+from .common import EXPECTED_METRICS
+
+
+@pytest.mark.unit
+def test_config():
+    instance = {}
+    c = PhpApcuCheck('php_apcu', {}, [instance])
+
+    with pytest.raises(ConfigurationError):
+        c.check(instance)
+
+    c.check({'url': 'http://foobar'})
+
+
+@pytest.mark.integration
+@pytest.mark.usefixtures('dd_environment')
+def test_service_check(aggregator, instance):
+    c = PhpApcuCheck('php_apcu', {}, [instance])
+
+    c.check(instance)
+    aggregator.assert_service_check('php_apcu.can_connect', PhpApcuCheck.OK)
+
+    instance['url'] = instance['url'].replace('.php', '')
+    c.check(instance)
+    aggregator.assert_service_check('php_apcu.can_connect', PhpApcuCheck.CRITICAL)
+
+
+@pytest.mark.integration
+@pytest.mark.usefixtures('dd_environment')
+def test_metrics(aggregator, instance):
+    c = PhpApcuCheck('php_apcu', {}, [instance])
+
+    c.check(instance)
+    for k, v in EXPECTED_METRICS.items():
+        aggregator.assert_metric(k, at_least=v)
+
+    aggregator.assert_all_metrics_covered()
