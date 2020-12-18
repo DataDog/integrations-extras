9ffcc04d99ba7ccec83b8c1c5f0a9db0b4902a71
withgod <noname@withgod.jp>
2020-12-19 02:45:08 +0900

    added send tags.

--- php_opcache/datadog_checks/php_opcache/check.py
+++ php_opcache/datadog_checks/php_opcache/check.py
@@ -1,3 +1,5 @@
+from six.moves.urllib.parse import urlparse
+
 from datadog_checks.base import AgentCheck, ConfigurationError
 
 
@@ -7,16 +9,23 @@ class PhpOpcacheCheck(AgentCheck):
         if not url:
             raise ConfigurationError('The `url` must be specified')
 
+        tags = instance.get('tags', [])
+        # https://github.com/DataDog/integrations-core/blob/master/apache/datadog_checks/apache/apache.py#L53
+        parsed_url = urlparse(url)
+        server_host = parsed_url.hostname
+        server_port = parsed_url.port or 80
+        service_check_tags = ['host:%s' % server_host, 'port:%s' % server_port] + tags
+
         self.log.debug('opcache check url[%s]', url)
 
         try:
             response = self.http.get(url)
             response.raise_for_status()
         except Exception as e:
-            self.service_check('php_opcache.can_connect', self.CRITICAL, message=str(e))
+            self.service_check('php_opcache.can_connect', self.CRITICAL, tags=service_check_tags, message=str(e))
         else:
-            self.service_check('php_opcache.can_connect', self.OK)
-            for line in response.iter_lines(decode_unicode=True):
+            self.service_check('php_opcache.can_connect', self.OK, tags=service_check_tags)
+            for line in response.text.splitlines():
                 values = line.split(' ')
                 if len(values) == 2:
                     metric, value = values
@@ -24,6 +33,6 @@ class PhpOpcacheCheck(AgentCheck):
                         value = float(value)
                     except ValueError:
                         continue
-                    self.gauge(metric, value)
+                    self.gauge(metric, value, tags=tags)
                 else:
                     self.log.debug("Unexpected response: %s", values)
