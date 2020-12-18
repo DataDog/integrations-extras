2b892a0a609659b5df3b879cf90288e77cfa9dbb
Ming <itestmycode@gmail.com>
2020-12-18 15:11:26 -0500

    Update pulsar/datadog_checks/pulsar/check.py
    Co-authored-by: Christine Chen <ChristineTChen@users.noreply.github.com>

--- pulsar/datadog_checks/pulsar/check.py
+++ pulsar/datadog_checks/pulsar/check.py
@@ -92,9 +92,8 @@ class PulsarCheck(OpenMetricsBaseCheck):
                 'namespace': self.NAMESPACE,
                 'metrics': [self.metrics_mapper],
                 # 'send_histograms_buckets': send_buckets,
-                # 'send_distribution_counts_as_monotonic': instance.get('send_distribution_counts_as_monotonic', True)
-                # default to True to submit _count histogram/summary as monotonic
-                # counts to Datadog
+                'send_distribution_counts_as_monotonic': instance.get('send_distribution_counts_as_monotonic', True),
+                'send_distribution_sums_as_monotonic': instance.get('send_distribution_sums_as_monotonic', True),
             }
         )
         super(PulsarCheck, self).__init__(name, init_config, instances)
