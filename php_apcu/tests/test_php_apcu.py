5659f324890a90bf1c3bcceca2557f12ecafe25e
takumi <noname@withgod.jp>
2020-12-22 17:45:29 +0900

    PHP APCu improved (#781)
    * remove unused file.
    
    * improve test.
    
    * fixed typo.
    
    * adde send tags.
    
    * bump version to 0.0.2
    
    * added download link.
    
    * Update php_apcu/README.md
    
    Co-authored-by: Kari Halsted <12926135+kayayarai@users.noreply.github.com>
    
    Co-authored-by: Kari Halsted <12926135+kayayarai@users.noreply.github.com>

--- php_apcu/tests/test_php_apcu.py
+++ php_apcu/tests/test_php_apcu.py
@@ -1,6 +1,7 @@
 import pytest
 
 from datadog_checks.base import ConfigurationError
+from datadog_checks.dev.utils import get_metadata_metrics
 from datadog_checks.php_apcu import PhpApcuCheck
 
 from .common import EXPECTED_METRICS
@@ -24,6 +25,7 @@ def test_service_check(aggregator, instance):
 
     c.check(instance)
     aggregator.assert_service_check('php_apcu.can_connect', PhpApcuCheck.OK)
+    aggregator.reset()
 
     instance['url'] = instance['url'].replace('.php', '')
     c.check(instance)
@@ -40,3 +42,4 @@ def test_metrics(aggregator, instance):
         aggregator.assert_metric(k, at_least=v)
 
     aggregator.assert_all_metrics_covered()
+    aggregator.assert_metrics_using_metadata(get_metadata_metrics(), check_submission_type=True)
