8db4072668cdf30dffa88fee0896fdbd10a07699
SIOS\tnumanoi <tnumanoi@sios.com>
2021-01-27 17:39:55 +0900

    Get AppKeeper metrics, initial commit

--- appkeeper/datadog_checks/appkeeper/appkeeper.py
+++ appkeeper/datadog_checks/appkeeper/appkeeper.py
@@ -1,5 +1,22 @@
+import json
+import requests
 from datadog_checks.base import AgentCheck, ConfigurationError
+
+APIURL = 'https://api.appkeeper.sios.com/v2/'
 
 class AppKeeperCheck(AgentCheck):
     def check(self, instance):
-        self.service_check('appkeeper.test', self.OK)
+        account = instance.get('account')
+        integrationToken = instance.get('integrationToken')
+
+        if not account or not integrationToken:
+            raise ConfigurationError('Configuration error, please fix conf.yaml')
+
+        # APIキーの取得
+        # response = requests.get(APIURL + 'authorize?token=' + integrationToken)
+        # TODO エラーの場合の処理
+        # apiKey = json.loads(response.text)
+        self.log.info('hoge')
+        self.gauge('appkeeper.api_recover_count', 0)
+        self.gauge('appkeeper.integrated_instances', 1)
+        self.gauge('appkeeper.all_instances', 3)
