8627bf928557e30b333247f4eedf87e726627f0a
SIOS\tnumanoi <tnumanoi@sios.com>
2021-02-01 15:40:11 +0900

    refactoring mockify

--- appkeeper/datadog_checks/appkeeper/appkeeper.py
+++ appkeeper/datadog_checks/appkeeper/appkeeper.py
@@ -18,61 +18,77 @@ class AppKeeperCheck(AgentCheck):
         if not account or not integrationToken:
             raise ConfigurationError('Configuration error, please fix conf.yaml')
 
-        # Get API key
-        try:
-            response = requests.get(AUTH_API_URL + '?token=' + integrationToken)
-        except requests.exceptions.Timeout:
-            raise CheckException('Failed to get API key by timeout')
-        except Exception as e:
-            raise CheckException('Failed to get API key by {}'.format(e))
-
-        if response.status_code != 200:
-            raise ConfigurationError('Failed to get API key. status_code={}'.format(response.status_code))
-
-        try:
-            tokenJson = json.loads(response.text)
-            token = tokenJson['accessToken']
-        except Exception as e:
-            raise CheckException('Failed to get API key by {}'.format(e))
+        token = self.get_token(integrationToken)
 
         # events
-        events = call_events_api(account, token)
+        events = self.get_events(account, token)
         recover_count = get_recover_count(events)
 
         # instances
-        instances = call_instances_api(account, token)
+        instances = self.get_instances(account, token)
         number_of_all_instances = len(instances)
-        number_of_monitored_instances = get_instances_number(instances)
+        number_of_monitored_instances = get_monitoring_instances_number(instances)
 
         # send data
-        # self.gauge('appkeeper.api_recover_count', recover_count)
-        # self.gauge('appkeeper.integrated_instances', 1)
-        # self.gauge('appkeeper.all_instances', 3)
+        self.gauge('appkeeper.api_recover_count', recover_count)
+        self.gauge('appkeeper.monitored_instances', number_of_monitored_instances)
+        self.gauge('appkeeper.all_instances', number_of_all_instances)
+
+    def get_token(self, integrationToken):
+        return call_auth_api(integrationToken)
+
+    def get_events(self, account, token):
+        return call_events_api(account, token)
+
+    def get_instances(self, account, token):
+        return call_instances_api(account, token)
+
+def call_auth_api(integrationToken):
+    # Get API key
+    try:
+        response = requests.get(AUTH_API_URL + '?token=' + integrationToken)
+    except requests.exceptions.Timeout:
+        raise CheckException('Failed to get API key by timeout')
+    except Exception as e:
+        raise CheckException('Failed to get API key by {}'.format(e))
+
+    if response.status_code != 200:
+        raise ConfigurationError('Failed to get API key. status_code={}'.format(response.status_code))
+
+    try:
+        tokenJson = json.loads(response.text)
+        token = tokenJson['accessToken']
+    except Exception as e:
+        raise CheckException('Failed to get API key by {}'.format(e))
+
+    return token
 
 def call_events_api(account, token):
-    eventsJson = call_api_get(EVENT_API_URL.format(account), token)
+    try:
+        eventsJson = call_api_get(EVENT_API_URL.format(account), token)
+    except requests.exceptions.Timeout:
+        raise CheckException('Failed to get events by timeout')
+    except Exception as e:
+        raise CheckException('Failed to get events by {}'.format(e))
     return eventsJson['events']
 
 def call_instances_api(account, token):
-    instancesJson = call_api_get(INSTANCES_API_URL.format(account), token)
+    try:
+        instancesJson = call_api_get(INSTANCES_API_URL.format(account), token)
+    except requests.exceptions.Timeout:
+        raise CheckException('Failed to get instances by timeout')
+    except Exception as e:
+        raise CheckException('Failed to get instances by {}'.format(e))
     return instancesJson['instances']
 
 def call_api_get(url, token):
-    try:
-        headers = {'Authorization' :'Bearer {}'.format(token)}
-        response = requests.get(url, headers=headers)
-    except requests.exceptions.Timeout:
-        raise CheckException('Failed to get {} by timeout'.format(key))
-    except Exception as e:
-        raise CheckException('Failed to get {} by {}'.format(key, e))
+    headers = {'Authorization' :'Bearer {}'.format(token)}
+    response = requests.get(url, headers=headers)
 
     if response.status_code != 200:
         raise ConfigurationError('Failed to get {}. status_code={}'.format(key, response.status_code))
 
-    try:
-        resultsJson = json.loads(response.text)
-    except Exception as e:
-        raise CheckException('Failed to get {} by {}'.format(key, e))
+    resultsJson = json.loads(response.text)
 
     return resultsJson
 
@@ -87,5 +103,5 @@ def isInLastOneHour(event, now=datetime.now(timezone.utc)):
     eventHour = parse(event['startTime'])
     return oneHourAgo < eventHour
 
-def get_instances_number(instances):
+def get_monitoring_instances_number(instances):
     return len(list(filter(lambda x: x['state'] == 'monitoring', instances)))
