7c11b8d4f3688867d9291fbb241a3427af80d0af
SIOS\tnumanoi <tnumanoi@sios.com>
2021-02-02 10:26:46 +0900

    refactoring

--- appkeeper/tests/test_appkeeper.py
+++ appkeeper/tests/test_appkeeper.py
@@ -1,5 +1,5 @@
+import pytest, os, json
 from mock import Mock
-import pytest
 from datadog_checks.appkeeper.appkeeper import AppKeeperCheck
 from datadog_checks.base import ConfigurationError
 from datadog_checks.base.errors import CheckException
@@ -35,99 +35,9 @@ def test_check(aggregator):
     # aggregator.assert_all_metrics_covered()
 
 def mock_call_events_api(account, token):
-    return [
-        {
-            "status": "success",
-            "checkStatus": None,
-            "instanceId": "i-000000001",
-            "eventId": "dummy_event_id1",
-            "type": "recovery",
-            "startTime": "2021-01-25T01:06:55.683Z",
-            "endTime": "2021-01-25T01:07:07.594Z",
-            "services": [
-                "crond"
-            ],
-            "awsAccountId": "1234556789012",
-            "region": "eu-central-1",
-            "instanceName": "dummyInstance01",
-            "coatiRecovery": True,
-            "requester": "api",
-            "faultLogId": None,
-        },
-        {
-            "status": "success",
-            "checkStatus": None,
-            "instanceId": "i-000000002",
-            "eventId": "dummy_event_id2",
-            "type": "recovery",
-            "startTime": "2021-01-25T00:50:45.274Z",
-            "endTime": "2021-01-25T00:50:49.027Z",
-            "services": [
-                "crond"
-            ],
-            "awsAccountId": "123456789012",
-            "region": "eu-central-1",
-            "instanceName": "dummyInstance02",
-            "coatiRecovery": True,
-            "requester": "api",
-            "faultLogId": None,
-        },
-        {
-            "status": "success",
-            "checkStatus": "success",
-            "instanceId": "i-00000001",
-            "eventId": "dummy_event_id3",
-            "type": "recovery",
-            "startTime": "2020-10-09T01:29:10.959Z",
-            "endTime": "2020-10-09T01:29:27.673Z",
-            "services": [
-                "ntpd.service"
-            ],
-            "awsAccountId": "123456789012",
-            "region": "eu-central-1",
-            "instanceName": "dummyInstance01",
-            "coatiRecovery": True,
-            "requester": "coati",
-            "faultLogId": None,
-        },
-    ]
+    with open(os.path.join(os.path.dirname(__file__), 'fixtures', 'events_data.json'), 'r') as f:
+        return json.load(f)
 
 def mock_call_instances_api(account, token):
-    return [
-        {
-            "instanceId": "i-00000001",
-            "instanceType": "t2.small",
-            "instanceState": "running",
-            "region": "ap-northeast-1",
-            "tags": [],
-            "state": "monitoringStopped",
-            "configuration": "notMonitored",
-            "check": False,
-            "serviceSearchStatus": "success",
-            "recovery": "full"
-        },
-        {
-            "instanceId": "i-00000002",
-            "instanceType": "t2.small",
-            "instanceState": "stopped",
-            "region": "ap-northeast-1",
-            "tags": [],
-            "state": "ssmNotAvailable",
-            "configuration": "outOfMonitored",
-            "check": False,
-            "serviceSearchStatus": None,
-            "recovery": "full"
-        },
-        {
-            "instanceId": "i-00000003",
-            "instanceType": "t3.small",
-            "instanceState": "running",
-            "region": "eu-central-1",
-            "tags": [],
-            "state": "monitoring",
-            "configuration": "monitored",
-            "check": True,
-            "serviceSearchStatus": "success",
-            "recovery": "apiOnly"
-        },
-    ]
+    with open(os.path.join(os.path.dirname(__file__), 'fixtures', 'instances_data.json'), 'r') as f:
+        return json.load(f)
