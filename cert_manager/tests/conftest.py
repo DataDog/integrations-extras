b00045c15bb04407d1c868087a949e22aee05b15
Ara Pulido <ara.pulido@datadoghq.com>
2021-03-10 16:41:52 +0100

    Fix lint errors

--- cert_manager/tests/conftest.py
+++ cert_manager/tests/conftest.py
@@ -5,7 +5,6 @@ import os
 
 import pytest
 
-from datadog_checks.base.utils.common import get_docker_hostname
 from datadog_checks.dev import run_command
 from datadog_checks.dev.kind import kind_run
 from datadog_checks.dev.kube_port_forward import port_forward
@@ -19,24 +18,41 @@ except ImportError:
 HERE = os.path.dirname(os.path.abspath(__file__))
 PORT = 9402
 
-def setup_cert_manager():
 
-    run_command(["kubectl", "apply", "-f", "https://github.com/jetstack/cert-manager/releases/download/v1.2.0/cert-manager.yaml"])
+def setup_cert_manager():
+    run_command(
+        [
+            "kubectl",
+            "apply",
+            "-f",
+            "https://github.com/jetstack/cert-manager/releases/download/v1.2.0/cert-manager.yaml",
+        ]
+    )
     run_command(
         ["kubectl", "wait", "deployments", "--all", "--for=condition=Available", "-n", "cert-manager", "--timeout=300s"]
     )
     run_command(["kubectl", "wait", "pods", "-n", "cert-manager", "--all", "--for=condition=Ready", "--timeout=300s"])
     config = os.path.join(HERE, 'kubernetes', 'test-resources.yaml')
     run_command(["kubectl", "create", "-f", config])
-    run_command(["kubectl", "wait", "certificates", "-n", "cert-manager-test", "--all", "--for=condition=Ready", "--timeout=300s"])
+    run_command(
+        [
+            "kubectl",
+            "wait",
+            "certificates",
+            "-n",
+            "cert-manager-test",
+            "--all",
+            "--for=condition=Ready",
+            "--timeout=300s",
+        ]
+    )
+
 
 @pytest.fixture(scope='session')
 def dd_environment():
     with kind_run(conditions=[setup_cert_manager]) as kubeconfig:
         with ExitStack() as stack:
-            ip_ports = [
-                stack.enter_context(port_forward(kubeconfig, 'cert-manager', 'cert-manager', PORT))
-            ]
+            ip_ports = [stack.enter_context(port_forward(kubeconfig, 'cert-manager', 'cert-manager', PORT))]
         instances = {
             'instances': [
                 {'prometheus_url': 'http://{}:{}/metrics'.format(*ip_ports[0])},
@@ -44,6 +60,7 @@ def dd_environment():
         }
 
         yield instances
+
 
 @pytest.fixture
 def instance():
