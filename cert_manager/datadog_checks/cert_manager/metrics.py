2da99ee1bff741ee731c6ec97e71e24b6b5f64c2
Ara Pulido <ara.pulido@datadoghq.com>
2021-03-16 09:13:06 +0100

    Fix ACME HTTP metrics names (#826)

--- cert_manager/datadog_checks/cert_manager/metrics.py
+++ cert_manager/datadog_checks/cert_manager/metrics.py
@@ -5,7 +5,7 @@
 METRIC_MAP = {
     'certmanager_certificate_ready_status': 'certificate.ready_status',
     'certmanager_certificate_expiration_timestamp_seconds': 'certificate.expiration_timestamp',
-    'certmanager_acme_client_request_count': 'acme_client.request.count',
-    'certmanager_acme_client_request_duration_seconds': 'acme_client.request.duration',
+    'certmanager_http_acme_client_request_count': 'http_acme_client.request.count',
+    'certmanager_http_acme_client_request_duration_seconds': 'http_acme_client.request.duration',
     'certmanager_controller_sync_call_count': 'controller.sync_call.count',
 }
