a903557a1e034e1a777093011e0c5b9a0384f893
Ara Pulido <ara.pulido@datadoghq.com>
2021-03-24 11:06:13 +0100

    Add e2e tests for cert_manager integration (#820)
    * Split metric names into different origins
    * Add e2e tests for cert_manager integration

--- cert_manager/datadog_checks/cert_manager/metrics.py
+++ cert_manager/datadog_checks/cert_manager/metrics.py
@@ -1,11 +1,17 @@
-# (C) Datadog, Inc. 2019
+# (C) Datadog, Inc. 2019-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
-METRIC_MAP = {
+CERT_METRICS = {
     'certmanager_certificate_ready_status': 'certificate.ready_status',
     'certmanager_certificate_expiration_timestamp_seconds': 'certificate.expiration_timestamp',
+}
+
+CONTROLLER_METRICS = {
+    'certmanager_controller_sync_call_count': 'controller.sync_call.count',
+}
+
+ACME_METRICS = {
     'certmanager_http_acme_client_request_count': 'http_acme_client.request.count',
     'certmanager_http_acme_client_request_duration_seconds': 'http_acme_client.request.duration',
-    'certmanager_controller_sync_call_count': 'controller.sync_call.count',
 }
