665944daffde8c2ebd3c667c3c220e72fa5d6ec1
MahithaB <60710901+MahithaB@users.noreply.github.com>
2021-04-01 11:45:19 -0700

    Moving jfrog_metrics to a branch

--- jfrog_metrics/CHANGELOG.md
+++
@@ -1,2 +0,0 @@
-# CHANGELOG - jfrog_metrics
-
--- jfrog_metrics/datadog_checks/jfrog_metrics/xray_metrics.py
+++
@@ -1,8 +0,0 @@
-# (C) Datadog, Inc. 2019
-# All rights reserved
-# Licensed under a 3-clause BSD style license (see LICENSE)
-
-XRAY_METRIC_MAP = {
-    'sys_memory_free_bytes': 'sys_memory_free',
-    'sys_memory_used_bytes': 'sys_memory_used',
-}
