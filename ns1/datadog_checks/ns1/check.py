0465f13bcdbbee9a1c8354d6a9bba8bf54a36094
Dragan <dblagojevic@daitan.com>
2021-04-09 13:08:08 -0700

    qps working

--- ns1/datadog_checks/ns1/check.py
+++ ns1/datadog_checks/ns1/check.py
@@ -1,53 +1,69 @@
-
 from typing import Any
-
 from datadog_checks.base import AgentCheck
-
 # from datadog_checks.base.utils.db import QueryManager
-# from requests.exceptions import ConnectionError, HTTPError, InvalidURL, Timeout
-# from json import JSONDecodeError
+from requests.exceptions import ConnectionError, HTTPError, InvalidURL, Timeout
+import json
 
 
 class Ns1Check(AgentCheck):
     NS1_SERVICE_CHECK = "ns1.can_connect"
 
-    def __init__(self, name, init_config, instances):
-        # super(Ns1Check, self).__init__(name, init_config, instances)
+    # def __init__(self, name, init_config, instances):
+    # super(Ns1Check, self).__init__(name, init_config, instances)
 
-        # If the check is going to perform SQL queries you should define a query manager here.
-        # More info at
-        # https://datadoghq.dev/integrations-core/base/databases/#datadog_checks.base.utils.db.core.QueryManager
-        # sample_query = {
-        #     "name": "sample",
-        #     "query": "SELECT * FROM sample_table",
-        #     "columns": [
-        #         {"name": "metric", "type": "gauge"}
-        #     ],
-        # }
-        # self._query_manager = QueryManager(self, self.execute_query, queries=[sample_query])
-        # self.check_initializations.append(self._query_manager.compile_queries)
-        pass
+    # If the check is going to perform SQL queries you should define a query manager here.
+    # More info at
+    # https://datadoghq.dev/integrations-core/base/databases/#datadog_checks.base.utils.db.core.QueryManager
+    # sample_query = {
+    #     "name": "sample",
+    #     "query": "SELECT * FROM sample_table",
+    #     "columns": [
+    #         {"name": "metric", "type": "gauge"}
+    #     ],
+    # }
+    # self._query_manager = QueryManager(self, self.execute_query, queries=[sample_query])
+    # self.check_initializations.append(self._query_manager.compile_queries)
+    
+    #pass
 
-    def check(self, _):
-        # type: (Any) -> None
-        # The following are useful bits of code to help new users get started.
-
+    def check(self, instance):
         # Use self.instance to read the check configuration
-        url = self.instance.get("url")
 
-        # Perform HTTP Requests with our HTTP wrapper.
-        # More info at https://datadoghq.dev/integrations-core/base/http/
-        # try:
-        #     response = self.http.get(url)
-        #     response.raise_for_status()
-        #     response_json = response.json()
+        self.api_endpoint = self.instance.get("api_endpoint")
+        self.api_key = self.instance.get("api_key")
+        self.headers = {
+            "X-NSONE-Key": self.api_key
+        }
 
-        
+        metrics = self.instance.get("metrics")
+        if len(metrics)==0 or len(metrics)<4:
+            raise Exception('Invalid metrics config!')
+
+        checkUrl={}
+        for key, val in metrics.items():
+            if key == "qps":
+                checkUrl.update(self.getStatsUrl(key, val)) #append dictionary
+            elif key == "usage":
+                checkUrl.update(self.getStatsUrl(key, val)) #append dictionary
+
+        for k,v in checkUrl.items():
+            #for u in checkUrl:
+            try:
+                res = self.getStats(v)
+                if res:                    
+                    # qps = res["qps"]
+                    qps = self.extractMetric(k, res)
+                else:
+                    qps = -1.0
+                self.sendMetrics(k, qps)
+            except Exception: #errors are already handled, just keep going
+                raise
+                #pass
 
         # This is how you submit metrics
         # There are different types of metrics that you can submit (gauge, event).
         # More info at https://datadoghq.dev/integrations-core/base/api/#datadog_checks.base.checks.base.AgentCheck
-        self.gauge("test", 1.23, tags=['foo:bar'])
+        #self.gauge("test", 1.23, tags=['foo:bar'])
 
         # Perform database queries using the Query Manager
         # self._query_manager.execute()
@@ -63,13 +79,79 @@ class Ns1Check(AgentCheck):
         # https://datadoghq.dev/integrations-core/base/api/#datadog_checks.base.checks.base.AgentCheck.service_check
         # self.service_check("ns1.can_connect", AgentCheck.OK)
 
-        pass
+        #pass
+    def extractMetric(self, key, result):
+        try:
+            if "qps" in key:
+                qps = result["qps"]
+                return qps
+            elif "usage" in key:
+                queries = result[0]["queries"]
+                return queries
+        except Exception:
+            raise
+            #return None
+
+    def getStatsUrl(self, key, val):
+        urlList={} #dictionary in form of <metric name>:<metric url>}
+        suffix = ""
+        if key == "qps":
+            suffix = ""
+        elif key == "usage":
+            suffix = "?period=1h&expand=false&networks=*"
+        elif key == "pulsar":
+            suffix = ""
+        elif key == "ddi":
+            suffix = ""
+
+        #just get account wide stats
+        url = "%s/v1/stats/%s%s" % (self.api_endpoint, key, suffix)
+        urlList[key] = url
+        
+        if val:
+            url = "%s/v1/stats/%s%s" % (self.api_endpoint, key, suffix)
+            urlList[key] = url
+            for zoneDict in val:
+                #zone is again dictionary, with zone name as key and records as list of objects
+                for domain, records in zoneDict.items():
+                    #here, domain is zone name, records is list of records and record types
+                    url = "%s/v1/stats/%s/%s%s" % (self.api_endpoint, key, domain, suffix)
+                    urlList["%s.%s" % (key, domain)] = url
+                    
+                    if records:
+                        for rec in records:
+                            for r,t in rec.items():
+                                #https://{{api_url}}/v1/stats/qps/{{zone_name}}/{{record_name}}/{{record_type}}
+                                #https://{{api_url}}/v1/stats/usage?period=24h&expand=true&networks=*
+                                url = "%s/v1/stats/%s/%s/%s/%s%s" % (self.api_endpoint, key, domain, r + "." + domain, t, suffix)
+                                urlList["%s.%s.%s.%s" % (key, domain, r, t)] = url                                
+        return urlList
+
+    #def _build_stats_url(self, querytype, zone='',record='',rtype=''):    
+    def _build_stats_url(self, querytype, url):
+            
+        if querytype == "qps":
+            return "%s/v1/stats/%s" % (self.api_endpoint, url)
+        elif querytype == "usage":
+            #usage account wide
+            return "%s/v1/stats/%s?period=1h&expand=true&networks=*" % (self.api_endpoint, url)
+            #https://{{api_url}}/v1/stats/usage?period=24h&expand=true&networks=*
+        elif querytype == "pulsar":
+            #usage account wide
+            return "%s/v1/stats/%s?period=1h&expand=true&networks=*" % (self.api_endpoint, url)
+            #https://{{api_url}}/v1/pulsar/apps/{{pulsar_app_id}}/jobs/{{pulsar_job_id}}/data?period=1h&agg=p50
 
     def getStats(self, url):
+        # Perform HTTP Requests with our HTTP wrapper.
+        # More info at https://datadoghq.dev/integrations-core/base/http/
         try:
-            response = self.http.get(url)
+            #response = self.http.get(self._build_url(url), headers=self.headers)
+            response = self.http.get(url, headers=self.headers)
             response.raise_for_status()
             response_json = response.json()
+
+            return response_json
+
         except Timeout as e:
             self.service_check(
                 self.NS1_SERVICE_CHECK,
@@ -86,7 +168,7 @@ class Ns1Check(AgentCheck):
             )
             raise
 
-        except JSONDecodeError as e:
+        except json.JSONDecodeError as e:
             self.service_check(
                 self.NS1_SERVICE_CHECK,
                 AgentCheck.CRITICAL,
@@ -100,11 +182,101 @@ class Ns1Check(AgentCheck):
             )
             raise
         except Exception:
-            self.service_check(self.NS1_SERVICE_CHECK, AgentCheck.CRITICAL, message = "Error getting stats frmo NS1 DNS")
+            self.service_check(self.NS1_SERVICE_CHECK, AgentCheck.CRITICAL, message="Error getting stats frmo NS1 DNS")
             raise
 
+    def getMetrics(self):
+        # Extracted variables (metrics) from below
+        # qps
+        # <list of zone names>
+        # <list of tuples for each zone <record name>:<record type>>
+        # usage
+        # <list of zones for usage>
+        # <list of tuples for each zone <record name>:<record type>>
+        # account - to extract account limits
+        # zone details - same list as above for each zone
+        # <list of pulsar app id's to get agregated perf data> For each app, need list of job id's
+        # <list of records to get pulsar info for r<record name><record type>>
+        # For ddi:
+        # leases
+        # leases - <list of scope groups> to get leases ofr each
+        # lps
+        # leases - <list of scope groups> to get leases ofr each
+        #QPS accont wide
+        #https://{{api_url}}/v1/stats/qps
         
-        return response_json
+        #List  zones
+        #https://{{api_url}}/v1/zones
 
-    def SendMetrics(self, metricName, metricValue):
+        #QPS fpr zone
+        #https://{{api_url}}/v1/stats/qps/{{zone_name}}
+
+        #QPS for zone record level
+        #https://{{api_url}}/v1/stats/qps/{{zone_name}}/{{record_name}}/{{record_type}}
+        
+        #USAGE
+        #usage account wide
+        #https://{{api_url}}/v1/stats/usage?period=24h&expand=true&networks=*
+
+        #usage zone level
+        #https://{{api_url}}/v1/stats/usage/{{zone_name}}?period=24h&expand=false&networks=*
+
+        #usage record level
+        #https://{{api_url}}/v1/stats/usage/{{zone_name}}/{{record_name}}/{{record_type}}?period=24h&expand=true&networks=*
+
+        #Account plan details
+        #https://{{api_url}}/v1/account/plan
+        
+        #Account zone details
+        #https://{{api_url}}/v1/zones/{{zone_name}}
+
+        #PULSAR
+        #aggregated performance data
+        #https://{{api_url}}/v1/pulsar/apps/{{pulsar_app_id}}/jobs/{{pulsar_job_id}}/data?period=1h&agg=p50
+        
+        #List jobs within an app
+        #https://{{api_url}}/v1/pulsar/apps/{{pulsar_app_id}}/jobs
+        
+        #View availability data
+        #https://{{api_url}}/v1/pulsar/apps/{appID}/jobs/{jobID}/availability
+        
+        #View decisions for account
+        #https://{{api_url}}/v1/pulsar/query/decision/customer?period=24h
+
+        #View insufficient decision data for account
+        #https://{{api_url}}/v1/pulsar/query/decision/customer/undetermined?period=24h
+
+        #View decisions for record
+        #https://{{api_url}}/v1/pulsar/query/decision/record/{{record_name}}/{{record_type}}?period=24h
+
+        #View all route map hits
+        #https://{{api_url}}/v1/pulsar/query/routemap/hit/customer?period=24h
+
+        #View route map misses by record
+        #https://{{api_url}}/v1/pulsar/query/routemap/miss/record/{{record_name}}/{{record_type}}?period=1h
+
+        #list pulsar applications
+        #https://{{api_url}}/v1/pulsar/apps/1xy4sn3
+
+        #DDI API
+        #account wide lease statistics
+        #https://{{api_url}}/v1/stats/leases?period=24h
+
+        #View lease statistics by scope group
+        #https://{{api_url}}/v1/stats/leases/{{scope_group_ID}}?period=24h
+
+        #View account-wide peak LPS
+        #https://{{api_url}}/v1/stats/lps?period=24h
+
+        #View account-wide peak LPS by scope group
+        #https://{{api_url}}/v1/stats/lps/{{scope_group_ID}}?period=24h
+
+
+
+
+
+        pass    
+        
+
+    def sendMetrics(self, metricName, metricValue):
         self.gauge('ns1.{}'.format(metricName), metricValue)
