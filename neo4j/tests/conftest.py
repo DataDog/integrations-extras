df256b7071972e1dcfdfa88423a5e4610db93430
npezzotti <nathan.pezzotti@datadoghq.com>
2021-05-27 15:51:16 -0400

    moved auth envar from docker-compose to conftest

--- neo4j/tests/conftest.py
+++ neo4j/tests/conftest.py
@@ -4,7 +4,7 @@ import pytest
 
 from datadog_checks.dev.docker import docker_run
 
-from .common import NEO4J_MINIMAL_CONFIG
+from .common import NEO4J_AUTH, NEO4J_MINIMAL_CONFIG
 
 HERE = os.path.dirname(os.path.abspath(__file__))
 DOCKER_DIR = os.path.join(HERE, 'docker')
@@ -13,7 +13,7 @@ DOCKER_DIR = os.path.join(HERE, 'docker')
 @pytest.fixture(scope='session')
 def dd_environment():
     instance = NEO4J_MINIMAL_CONFIG
-    envs = {'NEO4J_VERSION': os.environ['NEO4J_VERSION']}
+    envs = {'NEO4J_VERSION': os.environ['NEO4J_VERSION'], 'NEO4J_AUTH': NEO4J_AUTH}
     with docker_run(
         os.path.join(DOCKER_DIR, 'docker-compose.yaml'),
         env_vars=envs,
