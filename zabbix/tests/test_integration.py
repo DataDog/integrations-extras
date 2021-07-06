bd52b29a7457146df34f6d56be1f7ea01e1bb6cd
Julia Simon <611228+hithwen@users.noreply.github.com>
2021-07-06 11:35:04 +0200

    Make assertions optional

--- zabbix/tests/test_integration.py
+++ zabbix/tests/test_integration.py
@@ -1,5 +1,7 @@
 import pytest
 
+from datadog_checks.dev.utils import get_metadata_metrics
+
 from .common import EXPECTED_METRICS
 
 
@@ -9,6 +11,7 @@ def test_e2e(dd_agent_check, instance_e2e):
     aggregator = dd_agent_check(instance_e2e)
 
     for metric in EXPECTED_METRICS:
-        aggregator.assert_metric(metric)
+        aggregator.assert_metric(metric, at_least=0)
 
     aggregator.assert_all_metrics_covered()
+    aggregator.assert_metrics_using_metadata(get_metadata_metrics())
