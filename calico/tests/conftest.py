eb15c0ddd3fb37906e3f8bf9638fafdbd924dda6
MaxLmqr <max.lemasquerier@protonmail.com>
2021-08-16 14:04:26 +0200

    <contribution/calico> update conftest.py

--- calico/tests/conftest.py
+++ calico/tests/conftest.py
@@ -9,7 +9,7 @@ def dd_environment():
 @pytest.fixture
 def instance():
     return {
-        "prometheus_url": "http://localhost:54127//metrics",
+        "prometheus_url": "http://localhost:9091/metrics",
         "namespace": "calico",
         "metrics": ["felix_active_local_endpoints"],
     }
