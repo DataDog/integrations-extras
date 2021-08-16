3fd82b9682d11e58b90528e9c8e11bb343db6c71
MaxLmqr <max.lemasquerier@protonmail.com>
2021-08-16 17:22:14 +0200

    <contribution/calico> update test module and metadata

--- calico/tests/conftest.py
+++ calico/tests/conftest.py
@@ -11,5 +11,24 @@ def instance():
     return {
         "prometheus_url": "http://localhost:9091/metrics",
         "namespace": "calico",
-        "metrics": ["felix_active_local_endpoints"],
+        "metrics":
+            [
+                "felix_active_local_endpoints",
+                "felix_active_local_policies",
+                "felix_active_local_selectors",
+                "felix_active_local_tags",
+                "felix_cluster_num_host_endpoints",
+                "felix_cluster_num_hosts",
+                "felix_cluster_num_workload_endpoints",
+                "felix_ipset_calls",
+                "felix_ipset_errors",
+                "felix_ipsets_calico",
+                "felix_ipsets_total",
+                "felix_iptables_chains",
+                "felix_iptables_rules",
+                "felix_iptables_restore_calls",
+                "felix_iptables_restore_errors",
+                "felix_iptables_save_calls",
+                "felix_iptables_save_errors",
+            ],
     }
