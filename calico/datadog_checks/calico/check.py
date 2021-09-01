17bc7f78bbc8559dc2c072d2304b60692499a3aa
MaxLmqr <max.lemasquerier@protonmail.com>
2021-09-01 17:15:29 +0200

    <contribution:calico> update calico check && metadata

--- calico/datadog_checks/calico/check.py
+++ calico/datadog_checks/calico/check.py
@@ -1,22 +1,11 @@
-from typing import Any
+from datadog_checks.base import OpenMetricsBaseCheckV2
 
-from datadog_checks.base import ConfigurationError, OpenMetricsBaseCheckV2
 
 class CalicoCheck(OpenMetricsBaseCheckV2):
     def __init__(self, name, init_config, instances=None):
-        METRICS_MAP = {'felix_active_local_endpoints': 'felix_active_local_endpoints'}
 
         super(CalicoCheck, self).__init__(
             name,
             init_config,
             instances,
-            default_instances={
-                'calico-felix': {
-                    'prometheus_url': 'http://localhost:9091/metrics',
-                    'namespace': 'kube-system',
-                    'metrics': [METRICS_MAP],
-                }
-            },
-            default_namespace='kube-system',
         )
-
