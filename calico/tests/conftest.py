25f1a1d25db81a5526336b9653989bf712f803e4
MaxLmqr <max.lemasquerier@protonmail.com>
2021-09-01 17:14:39 +0200

    <contribution:calico> update tests

--- calico/tests/conftest.py
+++ calico/tests/conftest.py
@@ -9,9 +9,9 @@ def dd_environment():
 @pytest.fixture
 def instance():
     return {
-        "prometheus_url": "http://localhost:9091/metrics",
+        "openmetrics_endpoint": "http://localhost:9091/metrics",
         "namespace": "calico",
-        "metrics": [
+        "extra_metrics": [
             "felix_active_local_endpoints",
             "felix_active_local_policies",
             "felix_active_local_selectors",
--- calico/tests/test_calico.py
+++ calico/tests/test_calico.py
@@ -1,39 +1,13 @@
-from typing import Any, Dict
-
 import pytest
-from requests.exceptions import ConnectionError
 
-from datadog_checks.base import ConfigurationError
-from datadog_checks.base.errors import CheckException
-from datadog_checks.base.stubs.aggregator import AggregatorStub
 from datadog_checks.calico import CalicoCheck
 from datadog_checks.dev.utils import get_metadata_metrics
 
 
-@pytest.mark.unit
-def test_config():
-    # instance vide
-    with pytest.raises(CheckException):
-        instance = {}
-        CalicoCheck('calico', {}, [instance])
-
-    instance = {'prometheus_url': "http://foo.bar"}
-    c = CalicoCheck('calico', {}, instance)
-
-    # instance vide
-    with pytest.raises(ConfigurationError):
-        c.check({})
-
-    # url defined but not reachable
-    with pytest.raises(ConnectionError):
-        c.check({'prometheus_url': "http://foo.bar"})
-
-
 @pytest.mark.integration
-def test_check(aggregator, instance):
-    # type: (AggregatorStub, Dict[str, Any]) -> None
+def test_check(aggregator, instance, dd_run_check):
     check = CalicoCheck('calico', {}, [instance])
-    check.check(instance)
+    dd_run_check(check)
 
     aggregator.assert_metric("calico.felix_active_local_endpoints", metric_type=aggregator.GAUGE)
     aggregator.assert_metric("calico.felix_active_local_policies", metric_type=aggregator.GAUGE)
@@ -42,16 +16,16 @@ def test_check(aggregator, instance):
     aggregator.assert_metric("calico.felix_cluster_num_host_endpoints", metric_type=aggregator.GAUGE)
     aggregator.assert_metric("calico.felix_cluster_num_hosts", metric_type=aggregator.GAUGE)
     aggregator.assert_metric("calico.felix_cluster_num_workload_endpoints", metric_type=aggregator.GAUGE)
-    aggregator.assert_metric("calico.felix_ipset_calls", metric_type=aggregator.MONOTONIC_COUNT)
-    aggregator.assert_metric("calico.felix_ipset_errors", metric_type=aggregator.MONOTONIC_COUNT)
+    aggregator.assert_metric("calico.felix_ipset_calls.count", metric_type=aggregator.MONOTONIC_COUNT)
+    aggregator.assert_metric("calico.felix_ipset_errors.count", metric_type=aggregator.MONOTONIC_COUNT)
     aggregator.assert_metric("calico.felix_ipsets_calico", metric_type=aggregator.GAUGE)
     aggregator.assert_metric("calico.felix_ipsets_total", metric_type=aggregator.GAUGE)
     aggregator.assert_metric("calico.felix_iptables_chains", metric_type=aggregator.GAUGE)
     aggregator.assert_metric("calico.felix_iptables_rules", metric_type=aggregator.GAUGE)
-    aggregator.assert_metric("calico.felix_iptables_restore_calls", metric_type=aggregator.MONOTONIC_COUNT)
-    aggregator.assert_metric("calico.felix_iptables_restore_errors", metric_type=aggregator.MONOTONIC_COUNT)
-    aggregator.assert_metric("calico.felix_iptables_save_calls", metric_type=aggregator.MONOTONIC_COUNT)
-    aggregator.assert_metric("calico.felix_iptables_save_errors", metric_type=aggregator.MONOTONIC_COUNT)
+    aggregator.assert_metric("calico.felix_iptables_restore_calls.count", metric_type=aggregator.MONOTONIC_COUNT)
+    aggregator.assert_metric("calico.felix_iptables_restore_errors.count", metric_type=aggregator.MONOTONIC_COUNT)
+    aggregator.assert_metric("calico.felix_iptables_save_calls.count", metric_type=aggregator.MONOTONIC_COUNT)
+    aggregator.assert_metric("calico.felix_iptables_save_errors.count", metric_type=aggregator.MONOTONIC_COUNT)
 
     aggregator.assert_all_metrics_covered()
     aggregator.assert_metrics_using_metadata(get_metadata_metrics())
