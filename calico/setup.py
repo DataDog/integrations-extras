2e9e9b777d6bb7fd1738f0ea8d02df454f38f3d9
MaxLmqr <47948264+MaxLmqr@users.noreply.github.com>
2021-09-01 10:38:27 +0200

    Update calico/setup.py
    Co-authored-by: Julia <611228+hithwen@users.noreply.github.com>

--- calico/setup.py
+++ calico/setup.py
@@ -48,7 +48,7 @@ setup(
         'Intended Audience :: System Administrators',
         'Topic :: System :: Monitoring',
         'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.8',
     ],
     # The package we're going to ship
