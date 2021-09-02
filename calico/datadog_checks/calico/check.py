746d91f312e9d274ab99a8fbf9d59dbbd370aa1c
MaxLmqr <max.lemasquerier@protonmail.com>
2021-09-02 10:25:49 +0200

    <contribution:calico> set default metrics + namespace. User no longer have to configure it in his agent config files.

--- calico/datadog_checks/calico/check.py
+++ calico/datadog_checks/calico/check.py
@@ -1,5 +1,7 @@
 from datadog_checks.base import OpenMetricsBaseCheckV2
 
+from .metrics import METRIC_MAP
+
 
 class CalicoCheck(OpenMetricsBaseCheckV2):
     def __init__(self, name, init_config, instances=None):
@@ -9,3 +11,6 @@ class CalicoCheck(OpenMetricsBaseCheckV2):
             init_config,
             instances,
         )
+
+    def get_default_config(self):
+        return {'namespace': 'calico', 'metrics': [METRIC_MAP]}
