ed76f357865e0be29c8107e8b6ba5e1f772ad583
Julien Michaud <julien.michaud.ext@adevinta.com>
2021-11-19 15:11:12 +0100

    trying to test every metrics

--- argo_cd/tests/conftest.py
+++ argo_cd/tests/conftest.py
@@ -8,15 +8,35 @@ import mock
 import pytest
 
 
-@pytest.fixture()
+# @pytest.fixture()
+# def mock_argo_cd():
+#     f_name = os.path.join(os.path.dirname(__file__), 'fixtures', 'metrics.txt')
+#     with open(f_name, 'r') as f:
+#         text_data = f.read()
+#     with mock.patch(
+#         'requests.get',
+#         return_value=mock.MagicMock(
+#             status_code=200, iter_lines=lambda **kwargs: text_data.split("\n"), headers={'Content-Type': "text/plain"}
+#         ),
+#     ):
+#         yield
+
+
+@pytest.fixture
 def mock_argo_cd():
     f_name = os.path.join(os.path.dirname(__file__), 'fixtures', 'metrics.txt')
     with open(f_name, 'r') as f:
         text_data = f.read()
+
     with mock.patch(
         'requests.get',
         return_value=mock.MagicMock(
-            status_code=200, iter_lines=lambda **kwargs: text_data.split("\n"), headers={'Content-Type': "text/plain"}
+            status_code=200, iter_lines=lambda **kwargs: text_data.split('\n'), headers={'Content-Type': 'text/plain'}
         ),
     ):
         yield
+
+
+@pytest.fixture
+def instance():
+    return {'openmetrics_endpoint': 'http://localhost:50000/metrics'}
--- argo_cd/tests/test_argo_cd.py
+++ argo_cd/tests/test_argo_cd.py
@@ -4,15 +4,42 @@
 
 
 import pytest
-
 from datadog_checks.argo_cd import ArgoCdCheck
+from datadog_checks.argo_cd.metrics import APPLICATION_METRICS
+CHECK_NAME = 'argocd'
 
-CHECK_NAME = 'argo_cd'
+# EXPECTED_VALUES = (
+#     ("argocd.app_info", 4,1),
+#     ("argocd.cluster_api_resource_objects",1,1077.0),
+#     ("argocd.cluster_api_resources",1,89.0),
+#     ("argocd.kubectl_exec_pending", 1,0.0),
+#     ("argocd.cluster_info", 1,1.0),
+#     ("argocd.cluster_cache_age_seconds", 1,16003.0),
+# )
+
+# @pytest.mark.integration
+# def test_check_all_metrics(aggregator, dd_run_check, mock_argo_cd):
+#     instance = {'openmetrics_endpoint': 'http://localhost:50000/metrics'}
+#     c = ArgoCdCheck(CHECK_NAME, {}, [instance])
+#     requests.get(mock_argo_cd)
+#     dd_run_check(c)
+#     # aggregator.assert_metric("argocd.app_info", count=6, value=1)
+#     # print(aggregator.assert_metric("argocd.app_info"))
+#     #aggregator.assert_metric("argocd.cluster_api_resource_objects", count=1, value=1077.0)
+#     for metric, count,value in EXPECTED_VALUES:
+#         aggregator.assert_metric(metric, count=count, value=value)
 
 
-@pytest.mark.integration
-def test_check_all_metrics(aggregator, dd_run_check, mock_argo_cd):
-    instance = {'openmetrics_endpoint': 'http://localhost:8082/metrics'}
+@pytest.mark.usefixtures('mock_argo_cd')
+def test_external_dns(aggregator, dd_run_check, instance):
+    """
+    Testing argo_cd
+    """
+
     c = ArgoCdCheck(CHECK_NAME, {}, [instance])
     dd_run_check(c)
-    aggregator.assert_metric("argocd.app_info", count=4, value=1)
+
+    for metric in APPLICATION_METRICS.values():
+        aggregator.assert_metric('argocd'+'.'+metric)
+
+    aggregator.assert_all_metrics_covered()
