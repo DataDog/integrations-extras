a6b7d4bbb295f4a9dd028a34f9b974b588c47ebf
Andrew Zhang <andrew.zhang@datadoghq.com>
2022-02-22 15:00:20 -0500

    Test CI

--- storm/datadog_checks/storm/storm.py
+++ storm/datadog_checks/storm/storm.py
@@ -1,6 +1,6 @@
 # (C) Datadog, Inc. 2010-2016
 # All rights reserved
-# Licensed under Simplified BSD License (see LICENSE)
+# Licensed under Simplified BSD License (see LICENSE) test
 import json
 
 import requests
