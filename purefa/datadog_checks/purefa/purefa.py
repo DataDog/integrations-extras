5ca566ebd6578cb48b99c2868372027bb2c4a3fb
Christopher Roberts <90332286+chrroberts-pure@users.noreply.github.com>
2022-03-01 16:43:51 -0500

    Update purefa.py

--- purefa/datadog_checks/purefa/purefa.py
+++ purefa/datadog_checks/purefa/purefa.py
@@ -7,7 +7,7 @@ class PureFACheck(OpenMetricsBaseCheckV2):
     __NAMESPACE__ = "purefa"
 
     DEFAULT_METRIC_LIMIT = 100000
-    
+
     def __init__(self, name, init_config, instances):
         super(PureFACheck, self).__init__(name, init_config, instances)
 
@@ -15,12 +15,5 @@ class PureFACheck(OpenMetricsBaseCheckV2):
     def get_default_config(self):
         return {
             'metrics': [METRIC_MAP],
-            'openmetrics_endpoint': 'http://localhost:9491/metrics/flasharray/array?endpoint=array01',
+            'openmetrics_endpoint': "http://localhost:9491/metrics/flasharray/array?endpoint=array01",
             }
-
-    def check(self, instance):
-        endpoint = instance.get('openmetrics_endpoint')
-        if endpoint is None:
-            raise ConfigurationError("Unable to find openmetrics_endpoint in config file.")
-
-        super().check(instance)
