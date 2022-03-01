1c10d91aba3b3b4ef262c93c055b5664d9810011
Christopher Roberts <90332286+chrroberts-pure@users.noreply.github.com>
2022-03-01 11:36:04 -0500

    Refactor purefa.py

--- purefa/datadog_checks/purefa/purefa.py
+++ purefa/datadog_checks/purefa/purefa.py
@@ -1,21 +1,20 @@
-from datadog_checks.base import ConfigurationError, OpenMetricsBaseCheckV2
 
+from datadog_checks.base import OpenMetricsBaseCheckV2
+from datadog_checks.base import ConfigurationError
 from .metrics import METRIC_MAP
-
 
 class PureFACheck(OpenMetricsBaseCheckV2):
     __NAMESPACE__ = "purefa"
 
     def __init__(self, name, init_config, instances):
         super(PureFACheck, self).__init__(name, init_config, instances)
-        self.check_initializations.appendleft(self._parse_config)
 
     def get_default_config(self):
         return {'metrics': [METRIC_MAP]}
 
-    def _parse_config(self):
-        self.scraper_configs = []
-        endpoint = self.instance.get('openmetrics_endpoint')
+    def check(self, instance):
+          endpoint = self.instance.get('openmetrics_endpoint')
+          if endpoint is None:
+              raise ConfigurationError("Unable to find openmetrics_endpoint in config file.")
 
-        if endpoint is None:
-            raise ConfigurationError("Unable to find openmetrics URL in config file.")
+          super().check(instance)
