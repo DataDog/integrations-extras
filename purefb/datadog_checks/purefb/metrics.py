eb51f0d2441d207dcd0bcff06024fe9d6a99728f
Christopher Roberts <chrroberts@purestorage.com>
2022-04-27 08:57:57 -0600

    Reformat metrics.py

--- purefb/datadog_checks/purefb/metrics.py
+++ purefb/datadog_checks/purefb/metrics.py
@@ -1,46 +1,130 @@
 METRIC_MAP = [
     {
-        'purefb_info': {'name': 'info'},
-        'purefb_hardware_health': {'name': 'hardware.health'},
-        'purefb_alerts_open': {'name': 'alerts.open'},
-        'purefb_array_performance_latency_usec': {'name': 'array.performance_latency_usec'},
-        'purefb_array_performance_throughput_iops': {'name': 'array.performance_throughput_iops'},
-        'purefb_array_performance_bandwidth_bytes': {'name': 'array.performance_bandwidth_bytes'},
-        'purefb_array_performance_average_bytes': {'name': 'array.performance_average_bytes'},
-        'purefb_array_http_specific_performance_latency_usec': {'name': 'array.http_specific_performance_latency_usec'},
-        'purefb_array_http_specific_performance_throughput_iops': {'name': 'array.http_specific_performance_throughput_iops'},
-        'purefb_array_nfs_latency_usec': {'name': 'array.nfs_latency_usec'},
-        'purefb_array_nfs_throughput_iops': {'name': 'array.nfs_throughput_iops'},
-        'purefb_array_s3_specific_performance_latency_usec': {'name': 'array.s3_specific_performance_latency_usec'},
-        'purefb_array_s3_specific_performance_throughput_iops': {'name': 'array.s3_specific_performance_throughput_iops'},
-        'purefb_array_space_data_reduction_ratio': {'name': 'array.space_data_reduction_ratio'},
-        'purefb_array_space_bytes': {'name': 'array.space_bytes'},
-        'purefb_array_space_parity': {'name': 'array.space_parity'},
-        'purefb_buckets_performance_latency_usec': {'name': 'buckets.performance_latency_usec'},
-        'purefb_buckets_performance_throughput_iops': {'name': 'buckets.performance_throughput_iops'},
-        'purefb_buckets_performance_bandwidth_bytes': {'name': 'buckets.performance_bandwidth_bytes'},
-        'purefb_buckets_performance_average_bytes': {'name': 'buckets.performance_average_bytes'},
-        'purefb_buckets_s3_specific_performance_latency_usec': {'name': 'buckets.s3_specific_performance_latency_usec'},
-        'purefb_buckets_s3_specific_performance_throughput_iops': {'name': 'buckets.s3_specific_performance_throughput_iops'},
-        'purefb_buckets_space_data_reduction_ratio': {'name': 'buckets.space_data_reduction_ratio'},
-        'purefb_buckets_space_bytes': {'name': 'buckets.space_bytes'},
-        'purefb_buckets_space_objects': {'name': 'buckets.space_objects'},
-        'purefb_file_systems_performance_latency_usec': {'name': 'file.systems_performance_latency_usec'},
-        'purefb_file_systems_performance_throughput_iops': {'name': 'file.systems_performance_throughput_iops'},
-        'purefb_file_systems_performance_bandwidth_bytes': {'name': 'file.systems_performance_bandwidth_bytes'},
-        'purefb_file_systems_performance_average_bytes': {'name': 'file.systems_performance_average_bytes'},
-        'purefb_file_systems_space_data_reduction_ratio': {'name': 'file.systems_space_data_reduction_ratio'},
-        'purefb_file_systems_space_bytes': {'name': 'file.systems_space_bytes'},
-        'purefb_bucket_replica_links_lag_msec': {'name': 'bucket.replica_links_lag_msec'},
-        'purefb_file_system_links_lag_msec': {'name': 'file.system_links_lag_msec'},
-        'purefb_hardware_connectors_performance_bandwidth_bytes': {'name': 'hardware.connectors_performance_bandwidth_bytes'},
-        'purefb_hardware_connectors_performance_throughput_pkts': {'name': 'hardware.connectors_performance_throughput_pkts'},
-        'purefb_hardware_connectors_performance_errors': {'name': 'hardware.connectors_performance_errors'},
-        'purefb_array_clients_performance_latency_usec': {'name': 'array.clients_performance_latency_usec'},
-        'purefb_array_clients_performance_throughput_iops': {'name': 'array.clients_performance_throughput_iops'},
-        'purefb_array_clients_performance_bandwidth_bytes': {'name': 'array.clients_performance_bandwidth_bytes'},
-        'purefb_array_clients_performance_avg_size_bytes': {'name': 'array.clients_performance_avg_size_bytes'},
-        'purefb_file_system_usage_users_bytes': {'name': 'file.system_usage_users_bytes'},
-        'purefb_file_system_usage_groups_bytes': {'name': 'file.system_usage_groups_bytes'},
+        'purefb_info': {
+            'name': 'info'
+        },
+        'purefb_hardware_health': {
+            'name': 'hardware.health'
+        },
+        'purefb_alerts_open': {
+            'name': 'alerts.open'
+        },
+        'purefb_array_performance_latency_usec': {
+            'name': 'array.performance_latency_usec'
+        },
+        'purefb_array_performance_throughput_iops': {
+            'name': 'array.performance_throughput_iops'
+        },
+        'purefb_array_performance_bandwidth_bytes': {
+            'name': 'array.performance_bandwidth_bytes'
+        },
+        'purefb_array_performance_average_bytes': {
+            'name': 'array.performance_average_bytes'
+        },
+        'purefb_array_http_specific_performance_latency_usec': {
+            'name': 'array.http_specific_performance_latency_usec'
+        },
+        'purefb_array_http_specific_performance_throughput_iops': {
+            'name': 'array.http_specific_performance_throughput_iops'
+        },
+        'purefb_array_nfs_latency_usec': {
+            'name': 'array.nfs_latency_usec'
+        },
+        'purefb_array_nfs_throughput_iops': {
+            'name': 'array.nfs_throughput_iops'
+        },
+        'purefb_array_s3_specific_performance_latency_usec': {
+            'name': 'array.s3_specific_performance_latency_usec'
+        },
+        'purefb_array_s3_specific_performance_throughput_iops': {
+            'name': 'array.s3_specific_performance_throughput_iops'
+        },
+        'purefb_array_space_data_reduction_ratio': {
+            'name': 'array.space_data_reduction_ratio'
+        },
+        'purefb_array_space_bytes': {
+            'name': 'array.space_bytes'
+        },
+        'purefb_array_space_parity': {
+            'name': 'array.space_parity'
+        },
+        'purefb_buckets_performance_latency_usec': {
+            'name': 'buckets.performance_latency_usec'
+        },
+        'purefb_buckets_performance_throughput_iops': {
+            'name': 'buckets.performance_throughput_iops'
+        },
+        'purefb_buckets_performance_bandwidth_bytes': {
+            'name': 'buckets.performance_bandwidth_bytes'
+        },
+        'purefb_buckets_performance_average_bytes': {
+            'name': 'buckets.performance_average_bytes'
+        },
+        'purefb_buckets_s3_specific_performance_latency_usec': {
+            'name': 'buckets.s3_specific_performance_latency_usec'
+        },
+        'purefb_buckets_s3_specific_performance_throughput_iops': {
+            'name': 'buckets.s3_specific_performance_throughput_iops'
+        },
+        'purefb_buckets_space_data_reduction_ratio': {
+            'name': 'buckets.space_data_reduction_ratio'
+        },
+        'purefb_buckets_space_bytes': {
+            'name': 'buckets.space_bytes'
+        },
+        'purefb_buckets_space_objects': {
+            'name': 'buckets.space_objects'
+        },
+        'purefb_file_systems_performance_latency_usec': {
+            'name': 'file.systems_performance_latency_usec'
+        },
+        'purefb_file_systems_performance_throughput_iops': {
+            'name': 'file.systems_performance_throughput_iops'
+        },
+        'purefb_file_systems_performance_bandwidth_bytes': {
+            'name': 'file.systems_performance_bandwidth_bytes'
+        },
+        'purefb_file_systems_performance_average_bytes': {
+            'name': 'file.systems_performance_average_bytes'
+        },
+        'purefb_file_systems_space_data_reduction_ratio': {
+            'name': 'file.systems_space_data_reduction_ratio'
+        },
+        'purefb_file_systems_space_bytes': {
+            'name': 'file.systems_space_bytes'
+        },
+        'purefb_bucket_replica_links_lag_msec': {
+            'name': 'bucket.replica_links_lag_msec'
+        },
+        'purefb_file_system_links_lag_msec': {
+            'name': 'file.system_links_lag_msec'
+        },
+        'purefb_hardware_connectors_performance_bandwidth_bytes': {
+            'name': 'hardware.connectors_performance_bandwidth_bytes'
+        },
+        'purefb_hardware_connectors_performance_throughput_pkts': {
+            'name': 'hardware.connectors_performance_throughput_pkts'
+        },
+        'purefb_hardware_connectors_performance_errors': {
+            'name': 'hardware.connectors_performance_errors'
+        },
+        'purefb_array_clients_performance_latency_usec': {
+            'name': 'array.clients_performance_latency_usec'
+        },
+        'purefb_array_clients_performance_throughput_iops': {
+            'name': 'array.clients_performance_throughput_iops'
+        },
+        'purefb_array_clients_performance_bandwidth_bytes': {
+            'name': 'array.clients_performance_bandwidth_bytes'
+        },
+        'purefb_array_clients_performance_avg_size_bytes': {
+            'name': 'array.clients_performance_avg_size_bytes'
+        },
+        'purefb_file_system_usage_users_bytes': {
+            'name': 'file.system_usage_users_bytes'
+        },
+        'purefb_file_system_usage_groups_bytes': {
+            'name': 'file.system_usage_groups_bytes'
+        },
     }
 ]
