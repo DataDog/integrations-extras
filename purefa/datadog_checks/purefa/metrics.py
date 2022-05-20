edfb52c9d3965d1d179a371620ef3ffcc7f7040a
Christopher Roberts <chrroberts@purestorage.com>
2022-05-20 18:46:42 -0400

    Update metrics.py

--- purefa/datadog_checks/purefa/metrics.py
+++ purefa/datadog_checks/purefa/metrics.py
@@ -7,60 +7,34 @@ METRIC_MAP = [
         "purefa_hardware_power_volts": {"name": "hardware.power_volts"},
         "purefa_alerts_total": {"name": "alerts.total"},
         "purefa_info": {"name": "info"},
-        "purefa_array_space_datareduction_ratio": {
-            "name": "array.space_datareduction_ratio"
-        },
+        "purefa_array_space_datareduction_ratio": {"name": "array.space_datareduction_ratio"},
         "purefa_array_space_capacity_bytes": {"name": "array.space_capacity_bytes"},
-        "purefa_array_space_provisioned_bytes": {
-            "name": "array.space_provisioned_bytes"
-        },
+        "purefa_array_space_provisioned_bytes": {"name": "array.space_provisioned_bytes"},
         "purefa_array_space_used_bytes": {"name": "array.space_used_bytes"},
-        "purefa_array_performance_latency_usec": {
-            "name": "array.performance_latency_usec"
-        },
-        "purefa_array_performance_bandwidth_bytes": {
-            "name": "array.performance_bandwidth_bytes"
-        },
+        "purefa_array_performance_latency_usec": {"name": "array.performance_latency_usec"},
+        "purefa_array_performance_bandwidth_bytes": {"name": "array.performance_bandwidth_bytes"},
         "purefa_array_performance_iops": {"name": "array.performance_iops"},
-        "purefa_array_performance_avg_block_bytes": {
-            "name": "array.performance_avg_block_bytes"
-        },
+        "purefa_array_performance_avg_block_bytes": {"name": "array.performance_avg_block_bytes"},
         "purefa_array_performance_qdepth": {"name": "array.performance_qdepth"},
-        "purefa_host_performance_bandwidth_bytes": {
-            "name": "host.performance_bandwidth_bytes"
-        },
+        "purefa_host_performance_bandwidth_bytes": {"name": "host.performance_bandwidth_bytes"},
         "purefa_host_performance_iops": {"name": "host.performance_iops"},
-        "purefa_host_performance_latency_usec": {
-            "name": "host.performance_latency_usec"
-        },
+        "purefa_host_performance_latency_usec": {"name": "host.performance_latency_usec"},
         "purefa_host_space_bytes": {"name": "host.space_bytes"},
-        "purefa_host_space_datareduction_ratio": {
-            "name": "host.space_datareduction_ratio"
-        },
+        "purefa_host_space_datareduction_ratio": {"name": "host.space_datareduction_ratio"},
         "purefa_host_space_size_bytes": {"name": "host.space_size_bytes"},
         "purefa_pod_mediator_status": {"name": "pod.mediator_status"},
-        "purefa_pod_performance_bandwidth_bytes": {
-            "name": "pod.performance_bandwidth_bytes"
-        },
+        "purefa_pod_performance_bandwidth_bytes": {"name": "pod.performance_bandwidth_bytes"},
         "purefa_pod_performance_iops": {"name": "pod.performance_iops"},
         "purefa_pod_performance_latency_usec": {"name": "pod.performance_latency_usec"},
         "purefa_pod_space_bytes": {"name": "pod.space_bytes"},
-        "purefa_pod_space_datareduction_ratio": {
-            "name": "pod.space_datareduction_ratio"
-        },
+        "purefa_pod_space_datareduction_ratio": {"name": "pod.space_datareduction_ratio"},
         "purefa_pod_space_size_bytes": {"name": "pod.space_size_bytes"},
         "purefa_pod_status": {"name": "pod.status"},
         "purefa_volume_performance_iops": {"name": "volume.performance_iops"},
-        "purefa_volume_performance_latency_usec": {
-            "name": "volume.performance_latency_usec"
-        },
-        "purefa_volume_performance_throughput_bytes": {
-            "name": "volume.performance_throughput_bytes"
-        },
+        "purefa_volume_performance_latency_usec": {"name": "volume.performance_latency_usec"},
+        "purefa_volume_performance_throughput_bytes": {"name": "volume.performance_throughput_bytes"},
         "purefa_volume_space_bytes": {"name": "volume.space_bytes"},
-        "purefa_volume_space_datareduction_ratio": {
-            "name": "volume.space_datareduction_ratio"
-        },
+        "purefa_volume_space_datareduction_ratio": {"name": "volume.space_datareduction_ratio"},
         "purefa_volume_space_size_bytes": {"name": "volume.space_size_bytes"},
     }
 ]
