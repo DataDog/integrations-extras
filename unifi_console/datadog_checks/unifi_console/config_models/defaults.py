e8c051b8e629137b1d5e3c1c3582e34ef4bdfd5b
antonin bruneau <antonin.bruneau@datadoghq.com>
2022-07-01 14:09:44 +0200

    remove empty_default_hostname and persist_connections requirements

--- unifi_console/datadog_checks/unifi_console/config_models/defaults.py
+++ unifi_console/datadog_checks/unifi_console/config_models/defaults.py
@@ -58,6 +58,10 @@ def instance_disable_generic_tags(field, value):
     return False
 
 
+def instance_empty_default_hostname(field, value):
+    return True
+
+
 def instance_extra_headers(field, value):
     return get_default_field_value(field, value)
 
@@ -112,6 +116,10 @@ def instance_ntlm_domain(field, value):
 
 def instance_password(field, value):
     return get_default_field_value(field, value)
+
+
+def instance_persist_connections(field, value):
+    return True
 
 
 def instance_proxy(field, value):
