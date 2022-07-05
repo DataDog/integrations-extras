9faed43f9713d8e5ce4b98326fa08903163da1d6
rajeevthefiddler <87085921+rajeevthefiddler@users.noreply.github.com>
2022-07-05 15:22:16 -0500

    Update check.py
    Updated update_check.py with the changes to support pulling metrics from Fiddler and pushing it into DD. Have not tested this yet though.

--- fiddler/datadog_checks/fiddler/check.py
+++ fiddler/datadog_checks/fiddler/check.py
@@ -7,6 +7,29 @@ from datadog_checks.base import AgentCheck
 # from requests.exceptions import ConnectionError, HTTPError, InvalidURL, Timeout
 # from json import JSONDecodeError
 
+# We will get one hours worth data in chunks and process them.
+bin_size = 3600
+
+metrics_list = ['accuracy', 'histogram_drift', 'output_average', 'feature_average', 'traffic_count']
+tags = list()
+
+# Connection parameters for Fiddler. Currently hard coded to get some testing done. Will be parameterizing this later. 
+URL = 'https://rajeev2271.test.fiddler.ai'
+ORG_ID = 'rajeev2271'
+AUTH_TOKEN = ''
+
+client = fdl.FiddlerApi(
+    url=URL,
+    org_id=ORG_ID,
+    auth_token=AUTH_TOKEN
+)
+
+def create_tags(**tags_in):
+   tags.clear()
+   tags.append("project:" + tags_in['project'])
+   tags.append("model:" + tags_in['model'])
+   if (len(tags_in) > 2):
+      tags.append("feature:" + tags_in['feature'])
 
 class FiddlerCheck(AgentCheck):
 
@@ -19,78 +42,92 @@ class FiddlerCheck(AgentCheck):
         # Use self.instance to read the check configuration
         # self.url = self.instance.get("url")
 
-        # If the check is going to perform SQL queries you should define a query manager here.
-        # More info at
-        # https://datadoghq.dev/integrations-core/base/databases/#datadog_checks.base.utils.db.core.QueryManager
-        # sample_query = {
-        #     "name": "sample",
-        #     "query": "SELECT * FROM sample_table",
-        #     "columns": [
-        #         {"name": "metric", "type": "gauge"}
-        #     ],
-        # }
-        # self._query_manager = QueryManager(self, self.execute_query, queries=[sample_query])
-        # self.check_initializations.append(self._query_manager.compile_queries)
-
     def check(self, _):
-        # type: (Any) -> None
-        # The following are useful bits of code to help new users get started.
+        # Iterate through the projects and the models and push data into Fiddler
+        try:
+            project_path = ['list_projects', ORG_ID]
+            result_all = client._call(project_path)
+            print("Projects: ", result_all["projects"])
+            
+            start_time = time.time() * 1000 - (bin_size * 1000)
+            end_time = time.time() * 1000
+            print("Start time is : ", start_time)
+            print("End time is : ", end_time)
 
-        # Perform HTTP Requests with our HTTP wrapper.
-        # More info at https://datadoghq.dev/integrations-core/base/http/
-        # try:
-        #     response = self.http.get(self.url)
-        #     response.raise_for_status()
-        #     response_json = response.json()
+            # Iterate through all of the projects within the Fiddler instance and get the metrics
+            for project in result_all["projects"]:
+               models = project["models"]
 
-        # except Timeout as e:
-        #     self.service_check(
-        #         "can_connect",
-        #         AgentCheck.CRITICAL,
-        #         message="Request timeout: {}, {}".format(self.url, e),
-        #     )
-        #     raise
+               # Iterate through all of the models within a project
+               for model in models:
+                  if (model["id"] != "random_forest"):
+                     print("Model: ", model["id"])
+                     continue
+                  else:
+                     print("Model: ", model["id"])
+                  for metric in metrics_list:
+                     json_request = {"metric":metric,"time_range_start":start_time,"time_range_end":end_time,"bin_size":bin_size, "prediction" : '_'}
+                     agg_metrics_path = ['aggregated_metrics', ORG_ID, project["name"], model["id"]]
+                     print("ProjectModel: ", project["name"], model["id"], metric)
+                     result = client._call(agg_metrics_path, json_request)
 
-        # except (HTTPError, InvalidURL, ConnectionError) as e:
-        #     self.service_check(
-        #         "can_connect",
-        #         AgentCheck.CRITICAL,
-        #         message="Request failed: {}, {}".format(self.url, e),
-        #     )
-        #     raise
+                     print("Agg_metrics_path: " , agg_metrics_path)
+                     print("json request: " , json_request, "\n")
+                     print("Result : ", result, "\n")
 
-        # except JSONDecodeError as e:
-        #     self.service_check(
-        #         "can_connect",
-        #         AgentCheck.CRITICAL,
-        #         message="JSON Parse failed: {}, {}".format(self.url, e),
-        #     )
-        #     raise
+                     # iterate through the json result for that specific metric
+                     for single_value in result["values"]:
+                        start_time = int(time.time())
+            
+                        # Every metric has a different way of providing the value. So handle them separetly. 
+                        if (metric == 'traffic_count'):
+                            value = single_value["value"]
+                            print("Final list: ", project["name"], model["id"], start_time, metric, value)
+                            create_tags(project=project["name"], model=model["id"])
+                            self.gauge (metric, value, tags)
 
-        # except ValueError as e:
-        #     self.service_check(
-        #         "can_connect", AgentCheck.CRITICAL, message=str(e)
-        #     )
-        #     raise
+                        elif (metric == 'output_average' or metric == 'integrity_violation_count'):
+                            for key, value in single_value["value"].items():
+                               new_metric = key
+                               value = value
+                               print("Final list: ", project["name"], model["id"], start_time, new_metric, value)
+                               create_tags(project=project["name"], model=model["id"])
+                               self.gauge (metric, value, tags)
 
-        # This is how you submit metrics
-        # There are different types of metrics that you can submit (gauge, event).
-        # More info at https://datadoghq.dev/integrations-core/base/api/#datadog_checks.base.checks.base.AgentCheck
-        # self.gauge("test", 1.23, tags=['foo:bar'])
+                        elif (metric == 'histogram_drift'):
+                            for key, value in single_value["value"].items():
+                               new_metric = "histogram_drift-" + key
+                               value = value
+                               print("Final list: ", project["name"], model["id"], start_time, new_metric, value)
+                               create_tags(project=project["name"], model=model["id"], feature=key)
+                               self.gauge (metric, value, tags)
 
-        # Perform database queries using the Query Manager
-        # self._query_manager.execute()
+                        elif (metric == 'feature_average'):
+                            for key, value in single_value["value"].items():
+                               new_metric = key
+                               value = value
+                               print("Final list: ", project["name"], model["id"], start_time, new_metric, value)
+                               create_tags(project=project["name"], model=model["id"], feature=key)
+                               self.gauge (metric, value, tags)
 
-        # This is how you use the persistent cache. This cache file based and persists across agent restarts.
-        # If you need an in-memory cache that is persisted across runs
-        # You can define a dictionary in the __init__ method.
-        # self.write_persistent_cache("key", "value")
-        # value = self.read_persistent_cache("key")
+                        elif (metric == 'accuracy'):
+                            accuracy_metrics = single_value["value"]
+                            for key, value in accuracy_metrics["accuracy_metrics"].items():
+                               new_metric = key
+                               value = value
+                               print("Final list: ", project["name"], model["id"], start_time, new_metric, value)
+                               create_tags(project=project["name"], model=model["id"])
+                               self.gauge (metric, value, tags)
+        except Timeout as e:
+             self.service_check(
+                 "can_connect",
+                 AgentCheck.CRITICAL,
+                 message="Request timeout: {}, {}".format(self.url, e),
+             )
+             raise
 
-        # If your check ran successfully, you can send the status.
+        # If the check ran successfully, we can send the status.
         # More info at
         # https://datadoghq.dev/integrations-core/base/api/#datadog_checks.base.checks.base.AgentCheck.service_check
-        # self.service_check("can_connect", AgentCheck.OK)
 
-        # If it didn't then it should send a critical service check
-        self.service_check("can_connect", AgentCheck.CRITICAL)
+        self.service_check("can_connect", AgentCheck.OK)
