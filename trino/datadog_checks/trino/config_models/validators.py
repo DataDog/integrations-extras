1045dc2cd19585b56086c7973e57016971f79db5
Florent Clarret <florent.clarret@datadoghq.com>
2022-07-07 09:44:58 +0200

    Remove Datadog licenses from config_models files

--- trino/datadog_checks/trino/config_models/validators.py
+++ trino/datadog_checks/trino/config_models/validators.py
@@ -1,7 +1,3 @@
-# (C) Datadog, Inc. 2022-present
-# All rights reserved
-# Licensed under a 3-clause BSD style license (see LICENSE)
-
 # Here you can include additional config validators or transformers
 #
 # def initialize_instance(values, **kwargs):
