aec21317cdc273cc53ad69e815c75ab1087d6a85
Nick Ripley <nick.ripley@datadoghq.com>
2022-11-04 09:18:15 -0400

    go_pprof_scraper: remove metrics checks from tests

--- go_pprof_scraper/tests/test_go_pprof_scraper.py
+++ go_pprof_scraper/tests/test_go_pprof_scraper.py
@@ -9,7 +9,6 @@ import requests
 
 from datadog_checks.base import AgentCheck, ConfigurationError
 from datadog_checks.base.stubs.aggregator import AggregatorStub
-from datadog_checks.dev.utils import get_metadata_metrics
 from datadog_checks.go_pprof_scraper import GoPprofScraperCheck
 
 INIT_CONFIG = {}
@@ -64,9 +63,6 @@ def test_check(dd_run_check, aggregator, instance):
     # type: (Callable[[AgentCheck, bool], None], AggregatorStub, Dict[str, Any]) -> None
     check = GoPprofScraperCheck("go_pprof_scraper", INIT_CONFIG, [instance])
     dd_run_check(check)
-
-    aggregator.assert_all_metrics_covered()
-    aggregator.assert_metrics_using_metadata(get_metadata_metrics())
 
 
 def test_emits_critical_service_check_when_service_is_down(dd_run_check, aggregator, instance):
