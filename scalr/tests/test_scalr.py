b25e410e923316fa4d55ce3e9966d298eecaabdc
Yurii Soltys <y.soltys@scalr.com>
2022-12-07 20:07:45 +0200

    Scalr datadog integration (#1605)
    * SCALRCORE-23530 - Create datadog agent integration
    
    * SCALRCORE-23530 - Create datadog agent integration
    
    * SCALRCORE-23530 - Create datadog agent integration
    
    * SCALRCORE-23530 - Create datadog agent integration
    
    * SCALRCORE-23530 - Create datadog agent integration
    
    * SCALRCORE-23530 - Create datadog agent integration
    
    * Changes
    
    * Update scalr/manifest.json
    
    Co-authored-by: Ryan <ryan.fee625@gmail.com>
    
    * Scalr integration ci fix
    
    * Scalr integration CODE OWNERS
    
    * Scalr integration CODE OWNERS
    
    * Scalr manifest
    
    * Scalr manifest updated tags
    
    * PR some suggested changes
    
    * PR readme.md suggested changes
    
    * Scalr description, images, dashboard
    
    * dashboard image
    
    * logos to svg
    
    * Update scalr/metadata.csv
    
    Co-authored-by: Florent Clarret <florent.clarret@gmail.com>
    
    * Update scalr/assets/service_checks.json
    
    Co-authored-by: Florent Clarret <florent.clarret@gmail.com>
    
    * Update scalr/setup.py
    
    Co-authored-by: Florent Clarret <florent.clarret@gmail.com>
    
    * PR check and check_tests updates
    
    * type fix
    
    * Update README.md
    
    Better description
    
    * Update scalr/README.md
    
    Co-authored-by: emarsha94 <52013262+emarsha94@users.noreply.github.com>
    
    * Update scalr/CHANGELOG.md
    
    Co-authored-by: emarsha94 <52013262+emarsha94@users.noreply.github.com>
    
    * Delete .gitkeep
    
    * Update scalr/README.md
    
    Co-authored-by: emarsha94 <52013262+emarsha94@users.noreply.github.com>
    
    * Update scalr/README.md
    
    Co-authored-by: emarsha94 <52013262+emarsha94@users.noreply.github.com>
    
    * Update scalr/README.md
    
    Co-authored-by: emarsha94 <52013262+emarsha94@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: emarsha94 <52013262+emarsha94@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: emarsha94 <52013262+emarsha94@users.noreply.github.com>
    
    Co-authored-by: Igor Savchenko <igor@scalr.com>
    Co-authored-by: Ryan <ryan.fee625@gmail.com>
    Co-authored-by: Edgar Mocharnyk <mocharnik@gmail.com>
    Co-authored-by: Florent Clarret <florent.clarret@gmail.com>
    Co-authored-by: emarsha94 <52013262+emarsha94@users.noreply.github.com>

---
+++ scalr/tests/test_scalr.py
@@ -0,0 +1,88 @@
+from typing import Any, Callable, Dict
+
+from datadog_checks.base import AgentCheck
+from datadog_checks.base.stubs.aggregator import AggregatorStub
+from datadog_checks.dev.utils import get_metadata_metrics
+from datadog_checks.scalr import ScalrCheck
+
+SCALR_ACCOUNT_METRICS = {
+    "environments.count",
+    "workspaces.count",
+    "runs.count",
+    "runs.successful",
+    "runs.failed",
+    "runs.awaiting_confirmation",
+    "runs.concurrency",
+    "runs.queue_size",
+    "quota.max_concurrency",
+    "billing.runs.count",
+    "billing.run_minutes.count",
+    "billing.flex_runs.count",
+    "billing.flex_run_minutes.count",
+}
+
+SCALR_ACCOUNTS_RESPONSE = '''
+{
+    "data": [
+        {
+          "attributes": {},
+          "id": "acc-test",
+          "relationships": {},
+          "type": "accounts"
+        }
+    ]
+}
+'''
+
+
+def test_check(dd_run_check, aggregator, instance, requests_mock):
+    # type: (Callable[[AgentCheck, bool], None], AggregatorStub, Dict[str, Any]) -> None
+
+    SCALR_METRICS_RESPONSE = '''
+    {
+      "billings-flex-runs-count": 0,
+      "billings-flex-runs-minutes-count": 0,
+      "billings-run-minutes-count": 50,
+      "billings-runs-count": 10,
+      "environments-count": 4,
+      "quota-max-concurrency": 10,
+      "runs-awaiting-confirmation": 1,
+      "runs-concurrency": 0,
+      "runs-count": 10,
+      "runs-failed": 2,
+      "runs-queue-size": 2,
+      "runs-successful": 6,
+      "workspaces-count": 3
+    }'''
+
+    requests_mock.get(
+        'http://acc_name.localhost/api/iacp/v3/accounts/acc-test/metrics',
+        text=SCALR_METRICS_RESPONSE,
+    )
+
+    requests_mock.get(
+        'http://acc_name.localhost/api/iacp/v3/accounts?filter[name]=acc_name',
+        text=SCALR_ACCOUNTS_RESPONSE,
+    )
+
+    check = ScalrCheck('scalr', {}, [instance])
+    dd_run_check(check)
+
+    for metric_name in SCALR_ACCOUNT_METRICS:
+        aggregator.assert_metric('scalr.' + metric_name)
+
+    aggregator.assert_all_metrics_covered()
+    aggregator.assert_metrics_using_metadata(get_metadata_metrics())
+    aggregator.assert_service_check('scalr.can_connect', ScalrCheck.OK)
+
+
+def test_emits_critical_service_check_when_service_is_down(dd_run_check, aggregator, instance, requests_mock):
+    # type: (Callable[[AgentCheck, bool], None], AggregatorStub, Dict[str, Any]) -> None
+    requests_mock.get(
+        'http://acc_name.localhost/api/iacp/v3/accounts?filter[name]=acc_name',
+        text=SCALR_ACCOUNTS_RESPONSE,
+    )
+    requests_mock.get('http://acc_name.localhost/api/iacp/v3/accounts/acc-test/metrics')
+    check = ScalrCheck('scalr', {}, [instance])
+    dd_run_check(check)
+    aggregator.assert_service_check('scalr.can_connect', ScalrCheck.CRITICAL)
