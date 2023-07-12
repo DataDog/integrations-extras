b3afc41cf71452bb9113560461179eab4e942fa8
Jun Mukai <86621109+junm-cloudnatix@users.noreply.github.com>
2023-07-12 12:55:59 -0700

    Update cloudnatix/datadog_checks/cloudnatix/cloudnatix.py
    Co-authored-by: Alex Lopez <alex.lopez.zorzano@gmail.com>

--- cloudnatix/datadog_checks/cloudnatix/cloudnatix.py
+++ cloudnatix/datadog_checks/cloudnatix/cloudnatix.py
@@ -12,28 +12,7 @@ class CloudNatixCheck(OpenMetricsBaseCheckV2, ConfigMixin):
 
     DEFAULT_METRIC_LIMIT = 0
 
-    def __init__(self, name, init_config, instances):
-        super().__init__(name, init_config, instances)
-
-        self.cached_metric_data = {}
-        self.check_initializations.append(self.configure_additional_transformers)
-
-    def configure_transformer(self, metric_name):
-        def custom_transformer(metric, sample_data, runtime_data):
-            if metric.name in self.cached_metric_data:
-                transformer = self.cached_metric_data[metric.name]
-            else:
-                transformer = NATIVE_TRANSFORMERS[metric.type](self, metric_name, {}, {})
-                self.cached_metric_data[metric.name] = transformer
-
-            transformer(metric, sample_data, runtime_data)
-
-        return custom_transformer
-
-    def configure_additional_transformers(self):
-        metric_transformer = self.scrapers[self.config.openmetrics_endpoint].metric_transformer
-
-        for raw_metric_name, metric_name in METRIC_MAP.items():
-            metric_transformer.add_custom_transformer(
-                f'.*?{raw_metric_name}$', self.configure_transformer(metric_name), pattern=True
-            )
+    def get_default_config(self):
+        return {
+            "metrics": [METRIC_MAP],
+        }
