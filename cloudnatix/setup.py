e4e76398250077e56aa05b9b244f0cc21a74b932
Florent Clarret <florent.clarret@datadoghq.com>
2023-10-23 15:48:34 +0200

    Bump the Python version from py3.9 to py3.11

--- cloudnatix/setup.py
+++ cloudnatix/setup.py
@@ -55,7 +55,7 @@ setup(
         'Intended Audience :: System Administrators',
         'Topic :: System :: Monitoring',
         'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.11',
     ],
     # The package we're going to ship
     packages=['datadog_checks.cloudnatix'],
