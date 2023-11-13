6a4223f3acba079566786c21f904b9e409e9dafd
Mayank Rajoria <mayank26596@gmail.com>
2023-11-13 13:09:04 -0800

    Add Robust Intelligence AI Firewall integration tile (#2122)
    * [ENT-2819] Robust Intelligence AI firewall integration initial commit (#1)
    
    * initial commit for check
    
    * check with correct metrics
    
    * add dashboard
    
    * update dashboard and readme
    
    * update dashboard title
    
    * fix readme links and add logos
    
    * fix support email link
    
    * update homepage and support email
    
    * [END-2821] Update README (#2)
    
    * update section on metrics collected
    
    * update firewall link and dashboard
    
    * [PINT-2462] readme fixes (#3)
    
    * readme fixes
    
    * update risk name
    
    * fix metrics section content
    
    * add image assets
    
    * [PINT-2485] PR build issues (#4)
    
    * lint fixes
    
    * add sales email
    
    * [PINT-2485] Address PR comments (#5)
    
    * update overview and description
    
    * add integrtaion version
    
    * add classifier tags
    
    * add dashboard description
    
    * add dashboard to manifest (#6)
    
    * [PINT-2541] readme review (#7)
    
    * add details to configure section
    
    * Add suggestion
    
    Co-authored-by: Arley Lewis <arley@robustintelligence.com>
    
    ---------
    
    Co-authored-by: Arley Lewis <arley@robustintelligence.com>
    
    * [PINT-2618] PR feedback and dashboard update (#8)
    
    * update readme and remove unnecessary file
    
    * upgrade dashboard
    
    * [PINT-2651] add dashboard banner image (#9)
    
    * add dashboard banner
    
    * update banner url
    
    * address PR comments (#10)
    
    * update description (#11)
    
    * [PINT-2719] readme and dashboard image (#12)
    
    * update dashboard image and description
    
    * update firewall image
    
    * update image ratio (#13)
    
    * [PINT-2891] add scope variable to dashboard (#14)
    
    * add scope variable
    
    * fix description
    
    * remove folder (#15)
    
    * fix import (#16)
    
    * run ddev validate ci
    
    * fix codecov
    
    * remove assets that are not required
    
    * add metrics check
    
    * add codeowners
    
    * fix format
    
    * remove non ascii character
    
    * fix test
    
    * add test
    
    ---------
    
    Co-authored-by: Arley Lewis <arley@robustintelligence.com>

---
+++ robust_intelligence_ai_firewall/tests/test_robust_intelligence_ai_firewall.py
@@ -0,0 +1,31 @@
+import os
+from typing import Any, Callable, Dict  # noqa: F401
+
+import pytest
+
+from datadog_checks.base import AgentCheck  # noqa: F401
+from datadog_checks.base.stubs.aggregator import AggregatorStub  # noqa: F401
+from datadog_checks.dev import get_here
+from datadog_checks.dev.utils import get_metadata_metrics
+from datadog_checks.robust_intelligence_ai_firewall import RobustIntelligenceAiFirewallCheck
+
+METRICS = [
+    "robust_intelligence_ai_firewall.firewall_requests.count",
+    "robust_intelligence_ai_firewall.rule_evaluated.count",
+]
+
+
+@pytest.mark.e2e
+def test_check(dd_run_check, aggregator, instance, mock_http_response):
+    fixtures_path = os.path.join(get_here(), 'fixtures', 'firewall_metrics.txt')
+    mock_http_response(file_path=fixtures_path)
+
+    # type: (Callable[[AgentCheck, bool], None], AggregatorStub, Dict[str, Any]) -> None
+    check = RobustIntelligenceAiFirewallCheck('robust_intelligence_ai_firewall', {}, [instance])
+    dd_run_check(check)
+
+    for metric in METRICS:
+        aggregator.assert_metric(metric)
+
+    aggregator.assert_all_metrics_covered()
+    aggregator.assert_metrics_using_metadata(get_metadata_metrics(), check_metric_type=False)
