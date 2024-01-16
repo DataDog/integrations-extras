4ff52ac803149aa57a303493f56f7a0fe6268750
Alex Harrison <alexander.harrison@pleo.io>
2024-01-16 14:16:58 +0000

    feat(HikariCP): Add support for prometheus-style metrics
    Add additional metric-mapping to account for metric name differences between Prometheus & Micrometer
    metricTrackerFactory.
    
    We also expand HikariCP testing to include these new Prometheus-style metrics

--- hikaricp/tests/conftest.py
+++ hikaricp/tests/conftest.py
@@ -39,3 +39,20 @@ def mock_micrometer_metrics():
         ),
     ):
         yield
+
+@pytest.fixture()
+def mock_prometheus_metrics():
+    fixture_file = os.path.join(os.path.dirname(__file__), "fixtures", "metrics-prometheus.txt")
+
+    with open(fixture_file, "r") as f:
+        content = f.read()
+
+    with mock.patch(
+            "requests.get",
+            return_value=mock.MagicMock(
+                status_code=200,
+                iter_lines=lambda **kwargs: content.split("\n"),
+                headers={"Content-Type": "text/plain"},
+            ),
+    ):
+        yield
--- hikaricp/tests/test_hikaricp.py
+++ hikaricp/tests/test_hikaricp.py
@@ -21,11 +21,40 @@ EXPECTED_MICROMETER_METRICS = {
     "hikaricp.connections.usage.seconds.sum",
 }
 
+EXPECTED_PROMETHEUS_METRICS = {
+    "hikaricp.connections",         # "hikaricp_connections",
+    "hikaricp.connections.active",  # "hikaricp_active_connections"
+    "hikaricp.connections.idle",    # "hikaricp_idle_connections",
+    "hikaricp.connections.max",     # "hikaricp_max_connections",
+    "hikaricp.connections.min",     # "hikaricp_min_connections",
+    "hikaricp.threads.pending",     # "hikaricp_pending_threads",
+    "hikaricp.connections.timeout.count",   # "hikaricp_connection_timeout.count",
+     "hikaricp.connections.acquired.nanos.count",        # "hikaricp_connection_acquired_nanos.count",
+     "hikaricp.connections.acquired.nanos.sum",          # "hikaricp_connection_acquired_nanos.sum",
+     "hikaricp.connections.acquired.nanos.quantile",     # "hikaricp_connection_acquired_nanos.quantile",
+    "hikaricp.connections.creation.millis.count",       # "hikaricp_connection_creation_millis.count",
+    "hikaricp.connections.creation.millis.sum",         # "hikaricp_connection_creation_millis.sum",
+    "hikaricp.connections.creation.millis.quantile",    # "hikaricp_connection_creation_millis.quantile",
+    "hikaricp.connections.usage.millis.count",      # "hikaricp_connection_usage_millis.count",
+    "hikaricp.connections.usage.millis.sum",        # "hikaricp_connection_usage_millis.sum",
+    "hikaricp.connections.usage.millis.quantile",   # "hikaricp_connection_usage_millis.quantile",
+}
 
 @pytest.mark.unit
 def test_mock_assert_micrometer_metrics(dd_run_check, aggregator, check, mock_micrometer_metrics):
     dd_run_check(check)
     for metric_name in EXPECTED_MICROMETER_METRICS:
+        aggregator.assert_metric(metric_name)
+    aggregator.assert_all_metrics_covered()
+    aggregator.assert_metrics_using_metadata(get_metadata_metrics())
+    aggregator.assert_service_check("hikaricp.openmetrics.health", status=HikaricpCheck.OK)
+
+
+
+@pytest.mark.unit
+def test_mock_assert_prometheus_metrics(dd_run_check, aggregator, check, mock_prometheus_metrics):
+    dd_run_check(check)
+    for metric_name in EXPECTED_PROMETHEUS_METRICS:
         aggregator.assert_metric(metric_name)
     aggregator.assert_all_metrics_covered()
     aggregator.assert_metrics_using_metadata(get_metadata_metrics())
