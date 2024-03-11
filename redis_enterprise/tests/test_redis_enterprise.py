bc4ee91b6f3cd7089db3a932b5703c17b9344fb4
John Burke <john.burke@redis.com>
2024-03-11 09:47:55 -0400

    addressed comments, fixed lint errors

--- redis_enterprise/tests/test_redis_enterprise.py
+++ redis_enterprise/tests/test_redis_enterprise.py
@@ -11,8 +11,8 @@ from datadog_checks.redis_enterprise import RedisEnterpriseCheck
 @pytest.mark.unit
 def test_check(dd_run_check, aggregator, instance):
     # type: (Callable[[AgentCheck, bool], None], AggregatorStub, Dict[str, Any]) -> None
-    check = RedisEnterpriseCheck('redis_enterprise', {}, [{'openmetrics_endpoint': 'https://localhost:8070',
-                                                            'tls_verify': 'false'}])
+    check = RedisEnterpriseCheck(
+        'redis_enterprise', {}, [{'openmetrics_endpoint': 'https://localhost:8070', 'tls_verify': 'false'}])
     dd_run_check(check)
 
     aggregator.assert_all_metrics_covered()
@@ -22,8 +22,8 @@ def test_check(dd_run_check, aggregator, instance):
 @pytest.mark.unit
 def test_emits_critical_service_check_when_service_is_down(dd_run_check, aggregator, instance):
     # type: (Callable[[AgentCheck, bool], None], AggregatorStub, Dict[str, Any]) -> None
-    check = RedisEnterpriseCheck('redis_enterprise', {}, [{'openmetrics_endpoint': 'https://localhost:8071',
-                                                            'tls_verify': 'false'}])
+    check = RedisEnterpriseCheck(
+        'redis_enterprise', {}, [{'openmetrics_endpoint': 'https://localhost:8071', 'tls_verify': 'false'}])
 
     dd_run_check(check)
     aggregator.assert_service_check('redis_enterprise.can_connect', RedisEnterpriseCheck.CRITICAL)
