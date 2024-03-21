855831ca9ba3ec07da74cc75af56ecda332a1b03
John Burke <john.burke@redis.com>
2024-03-21 15:47:08 -0400

    fix lint errors on metrics

--- redis_enterprise/datadog_checks/redis_enterprise/metrics.py
+++ redis_enterprise/datadog_checks/redis_enterprise/metrics.py
@@ -124,236 +124,62 @@ METRIC_MAP = {
     'node_provisional_memory_no_overbooking': 'node_provisional_memory_no_overbooking',
     'node_total_req': 'node_total_req',
     'node_up': 'node_up',
-    'redis_active_defrag_running': {
-        'name': 'redis_active_defrag_running',
-        'type': 'rate'
-    },
-    'redis_allocator_active': {
-        'name': 'redis_allocator_active',
-        'type': 'gauge'
-    },
-    'redis_allocator_allocated': {
-        'name': 'redis_allocator_allocated',
-        'type': 'gauge'
-    },
-    'redis_allocator_resident': {
-        'name': 'redis_allocator_resident',
-        'type': 'gauge'
-    },
-    'redis_aof_last_cow_size': {
-        'name': 'redis_aof_last_cow_size',
-        'type': 'gauge'
-    },
-    'redis_aof_rewrite_in_progress': {
-        'name': 'redis_aof_rewrite_in_progress',
-        'type': 'gauge'
-    },
-    'redis_aof_rewrites': {
-        'name': 'redis_aof_rewrites',
-        'type': 'gauge'
-    },
-    'redis_aof_delayed_fsync': {
-        'name': 'redis_aof_rewrites',
-        'type': 'gauge'
-    },
-    'redis_blocked_clients': {
-        'name': 'redis_blocked_clients',
-        'type': 'gauge'
-    },
-    'redis_blocking_reads': {
-        'name': 'redis_blocking_reads',
-        'type': 'gauge'
-    },
-    'redis_blocking_writes': {
-        'name': 'redis_blocking_writes',
-        'type': 'gauge'
-    },
-    'redis_connected_clients': {
-        'name': 'redis_connected_clients',
-        'type': 'gauge'
-    },
-    'redis_connected_slaves': {
-        'name': 'redis_connected_slaves',
-        'type': 'gauge'
-    },
-    'redis_db0_avg_ttl': {
-        'name': 'redis_db0_avg_ttl',
-        'type': 'rate'
-    },
-    'redis_db0_expires': {
-        'name': 'redis_db0_expires',
-        'type': 'gauge'
-    },
-    'redis_db0_keys': {
-        'name': 'redis_db0_keys',
-        'type': 'gauge'
-    },
-    'redis_evicted_keys': {
-        'name': 'redis_evicted_keys',
-        'type': 'gauge'
-    },
-    'redis_expire_cycle_cpu_milliseconds': {
-        'name': 'redis_expire_cycle_cpu_milliseconds',
-        'type': 'gauge'
-    },
-    'redis_expired_keys': {
-        'name': 'redis_expired_keys',
-        'type': 'gauge'
-    },
-    'redis_forwarding_state': {
-        'name': 'redis_forwarding_state',
-        'type': 'gauge'
-    },
-    'redis_keys_trimmed': {
-        'name': 'redis_keys_trimmed',
-        'type': 'gauge'
-    },
-    'redis_keyspace_read_hits': {
-        'name': 'redis_keyspace_read_hits',
-        'type': 'gauge'
-    },
-    'redis_keyspace_read_misses': {
-        'name': 'redis_keyspace_read_misses',
-        'type': 'gauge'
-    },
-    'redis_keyspace_write_hits': {
-        'name': 'redis_keyspace_write_hits',
-        'type': 'gauge'
-    },
-    'redis_keyspace_write_misses': {
-        'name': 'redis_keyspace_write_misses',
-        'type': 'gauge'
-    },
-    'redis_master_link_status': {
-        'name': 'redis_master_link_status',
-        'type': 'gauge'
-    },
-    'redis_master_repl_offset': {
-        'name': 'redis_master_repl_offset',
-        'type': 'gauge'
-    },
-    'redis_master_sync_in_progress': {
-        'name': 'redis_master_sync_in_progress',
-        'type': 'gauge'
-    },
-    'redis_max_process_mem': {
-        'name': 'redis_max_process_mem',
-        'type': 'gauge'
-    },
-    'redis_maxmemory': {
-        'name': 'redis_maxmemory',
-        'type': 'gauge'
-    },
-    'redis_mem_aof_buffer': {
-        'name': 'redis_mem_aof_buffer',
-        'type': 'gauge'
-    },
-    'redis_mem_clients_normal': {
-        'name': 'redis_mem_clients_normal',
-        'type': 'gauge'
-    },
-    'redis_mem_clients_slaves': {
-        'name': 'redis_mem_clients_slaves',
-        'type': 'gauge'
-    },
-    'redis_mem_fragmentation_ratio': {
-        'name': 'redis_mem_fragmentation_ratio',
-        'type': 'histogram'
-    },
-    'redis_mem_not_counted_for_evict': {
-        'name': 'redis_mem_not_counted_for_evict',
-        'type': 'gauge'
-    },
-    'redis_mem_replication_backlog': {
-        'name': 'redis_mem_replication_backlog',
-        'type': 'gauge'
-    },
-    'redis_module_fork_in_progress': {
-        'name': 'redis_module_fork_in_progress',
-        'type': 'gauge'
-    },
-    'redis_process_cpu_system_seconds_total': {
-        'name': 'redis_process_cpu_system_seconds_total',
-        'type': 'gauge'
-    },
-    'redis_process_cpu_usage_percent': {
-        'name': 'redis_process_cpu_usage_percent',
-        'type': 'gauge'
-    },
-    'redis_process_cpu_user_seconds_total': {
-        'name': 'redis_process_cpu_user_seconds_total',
-        'type': 'gauge'
-    },
-    'redis_process_main_thread_cpu_system_seconds_total': {
-        'name': 'redis_process_main_thread_cpu_system_seconds_total',
-        'type': 'gauge'
-    },
-    'redis_process_main_thread_cpu_user_seconds_total': {
-        'name': 'redis_process_main_thread_cpu_user_seconds_total',
-        'type': 'gauge'
-    },
-    'redis_process_max_fds': {
-        'name': 'redis_process_max_fds',
-        'type': 'gauge'
-    },
-    'redis_process_open_fds': {
-        'name': 'redis_process_open_fds',
-        'type': 'gauge'
-    },
-    'redis_process_resident_memory_bytes': {
-        'name': 'redis_process_resident_memory_bytes',
-        'type': 'gauge'
-    },
-    'redis_process_start_time_seconds': {
-        'name': 'redis_process_start_time_seconds',
-        'type': 'gauge'
-    },
-    'redis_process_virtual_memory_bytes': {
-        'name': 'redis_process_virtual_memory_bytes',
-        'type': 'gauge'
-    },
-    'redis_rdb_bgsave_in_progress': {
-        'name': 'redis_rdb_bgsave_in_progress',
-        'type': 'gauge'
-    },
-    'redis_rdb_last_cow_size': {
-        'name': 'redis_rdb_last_cow_size',
-        'type': 'gauge'
-    },
-    'redis_rdb_saves': {
-        'name': 'redis_rdb_saves',
-        'type': 'gauge'
-    },
-    'redis_repl_touch_bytes': {
-        'name': 'redis_repl_touch_bytes',
-        'type': 'gauge'
-    },
-    'redis_total_commands_processed': {
-        'name': 'redis_total_commands_processed',
-        'type': 'gauge'
-    },
-    'redis_total_connections_received': {
-        'name': 'redis_total_connections_received',
-        'type': 'gauge'
-    },
-    'redis_total_net_input_bytes': {
-        'name': 'redis_total_net_input_bytes',
-        'type': 'gauge'
-    },
-    'redis_total_net_output_bytes': {
-        'name': 'redis_total_net_output_bytes',
-        'type': 'gauge'
-    },
-    'redis_up': {
-        'name': 'redis_up',
-        'type': 'gauge'
-    },
-    'redis_used_disk': {
-        'name': 'redis_used_disk',
-        'type': 'gauge'
-    },
-    'redis_used_memory': {
-        'name': 'redis_used_memory',
-        'type': 'gauge'
-    },
+    'redis_active_defrag_running': {'name': 'redis_active_defrag_running', 'type': 'rate'},
+    'redis_allocator_active': {'name': 'redis_allocator_active', 'type': 'gauge'},
+    'redis_allocator_allocated': {'name': 'redis_allocator_allocated', 'type': 'gauge'},
+    'redis_allocator_resident': {'name': 'redis_allocator_resident', 'type': 'gauge'},
+    'redis_aof_last_cow_size': {'name': 'redis_aof_last_cow_size', 'type': 'gauge'},
+    'redis_aof_rewrite_in_progress': {'name': 'redis_aof_rewrite_in_progress', 'type': 'gauge'},
+    'redis_aof_rewrites': {'name': 'redis_aof_rewrites', 'type': 'gauge'},
+    'redis_aof_delayed_fsync': {'name': 'redis_aof_rewrites', 'type': 'gauge'},
+    'redis_blocked_clients': {'name': 'redis_blocked_clients', 'type': 'gauge'},
+    'redis_blocking_reads': {'name': 'redis_blocking_reads', 'type': 'gauge'},
+    'redis_blocking_writes': {'name': 'redis_blocking_writes', 'type': 'gauge'},
+    'redis_connected_clients': {'name': 'redis_connected_clients', 'type': 'gauge'},
+    'redis_connected_slaves': {'name': 'redis_connected_slaves', 'type': 'gauge'},
+    'redis_db0_avg_ttl': {'name': 'redis_db0_avg_ttl', 'type': 'rate'},
+    'redis_db0_expires': {'name': 'redis_db0_expires', 'type': 'gauge'},
+    'redis_db0_keys': {'name': 'redis_db0_keys', 'type': 'gauge'},
+    'redis_evicted_keys': {'name': 'redis_evicted_keys', 'type': 'gauge'},
+    'redis_expire_cycle_cpu_milliseconds': {'name': 'redis_expire_cycle_cpu_milliseconds', 'type': 'gauge'},
+    'redis_expired_keys': {'name': 'redis_expired_keys', 'type': 'gauge'},
+    'redis_forwarding_state': {'name': 'redis_forwarding_state', 'type': 'gauge'},
+    'redis_keys_trimmed': {'name': 'redis_keys_trimmed', 'type': 'gauge'},
+    'redis_keyspace_read_hits': {'name': 'redis_keyspace_read_hits', 'type': 'gauge'},
+    'redis_keyspace_read_misses': {'name': 'redis_keyspace_read_misses', 'type': 'gauge'},
+    'redis_keyspace_write_hits': {'name': 'redis_keyspace_write_hits', 'type': 'gauge'},
+    'redis_keyspace_write_misses': {'name': 'redis_keyspace_write_misses', 'type': 'gauge'},
+    'redis_master_link_status': {'name': 'redis_master_link_status', 'type': 'gauge'},
+    'redis_master_repl_offset': {'name': 'redis_master_repl_offset', 'type': 'gauge'},
+    'redis_master_sync_in_progress': {'name': 'redis_master_sync_in_progress', 'type': 'gauge'},
+    'redis_max_process_mem': {'name': 'redis_max_process_mem', 'type': 'gauge'},
+    'redis_maxmemory': {'name': 'redis_maxmemory', 'type': 'gauge'},
+    'redis_mem_aof_buffer': {'name': 'redis_mem_aof_buffer', 'type': 'gauge'},
+    'redis_mem_clients_normal': {'name': 'redis_mem_clients_normal', 'type': 'gauge'},
+    'redis_mem_clients_slaves': {'name': 'redis_mem_clients_slaves', 'type': 'gauge'},
+    'redis_mem_fragmentation_ratio': {'name': 'redis_mem_fragmentation_ratio', 'type': 'histogram'},
+    'redis_mem_not_counted_for_evict': {'name': 'redis_mem_not_counted_for_evict', 'type': 'gauge'},
+    'redis_mem_replication_backlog': {'name': 'redis_mem_replication_backlog', 'type': 'gauge'},
+    'redis_module_fork_in_progress': {'name': 'redis_module_fork_in_progress', 'type': 'gauge'},
+    'redis_process_cpu_system_seconds_total': {'name': 'redis_process_cpu_system_seconds_total', 'type': 'gauge'},
+    'redis_process_cpu_usage_percent': {'name': 'redis_process_cpu_usage_percent', 'type': 'gauge'},
+    'redis_process_cpu_user_seconds_total': {'name': 'redis_process_cpu_user_seconds_total', 'type': 'gauge'},
+    'redis_process_main_thread_cpu_system_seconds_total': {'name': 'redis_process_main_thread_cpu_system_seconds_total', 'type': 'gauge'},
+    'redis_process_main_thread_cpu_user_seconds_total': {'name': 'redis_process_main_thread_cpu_user_seconds_total', 'type': 'gauge'},
+    'redis_process_max_fds': {'name': 'redis_process_max_fds', 'type': 'gauge'},
+    'redis_process_open_fds': {'name': 'redis_process_open_fds', 'type': 'gauge'},
+    'redis_process_resident_memory_bytes': {'name': 'redis_process_resident_memory_bytes', 'type': 'gauge'},
+    'redis_process_start_time_seconds': {'name': 'redis_process_start_time_seconds', 'type': 'gauge'},
+    'redis_process_virtual_memory_bytes': {'name': 'redis_process_virtual_memory_bytes', 'type': 'gauge'},
+    'redis_rdb_bgsave_in_progress': {'name': 'redis_rdb_bgsave_in_progress', 'type': 'gauge'},
+    'redis_rdb_last_cow_size': {'name': 'redis_rdb_last_cow_size', 'type': 'gauge'},
+    'redis_rdb_saves': {'name': 'redis_rdb_saves', 'type': 'gauge'},
+    'redis_repl_touch_bytes': {'name': 'redis_repl_touch_bytes', 'type': 'gauge'},
+    'redis_total_commands_processed': {'name': 'redis_total_commands_processed', 'type': 'gauge'},
+    'redis_total_connections_received': {'name': 'redis_total_connections_received', 'type': 'gauge'},
+    'redis_total_net_input_bytes': {'name': 'redis_total_net_input_bytes', 'type': 'gauge'},
+    'redis_total_net_output_bytes': {'name': 'redis_total_net_output_bytes', 'type': 'gauge'},
+    'redis_up': {'name': 'redis_up', 'type': 'gauge'},
+    'redis_used_disk': {'name': 'redis_used_disk', 'type': 'gauge'},
+    'redis_used_memory': {'name': 'redis_used_memory', 'type': 'gauge'},
 }
