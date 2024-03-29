aa74a3674b8f1d0c087d6d6fc54e55f6d7ca1031
david fauth <dsfauth@gmail.com>
2024-03-29 17:01:05 +0000

    Validated models

--- neo4j/datadog_checks/neo4j/config_models/instance.py
+++ neo4j/datadog_checks/neo4j/config_models/instance.py
@@ -28,7 +28,7 @@ class AuthToken(BaseModel):
 class ExtraMetric(BaseModel):
     model_config = ConfigDict(
         arbitrary_types_allowed=True,
-        extra="allow",
+        extra='allow',
         frozen=True,
     )
     name: Optional[str] = None
@@ -47,7 +47,7 @@ class MetricPatterns(BaseModel):
 class Metric(BaseModel):
     model_config = ConfigDict(
         arbitrary_types_allowed=True,
-        extra="allow",
+        extra='allow',
         frozen=True,
     )
     name: Optional[str] = None
@@ -116,7 +116,7 @@ class InstanceConfig(BaseModel):
     metric_patterns: Optional[MetricPatterns] = None
     metrics: Optional[tuple[Union[str, MappingProxyType[str, Union[str, Metric]]], ...]] = None
     min_collection_interval: Optional[float] = None
-    namespace: Optional[str] = Field(None, pattern="\\w*")
+    namespace: Optional[str] = Field(None, pattern='\\w*')
     neo4j_dbs: Optional[tuple[str, ...]] = None
     neo4j_version: Optional[str] = None
     non_cumulative_histogram_buckets: Optional[bool] = None
@@ -149,21 +149,21 @@ class InstanceConfig(BaseModel):
     use_process_start_time: Optional[bool] = None
     username: Optional[str] = None
 
-    @model_validator(mode="before")
+    @model_validator(mode='before')
     def _initial_validation(cls, values):
-        return validation.core.initialize_config(getattr(validators, "initialize_instance", identity)(values))
+        return validation.core.initialize_config(getattr(validators, 'initialize_instance', identity)(values))
 
-    @field_validator("*", mode="before")
+    @field_validator('*', mode='before')
     def _validate(cls, value, info):
         field = cls.model_fields[info.field_name]
         field_name = field.alias or info.field_name
-        if field_name in info.context["configured_fields"]:
-            value = getattr(validators, f"instance_{info.field_name}", identity)(value, field=field)
+        if field_name in info.context['configured_fields']:
+            value = getattr(validators, f'instance_{info.field_name}', identity)(value, field=field)
         else:
-            value = getattr(defaults, f"instance_{info.field_name}", lambda: value)()
+            value = getattr(defaults, f'instance_{info.field_name}', lambda: value)()
 
         return validation.utils.make_immutable(value)
 
-    @model_validator(mode="after")
+    @model_validator(mode='after')
     def _final_validation(cls, model):
-        return validation.core.check_model(getattr(validators, "check_instance", identity)(model))
+        return validation.core.check_model(getattr(validators, 'check_instance', identity)(model))
