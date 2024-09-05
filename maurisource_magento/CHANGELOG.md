0e1f5215d66f55a410dc1286f8e2e47c0380e321
Isaac Johnson <40847491+ICEJ-jr@users.noreply.github.com>
2024-09-05 17:26:51 +0000

    feat: initial release

---
+++ maurisource_magento/CHANGELOG.md
@@ -0,0 +1,8 @@
+# CHANGELOG - maurisource_magento
+
+## 1.0.0 / YYYY-MM-DD
+
+***Added***:
+
+* Initial Release
+
---
+++ maurisource_magento/datadog_checks/maurisource_magento/__init__.py
@@ -0,0 +1,5 @@
+
+from .__about__ import __version__
+from .check import MaurisourceMagentoCheck
+
+__all__ = ['__version__', 'MaurisourceMagentoCheck']
