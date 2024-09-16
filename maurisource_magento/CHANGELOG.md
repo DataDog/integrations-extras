43492fb0e79b09544d17f29d85ef7fddfb86cae1
Isaac Johnson <40847491+ICEJ-jr@users.noreply.github.com>
2024-09-16 15:00:16 +0000

    Revert "feat: initial release"
    This reverts commit 0e1f5215d66f55a410dc1286f8e2e47c0380e321.

--- maurisource_magento/CHANGELOG.md
+++
@@ -1,8 +0,0 @@
-# CHANGELOG - maurisource_magento
-
-## 1.0.0 / YYYY-MM-DD
-
-***Added***:
-
-* Initial Release
-
--- maurisource_magento/datadog_checks/maurisource_magento/__init__.py
+++
@@ -1,5 +0,0 @@
-
-from .__about__ import __version__
-from .check import MaurisourceMagentoCheck
-
-__all__ = ['__version__', 'MaurisourceMagentoCheck']
