ae033755e7e85b84315b69f76708bb869cf39125
Kyle-Neale <kyle.neale@datadoghq.com>
2024-10-15 16:43:33 -0400

    Remove whitespace

--- upbound_uxp/setup.py
+++ upbound_uxp/setup.py
@@ -63,7 +63,6 @@ setup(
         'Intended Audience :: System Administrators',
         'Topic :: System :: Monitoring',
         'License :: OSI Approved :: BSD License',
-
         'Programming Language :: Python :: 3.12',
     ],
     # The package we're going to ship
