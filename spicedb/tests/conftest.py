6ae324c8cdde3ee5c8525654375aa3d00d509a8c
Tanner Stirrat <tstirrat@gmail.com>
2024-12-02 12:53:33 -0700

    Start fleshing out conftest

--- spicedb/tests/conftest.py
+++ spicedb/tests/conftest.py
@@ -1,12 +1,27 @@
-
+import os
 import pytest
 
+from datadog_checks.dev import docker_run, get_here
+
+from .util import HOST, PORT
 
 @pytest.fixture(scope='session')
-def dd_environment():
-    yield
+def dd_environment(instance):
+    env_vars = {"SPICEDB_GRPC_PRESHARED_KEY": "some random key"}
+
+    with docker_run(
+            os.path.join(get_here(), "docker", "docker-compose.yml"),
+            env_vars=env_vars,
+            endpoints=instance['openmetrics_endpoint'],
+            conditions=None,
+            ):
+        yield instance
 
 
 @pytest.fixture
-def instance():
-    return {}
+def instance(scope='session'):
+    return {
+        'openmetrics_endpoint': 'http://{}:{}/metrics'.format(HOST, PORT),
+        'histogram_buckets_as_distributions': True,
+        'tags': ['cluster:cockroachdb-cluster', 'node:1'],
+    }
