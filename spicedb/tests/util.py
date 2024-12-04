8cd73f59b1e90efec15bf1903177543ea957241e
Tanner Stirrat <tstirrat@gmail.com>
2024-12-04 11:09:24 -0700

    Fix port, split up histogram and non-histogram metrics

--- spicedb/tests/util.py
+++ spicedb/tests/util.py
@@ -2,19 +2,21 @@ import os
 from itertools import chain
 
 from datadog_checks.dev import get_docker_hostname, get_here
-from datadog_checks.spicedb.metrics import COUNTER_METRICS, OTHER_METRICS
+from datadog_checks.spicedb.metrics import COUNTER_METRICS, GAUGE_METRICS, HISTOGRAM_METRICS
 
 HOST = get_docker_hostname()
-PORT = "8080"
+PORT = "9090"
 
-def get_expected_metrics():
+def get_expected_non_histogram_metrics():
     return list(chain(
             # We add a .count suffix because datadog is going to append it before it's sent.
             (f"{datadog_name}.count" for datadog_name in COUNTER_METRICS.values()),
             # We pass through the other metrics straight.
-            OTHER_METRICS.values(),
+            GAUGE_METRICS.values(),
 ))
 
+def get_expected_histogram_metrics():
+    return HISTOGRAM_METRICS.values()
 
 def get_fixture_path(filename):
     return os.path.join(get_here(), "fixtures", filename)
