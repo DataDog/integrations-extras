f49eed66e29afbf72fc1423b8f85852a9a405d06
Tanner Stirrat <tstirrat@gmail.com>
2024-12-04 11:10:43 -0700

    Add integration test

--- spicedb/tests/test_integration.py
+++ spicedb/tests/test_integration.py
@@ -4,6 +4,8 @@ from datadog_checks.base.constants import ServiceCheck
 from datadog_checks.spicedb import SpicedbCheck
 from datadog_checks.dev.utils import assert_service_checks, get_metadata_metrics
 
+from .util import get_expected_non_histogram_metrics, get_expected_histogram_metrics
+
 # NOTE: this is the same as annotating all of the test functions in this file with the dd_environment fixture.
 pytestmark = [pytest.mark.usefixtures("dd_environment")]
 
@@ -12,9 +14,10 @@ def test_metrics(aggregator, instance, dd_run_check):
     check = SpicedbCheck("spicedb", {}, [instance])
     dd_run_check(check)
 
-    expected_metrics = []
+    for metric in get_expected_non_histogram_metrics():
+        aggregator.assert_metric(f"spicedb.{metric}", at_least=0)
 
-    for metric in expected_metrics:
+    for metric in get_expected_histogram_metrics():
         aggregator.assert_metric(f"spicedb.{metric}", at_least=0)
 
     aggregator.assert_service_check("spicedb.openmetrics.health", ServiceCheck.OK)
