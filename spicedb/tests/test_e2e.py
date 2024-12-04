3d555ab26a59051f8ca290bae041ff12411255e0
Tanner Stirrat <tstirrat@gmail.com>
2024-12-04 11:10:55 -0700

    Add e2e test

--- spicedb/tests/test_e2e.py
+++ spicedb/tests/test_e2e.py
@@ -1,6 +1,10 @@
+from .util import get_expected_non_histogram_metrics, get_expected_histogram_metrics
+
 def test_metrics(dd_agent_check, instance):
     aggregator = dd_agent_check(instance, rate=True)
-    expected_metrics = []
 
-    for metric in expected_metrics:
+    for metric in get_expected_non_histogram_metrics():
+        aggregator.assert_metric(f"spicedb.{metric}", at_least=0)
+
+    for metric in get_expected_histogram_metrics():
         aggregator.assert_metric(f"spicedb.{metric}", at_least=0)
