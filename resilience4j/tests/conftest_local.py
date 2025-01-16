6524032d80fb2fd8c5a9bb9e448fbd6c28355bb1
Willian Cesar Cincerre Da Silva <3329-willian.cincerre@users.noreply.code.ifoodcorp.com.br>
2025-01-16 11:58:41 -0300

    fix: checks | docs: included tests and assets

--- resilience4j/tests/conftest_local.py
+++ resilience4j/tests/conftest_local.py
@@ -1,35 +1,44 @@
 import os
+from unittest import mock
 
-import mock
 import pytest
 
-from datadog_checks.dev import docker_run, get_here
 from datadog_checks.resilience4j.check import Resilience4jCheck
 
-INSTANCE_URL="http://localhost:9080/actuator/prometheus"
-HERE = get_here()
-DOCKER_DIR = os.path.join(HERE, 'docker')
 
-
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def dd_environment():
-    compose_file = os.path.join(DOCKER_DIR, 'docker-compose.yaml')
-
-    with docker_run(compose_file, log_patterns=[r'Successfully started Resilience4j!']):
-        instances = {'instances': [{'prometheus_url': INSTANCE_URL}]}
-        yield instances
+    yield
 
 
 @pytest.fixture
 def instance():
     return {
-        "prometheus_url": INSTANCE_URL,
+        "prometheus_url": "http://localhost:3000/metrics",
     }
 
 
 @pytest.fixture
 def check(instance):
     return Resilience4jCheck("resilience4j", {}, [instance])
+
+
+# @pytest.fixture()
+# def mock_micrometer_metrics():
+#     fixture_file = os.path.join(os.path.dirname(__file__), "fixtures", "metrics-micrometer.txt")
+
+#     with open(fixture_file, "r") as f:
+#         content = f.read()
+
+#     with mock.patch(
+#         "requests.get",
+#         return_value=mock.MagicMock(
+#             status_code=200,
+#             iter_lines=lambda **kwargs: content.split("\n"),
+#             headers={"Content-Type": "text/plain"},
+#         ),
+#     ):
+#         yield
 
 
 @pytest.fixture()
--- resilience4j/tests/test_resilience4j.py
+++ resilience4j/tests/test_resilience4j.py
@@ -3,27 +3,6 @@ import pytest
 from datadog_checks.dev.utils import get_metadata_metrics
 from datadog_checks.resilience4j import Resilience4jCheck
 
-# EXPECTED_MICROMETER_METRICS = {
-#     "resilience4j.bulkhead.available.concurrent.calls",
-#     "resilience4j.bulkhead.max.allowed.concurrent.calls",
-#     "resilience4j.bulkhead.max.thread.pool.size",
-#     "resilience4j.bulkhead.queue.capacity",
-#     "resilience4j.bulkhead.queue.depth",
-#     "resilience4j.bulkhead.thread.pool.size",
-#     "resilience4j.circuitbreaker.buffered.calls",
-#     "resilience4j.circuitbreaker.calls.seconds.count",
-#     "resilience4j.circuitbreaker.calls.seconds.max",
-#     "resilience4j.circuitbreaker.calls.seconds.sum",
-#     # "resilience4j.circuitbreaker.calls",
-#     "resilience4j.circuitbreaker.failure.rate",
-#     "resilience4j.circuitbreaker.max.buffered.calls",
-#     "resilience4j.circuitbreaker.state",
-#     "resilience4j.ratelimiter.available.permissions",
-#     "resilience4j.ratelimiter.waiting.threads",
-#     "resilience4j.retry.calls",
-#     "resilience4j.timelimiter.calls.count",
-# }
-
 EXPECTED_PROMETHEUS_METRICS = {
     "resilience4j.bulkhead.available.concurrent.calls",
     "resilience4j.bulkhead.max.allowed.concurrent.calls",
@@ -45,22 +24,11 @@ EXPECTED_PROMETHEUS_METRICS = {
     "resilience4j.timelimiter.calls",
 }
 
-
-# @pytest.mark.unit
-# def test_mock_assert_micrometer_metrics(dd_run_check, aggregator, check, mock_micrometer_metrics):
-#     dd_run_check(check)
-#     for metric_name in EXPECTED_MICROMETER_METRICS:
-#         aggregator.assert_metric(metric_name)
-#     aggregator.assert_all_metrics_covered()
-#     aggregator.assert_metrics_using_metadata(get_metadata_metrics())
-#     aggregator.assert_service_check("resilience4j.openmetrics.health", status=Resilience4jCheck.OK)
-
-
-@pytest.mark.unit
-def test_mock_assert_prometheus_metrics(dd_run_check, aggregator, check, mock_prometheus_metrics):
+@pytest.mark.integration
+def test_check(dd_run_check, aggregator, check):
     dd_run_check(check)
     for metric_name in EXPECTED_PROMETHEUS_METRICS:
-        aggregator.assert_metric(metric_name)
+        aggregator.assert_metric(metric_name, at_least=0)
     aggregator.assert_all_metrics_covered()
     aggregator.assert_metrics_using_metadata(get_metadata_metrics())
-    # aggregator.assert_service_check("resilience4j.openmetrics.health", status=Resilience4jCheck.OK)
+    aggregator.assert_service_check("resilience4j.prometheus.health", status=Resilience4jCheck.OK)
