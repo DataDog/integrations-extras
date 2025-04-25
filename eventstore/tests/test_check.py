045dbe9f7711f6c60a5e10c246afa3e7b4dd2f29
steveny91 <steven.yuen@datadoghq.com>
2025-04-25 11:37:23 -0400

    change test assertion

--- eventstore/tests/test_check.py
+++ eventstore/tests/test_check.py
@@ -61,6 +61,7 @@ def test_integration(aggregator, instance):
 
     for endpoint in instance['endpoints']:
         for metric in init_config['metric_definitions'][endpoint]:
-            aggregator.assert_metric(metric['metric_name'], tags=['test:tag'])
+            aggregator.assert_metric(metric['metric_name'], tags=[])
+            aggregator.assert_metric_has_tag(metric['metric_name'], 'test:tag')
 
     aggregator.assert_all_metrics_covered()
