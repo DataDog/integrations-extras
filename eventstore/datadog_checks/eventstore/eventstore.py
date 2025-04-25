731c7f2cd3e490726950b8835a3f2c2064a531bb
steveny91 <steven.yuen@datadoghq.com>
2025-04-25 11:30:01 -0400

    small change and validation sync

--- eventstore/datadog_checks/eventstore/eventstore.py
+++ eventstore/datadog_checks/eventstore/eventstore.py
@@ -320,13 +320,13 @@ class EventStoreCheck(AgentCheck):
     def dispatch_metric(self, value, metric):
         """Formats the metric into the correct type with relevant tags"""
         metric_type = metric['metric_type']
-        tags = metric['tag_by']
+        tags = metric['tag_by'] + self.instance_tags
         metric_name = metric['metric_name']
         if metric_type == 'gauge':
             self.log.debug("Sending gauge %s v: %s t: %s", metric_name, value, tags)
-            self.gauge(metric_name, value, tags+self.instance_tags)
+            self.gauge(metric_name, value, tags)
         elif metric_type == 'histogram':
             self.log.debug("Sending histogram %s v: %s t: %s", metric_name, value, tags)
-            self.histogram(metric_name, value, tags+self.instance_tags)
+            self.histogram(metric_name, value, tags)
         else:
             self.log.info('Unable to send metric %s due to invalid metric type of %s', metric_name, metric_type)
