66904f0597c565e669c83e3c25e3b2199cd70d24
mphanias <pmokrala@aerospike.com>
2025-06-15 23:22:06 +0530

    aerospike-agent-and-dashboard

--- aerospike_enterprise/tests/test_unit.py
+++ aerospike_enterprise/tests/test_unit.py
@@ -10,7 +10,6 @@ from .common import (
 )
 
 
-@pytest.mark.unit
 def test_invalid_instance(aggregator, dd_run_check, mock_http_response):
     instance = deepcopy(OPENMETRICS_V2_INSTANCE)
     instance.pop("openmetrics_endpoint")
@@ -21,3 +20,17 @@ def test_invalid_instance(aggregator, dd_run_check, mock_http_response):
         dd_run_check(check)
 
     aggregator.assert_service_check(f"{AerospikeEnterpriseCheck.__NAMESPACE__}.node.up", count=0)
+
+
+@pytest.mark.integration
+def test_metrics_warning(dd_run_check, instance_openmetrics_v2):
+    instance_openmetrics_v2['metrics'] = ['migrate_rx_objs', 'migrate_tx_objs']
+    check = AerospikeEnterpriseCheck(AerospikeEnterpriseCheck.__NAMESPACE__, {}, [instance_openmetrics_v2])
+
+    with pytest.raises(Exception):
+        dd_run_check(check)
+        
+@pytest.mark.e2e
+def test_e2e(aggregator, dd_run_check):
+    print("\t test_e2e -- aggregator", aggregator)
+    assert True
