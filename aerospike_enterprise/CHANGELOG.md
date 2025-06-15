14d86d23cb11509af9ad109041616c43a5ee4135
mphanias <pmokrala@aerospike.com>
2025-06-15 19:40:14 +0530

    aerospike-agent-and-dashboards
    initial checkin

---
+++ aerospike_enterprise/CHANGELOG.md
@@ -0,0 +1,8 @@
+# CHANGELOG - aerospike_enterprise
+
+## 1.0.0 / YYYY-MM-DD
+
+***Added***:
+
+* Initial Release
+
---
+++ aerospike_enterprise/tests/conftest.py
@@ -0,0 +1,28 @@
+# (C) Datadog, Inc. 2019-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+
+import os
+from copy import deepcopy
+
+import pytest
+
+from datadog_checks.dev.docker import CheckDockerLogs, docker_run
+
+from .common import COMPOSE_FILE, HERE, OPENMETRICS_V2_INSTANCE
+
+
+@pytest.fixture(scope="session")
+def dd_environment():
+    with docker_run(
+        COMPOSE_FILE,
+        conditions=[CheckDockerLogs(COMPOSE_FILE, ["service ready: soon there will be cake!"])],
+        env_vars={"AEROSPIKE_CONFIG": os.path.join(HERE, "docker/config")},
+        attempts=2,
+    ):
+        yield OPENMETRICS_V2_INSTANCE
+
+
+@pytest.fixture
+def instance_openmetrics_v2():
+    return deepcopy(OPENMETRICS_V2_INSTANCE)
