b7d54ce9e6a6dd14b151cbf96bfca3161deb02fe
mphanias <pmokrala@aerospike.com>
2025-06-15 22:34:21 +0530

    aerospike-agent-and-dashboards
    corrected test-cases

--- aerospike_enterprise/tests/test_unit.py
+++ aerospike_enterprise/tests/test_unit.py
@@ -4,41 +4,20 @@ import pytest
 
 from datadog_checks.aerospike_enterprise import AerospikeEnterpriseCheck
 from datadog_checks.base import AgentCheck  # noqa: F401
-from datadog_checks.dev.utils import get_metadata_metrics
 
 from .common import (
-    ALL_METRICS,
     OPENMETRICS_V2_INSTANCE,
 )
-
-
-@pytest.mark.usefixtures('dd_environment')
-@pytest.mark.e2e
-def test_openmetrics_e2e(dd_agent_check, instance_openmetrics_v2):
-    aggregator = dd_agent_check(instance_openmetrics_v2, rate=True)
-
-    tags = "endpoint:" + instance_openmetrics_v2.get('openmetrics_endpoint')
-    tags = instance_openmetrics_v2.get('tags').append(tags)
-
-    aggregator.assert_service_check('aerospike.openmetrics.health', AgentCheck.OK, tags=tags)
-
-    for metric in ALL_METRICS:
-        aggregator.assert_metric(metric, tags=tags)
-
-    aggregator.assert_all_metrics_covered()
-    aggregator.assert_metrics_using_metadata(get_metadata_metrics(), check_submission_type=True)
 
 
 @pytest.mark.unit
 def test_invalid_instance(aggregator, dd_run_check, mock_http_response):
     instance = deepcopy(OPENMETRICS_V2_INSTANCE)
-    instance.pop('openmetrics_endpoint')
+    instance.pop("openmetrics_endpoint")
 
     check = AerospikeEnterpriseCheck(AerospikeEnterpriseCheck.__NAMESPACE__, {}, [instance])
 
-    try:
+    with pytest.raises(Exception):
         dd_run_check(check)
-    except Exception:
-        assert True
 
-    aggregator.assert_service_check(f'{AerospikeEnterpriseCheck.__NAMESPACE__}.node.up', count=0)
+    aggregator.assert_service_check(f"{AerospikeEnterpriseCheck.__NAMESPACE__}.node.up", count=0)
