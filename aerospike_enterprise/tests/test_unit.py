711d9a86855913d8b01b5dbc4f9c99b8ef65a381
mphanias <pmokrala@aerospike.com>
2025-06-16 20:24:38 +0530

    aerospike-agent-and-dashboard
    added e2e test and removed unnecessary integration tests
    modified service_checks

--- aerospike_enterprise/tests/test_unit.py
+++ aerospike_enterprise/tests/test_unit.py
@@ -20,18 +20,3 @@ def test_invalid_instance(aggregator, dd_run_check, mock_http_response):
         dd_run_check(check)
 
     aggregator.assert_service_check(f"{AerospikeEnterpriseCheck.__NAMESPACE__}.node.up", count=0)
-
-
-@pytest.mark.integration
-def test_metrics_warning(dd_run_check, instance_openmetrics_v2):
-    instance_openmetrics_v2["metrics"] = ["migrate_rx_objs", "migrate_tx_objs"]
-    check = AerospikeEnterpriseCheck(AerospikeEnterpriseCheck.__NAMESPACE__, {}, [instance_openmetrics_v2])
-
-    with pytest.raises(Exception):
-        dd_run_check(check)
-
-
-@pytest.mark.e2e
-def test_e2e(aggregator, dd_run_check):
-    print("\t test_e2e -- aggregator", aggregator)
-    assert True
