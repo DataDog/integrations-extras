ffbe324f9cba7c471a9ba03952a29c341894a4ac
slorello89 <steve.lorello@redis.com>
2025-07-09 07:55:00 -0400

    fixing linting issues (probably will break validator)

--- redis_enterprise_prometheus/datadog_checks/redis_enterprise_prometheus/config_models/instance.py
+++ redis_enterprise_prometheus/datadog_checks/redis_enterprise_prometheus/config_models/instance.py
@@ -37,21 +37,21 @@ class InstanceConfig(BaseModel):
     service: Optional[str] = None
     tags: Optional[tuple[str, ...]] = None
 
-    @model_validator(mode='before')
+    @model_validator(mode="before")
     def _initial_validation(cls, values):
-        return validation.core.initialize_config(getattr(validators, 'initialize_instance', identity)(values))
+        return validation.core.initialize_config(getattr(validators, "initialize_instance", identity)(values))
 
-    @field_validator('*', mode='before')
+    @field_validator("*", mode="before")
     def _validate(cls, value, info):
         field = cls.model_fields[info.field_name]
         field_name = field.alias or info.field_name
-        if field_name in info.context['configured_fields']:
-            value = getattr(validators, f'instance_{info.field_name}', identity)(value, field=field)
+        if field_name in info.context["configured_fields"]:
+            value = getattr(validators, f"instance_{info.field_name}", identity)(value, field=field)
         else:
-            value = getattr(defaults, f'instance_{info.field_name}', lambda: value)()
+            value = getattr(defaults, f"instance_{info.field_name}", lambda: value)()
 
         return validation.utils.make_immutable(value)
 
-    @model_validator(mode='after')
+    @model_validator(mode="after")
     def _final_validation(cls, model):
-        return validation.core.check_model(getattr(validators, 'check_instance', identity)(model))
+        return validation.core.check_model(getattr(validators, "check_instance", identity)(model))
