fb555571778580030c395fe39a5fb7094a31b27e
Len-Maurice Seemann <len-maurice.seemann@stonebranch>
2025-07-23 10:03:28 +0200

    used ddev create -t check_only stonebranch command

--- stonebranch/tests/conftest.py
+++ stonebranch/tests/conftest.py
@@ -6,10 +6,7 @@ import pytest
 def dd_environment():
     yield
 
+
 @pytest.fixture
 def instance():
-    return {
-        'url': 'http://localhost:8080/uc',
-        'username': 'test_user',
-        'password': 'test_password'
-    }
+    return {}
