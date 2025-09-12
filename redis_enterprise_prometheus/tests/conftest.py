c1f5d96c6da907a253c1407c8f85c64a0b28c630
slorello89 <steve.lorello@redis.com>
2025-09-12 14:39:34 -0400

    maybe because its a different request?

--- redis_enterprise_prometheus/tests/conftest.py
+++ redis_enterprise_prometheus/tests/conftest.py
@@ -25,12 +25,16 @@ def mock_http_response():
     f_name = os.path.join(os.path.dirname(__file__), "data", "metrics.txt")
     with open(f_name, "r") as f:
         text_data = f.read()
-    with mock.patch(
-        "requests.get",
-        return_value=mock.MagicMock(
-            status_code=200,
-            iter_lines=lambda **kwargs: text_data.split("\n"),
-            headers={"Content-Type": "text/plain"},
-        ),
+
+    mock_response = mock.MagicMock()
+    mock_response.status_code = 200
+    mock_response.content = text_data
+    mock_response.text = text_data
+    mock_response.iter_lines = lambda **kwargs: text_data.split("\n")
+    mock_response.headers = {"Content-Type": "text/plain"}
+
+    with (
+        mock.patch("requests.get", return_value=mock_response),
+        mock.patch("requests.Session.get", return_value=mock_response),
     ):
         yield
