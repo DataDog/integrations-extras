642c98bf70fd0ca13ebef845f96903cd92865bb6
reuben <reubentjl@gmail.com>
2025-11-04 08:29:45 +0800

    remove testing code

--- grafana/tests/test_e2e.py
+++ grafana/tests/test_e2e.py
@@ -12,15 +12,3 @@ def test_metrics(dd_agent_check):
     aggregator.assert_service_check('grafana.openmetrics.health', ServiceCheck.OK, at_least=1)
 
     aggregator.assert_metrics_using_metadata(get_metadata_metrics())
-
-
-# def test_metrics_with_extra(dd_agent_check, instance_with_extra_metrics):
-#     aggregator = dd_agent_check(instance_with_extra_metrics)
-#     aggregator.assert_metric('grafana.alerting.scheduler_behind_seconds')
-#     aggregator.assert_metric('grafana.alerting.alertmanager_alerts')
-#     aggregator.assert_metric('grafana.database.conn_idle')
-#     aggregator.assert_service_check('grafana.openmetrics.health', ServiceCheck.OK, count=1)
-#     assert_service_checks(aggregator)
-#     for m in METRICS:
-#         aggregator.assert_metric(m)
-#     aggregator.assert_all_metrics_covered()
--- grafana/tests/test_unit.py
+++ grafana/tests/test_unit.py
@@ -24,26 +24,3 @@ def test_emits_critical_service_check_when_service_is_down(dd_run_check, aggrega
     with pytest.raises(Exception, match="requests.exceptions.HTTPError"):
         dd_run_check(check)
     aggregator.assert_service_check('grafana.openmetrics.health', GrafanaCheck.CRITICAL)
-
-
-# EXPECTED_METRICS = ["alerting_scheduler_behind_seconds"]
-# EXTRA_METRICS = ["alerting_alertmanager_alerts", "database_conn_idle"]
-#
-#
-# def test_check_with_extra_metrics(dd_run_check, aggregator, instance_with_extra_metrics, mock_http_response):
-#     # Given
-#     mock_http_response(file_path=Path(__file__).parent.absolute() / "fixtures" / "grafana_metrics.txt")
-#     check = GrafanaCheck('grafana', {}, [instance_with_extra_metrics])
-#     # When
-#     dd_run_check(check)
-#     # Then
-#     for m in EXPECTED_METRICS + EXTRA_METRICS:
-#         aggregator.assert_metric('grafana.' + m)
-
-#     # for sm in EXPECTED_SUMMARIES:
-#     #     aggregator.assert_metric('quarkus.' + sm + '.count')
-#     #     aggregator.assert_metric('quarkus.' + sm + '.sum')
-
-#     # completed_extra_metric_names = ["grafana." + name for name in EXTRA_METRICS]
-#     aggregator.assert_metrics_using_metadata(get_metadata_metrics())
-#     aggregator.assert_all_metrics_covered()
