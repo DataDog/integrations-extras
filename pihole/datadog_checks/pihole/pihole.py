69636a6fa8088438758e8ced3d24ccf2011b0548
Aidan Mongan <aidan.mongan@datadoghq.com>
2025-11-27 17:45:17 +0000

    Added requested changes

--- pihole/datadog_checks/pihole/pihole.py
+++ pihole/datadog_checks/pihole/pihole.py
@@ -1,14 +1,87 @@
 import json
 
 from datadog_checks.base import AgentCheck, ConfigurationError
-
+V6_METRIC_NAMES = [
+                            # Clients
+                            'pihole.clients.active',
+                            'pihole.clients.total',
+                            # Gravity
+                            'pihole.gravity.domains_being_blocked',
+                            'pihole.gravity.last_update',
+                            # Summary
+                            'pihole.queries.blocked',
+                            'pihole.queries.cached',
+                            'pihole.queries.forwarded',
+                            'pihole.queries.frequency',
+                            'pihole.queries.percent_blocked',
+                            'pihole.queries.total',
+                            'pihole.queries.unique_domains',
+                            'pihole.took',
+                            # Replies
+                            'pihole.queries.replies.BLOB',
+                            'pihole.queries.replies.CNAME',
+                            'pihole.queries.replies.DNSSEC',
+                            'pihole.queries.replies.DOMAIN',
+                            'pihole.queries.replies.IP',
+                            'pihole.queries.replies.NODATA',
+                            'pihole.queries.replies.NONE',
+                            'pihole.queries.replies.NOTIMP',
+                            'pihole.queries.replies.NXDOMAIN',
+                            'pihole.queries.replies.OTHER',
+                            'pihole.queries.replies.REFUSED',
+                            'pihole.queries.replies.RRNAME',
+                            'pihole.queries.replies.SERVFAIL',
+                            'pihole.queries.replies.UNKNOWN',
+                            # Status
+                            'pihole.queries.status.CACHE',
+                            'pihole.queries.status.CACHE_STALE',
+                            'pihole.queries.status.DBBUSY',
+                            'pihole.queries.status.DENYLIST',
+                            'pihole.queries.status.DENYLIST_CNAME',
+                            'pihole.queries.status.EXTERNAL_BLOCKED_EDE15',
+                            'pihole.queries.status.EXTERNAL_BLOCKED_IP',
+                            'pihole.queries.status.EXTERNAL_BLOCKED_NULL',
+                            'pihole.queries.status.EXTERNAL_BLOCKED_NXRA',
+                            'pihole.queries.status.FORWARDED',
+                            'pihole.queries.status.GRAVITY',
+                            'pihole.queries.status.GRAVITY_CNAME',
+                            'pihole.queries.status.IN_PROGRESS',
+                            'pihole.queries.status.REGEX',
+                            'pihole.queries.status.REGEX_CNAME',
+                            'pihole.queries.status.RETRIED',
+                            'pihole.queries.status.RETRIED_DNSSEC',
+                            'pihole.queries.status.SPECIAL_DOMAIN',
+                            'pihole.queries.status.UNKNOWN',
+                            # Types
+                            'pihole.queries.types.A',
+                            'pihole.queries.types.AAAA',
+                            'pihole.queries.types.ANY',
+                            'pihole.queries.types.DNSKEY',
+                            'pihole.queries.types.DS',
+                            'pihole.queries.types.HTTPS',
+                            'pihole.queries.types.MX',
+                            'pihole.queries.types.NAPTR',
+                            'pihole.queries.types.NS',
+                            'pihole.queries.types.OTHER',
+                            'pihole.queries.types.PTR',
+                            'pihole.queries.types.RRSIG',
+                            'pihole.queries.types.SOA',
+                            'pihole.queries.types.SRV',
+                            'pihole.queries.types.SVCB',
+                            'pihole.queries.types.TXT',
+                            ]
 
 class PiholeCheck(AgentCheck):
     def __init__(self, name, init_config, instances):
         super(PiholeCheck, self).__init__(name, init_config, instances)
-        host = self.instance.get('host')
-        if not host:  # Check if a host parameter exists in conf.yaml
+        self.host = self.instance.get('host')
+        if not self.host:  # Check if a host parameter exists in conf.yaml
             raise ConfigurationError('Error, please fix pihole.d/conf.yaml, host parameter is required')
+        self.web_password = self.instance.get('web_password')
+        self.v5_pihole = self.instance.get('legacy_check')
+        self.custom_tags = self.instance.get("tags", [])
+        self.custom_tags.append("target_host:{}".format(self.host))
+            
 
     def _collect_response(self, url):
         response = self.http.get(url)
@@ -121,14 +194,20 @@ class PiholeCheck(AgentCheck):
             else:
                 items.append((new_key, v))
         return dict(items)
+    
+    # Helper function to submit a single metric safely, verifying it is present in the collected output
+    def submit_metrics(self, metric_name, metrics, custom_tags):
+        value = metrics.get(metric_name)
+        if value is not None:
+            self.gauge(metric_name, value, custom_tags)
 
     # Collect all metrics retunred from the /stats/summary endpoint and submit
     def _collect_v6_metrics(self, host, headers, custom_tags):
         statsUrl = 'http://' + host + '/api/stats/summary'
         stats_resp = self.http.get(statsUrl, headers=headers)
-        flattened = self._flatten_dict(stats_resp.json())
-        for m, v in flattened.items():
-            self.gauge(m, v, custom_tags)
+        metrics = self._flatten_dict(stats_resp.json())
+        for metric_name in V6_METRIC_NAMES:
+            self.submit_metrics(metric_name, metrics, custom_tags)
         return stats_resp
 
     # End session by submitting a delete request with our created SID
@@ -161,14 +240,13 @@ class PiholeCheck(AgentCheck):
                 )
             logout_resp = self._end_v6_session(host, headers)
             self.log.debug("logout status %s", logout_resp)
+        else:
+            self.service_check('pihole.running', self.CRITICAL)
+            raise Exception('Unexpected response from server')
+
 
     def check(self, instance):
-        host = self.instance.get('host')
-        custom_tags = self.instance.get("tags", [])
-        custom_tags.append("target_host:{}".format(host))
-        web_password = self.instance.get('web_password')
-        v5_pihole = self.instance.get('legacy_check')
-        if v5_pihole:
-            self._legacy_check(host, custom_tags)
+        if self.v5_pihole:
+            self._legacy_check(self.host, self.custom_tags)
         else:
-            self._v6_check(host, web_password, custom_tags)
+            self._v6_check(self.host, self.web_password, self.custom_tags)
