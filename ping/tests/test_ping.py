095c5a003081b22f583675f595e226af0e170188
sunryeokkim <sunryeok.kim@datadoghq.com>
2025-11-28 14:24:46 +0900

    Update test_valid_check and test_valid_check_ipv6 to match new code

--- ping/tests/test_ping.py
+++ ping/tests/test_ping.py
@@ -32,7 +32,7 @@ def test_incorrect_ip_check(incorrect_ip_instance):
 def test_valid_check(aggregator, instance):
     check = PingCheck("ping", {}, {})
 
-    with mock.patch.object(check, "_exec_ping", return_value=mock_exec_ping()):
+    with mock.patch.object(check, "_exec_ping", return_value={"status": "ok", "output": mock_exec_ping(), "error": ""}):
         check.check(instance)
     aggregator.assert_service_check("network.ping.can_connect", AgentCheck.OK)
     aggregator.assert_metric("network.ping.can_connect", value=1)
@@ -42,8 +42,8 @@ def test_valid_check(aggregator, instance):
 def test_valid_check_ipv6(aggregator, instance_ipv6):
     check = PingCheck("ping", {}, {})
 
-    with mock.patch.object(check, "_exec_ping", return_value=mock_exec_ping()):
-        check.check(instance_ipv6)
+    with mock.patch.object(check, "_exec_ping", return_value={"status": "ok", "output": mock_exec_ping(), "error": ""}):
+        check.check(instance)
     aggregator.assert_service_check("network.ping.can_connect", AgentCheck.OK)
     aggregator.assert_metric("network.ping.can_connect", value=1)
     aggregator.assert_all_metrics_covered()
