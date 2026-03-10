e6bb9a9c7e1d8927a63f80363634f17e4e2ce2c9
reuben <reubentjl@gmail.com>
2026-03-10 08:47:59 +0800

    sync config models

--- rundeck/datadog_checks/rundeck/config_models/instance.py
+++ rundeck/datadog_checks/rundeck/config_models/instance.py
@@ -34,6 +34,7 @@ class InstanceConfig(BaseModel):
     api_version: Optional[float] = None
     disable_generic_tags: Optional[bool] = None
     empty_default_hostname: Optional[bool] = None
+    enable_legacy_tags_normalization: Optional[bool] = None
     metric_patterns: Optional[MetricPatterns] = None
     min_collection_interval: Optional[float] = None
     service: Optional[str] = None
