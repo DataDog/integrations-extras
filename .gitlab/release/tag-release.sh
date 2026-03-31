6eb582a76a589096c1dd328e97a3eb72a62a8e8c
Steven Yuen <steven.yuen@datadoghq.com>
2026-03-31 12:06:03 -0400

    Migrate to use octo-sts for github auth (#2945)
    * Migrate to use octo-sts for github auth
    
    * codeowners entry
    
    * combine files

--- .gitlab/release/tag-release.sh
+++ .gitlab/release/tag-release.sh
@@ -3,6 +3,18 @@
 set -euxo pipefail
 IFS=$'\n\t'
 
+echo "Obtaining GitHub token via dd-octo-sts"
+set +x
+GITHUB_TOKEN=$(dd-octo-sts token --scope DataDog/integrations-extras --policy self.gitlab.release.master)
+trap 'set +x; dd-octo-sts revoke -t "$GITHUB_TOKEN" 2>/dev/null || true' EXIT
+export GITHUB_TOKEN
+git remote set-url origin "https://x-access-token:${GITHUB_TOKEN}@github.com/DataDog/integrations-extras.git"
+set -x
+
+git config --global user.email "$TAGGER_EMAIL"
+git config --global user.name "$TAGGER_NAME"
+git checkout master
+
 set +e
 # Check if there are any new tag to push
 ddev release tag all --dry-run
