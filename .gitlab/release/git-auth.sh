64bffb97d67ba5315cc91c62e300d509d1935561
steveny91 <steven.yuen@datadoghq.com>
2026-03-31 11:09:47 -0400

    Migrate to use octo-sts for github auth

--- .gitlab/release/git-auth.sh
+++ .gitlab/release/git-auth.sh
@@ -3,12 +3,14 @@
 set -euxo pipefail
 IFS=$'\n\t'
 
-echo "Grabbing GitHub deploy key"
+echo "Obtaining GitHub token via dd-octo-sts"
 set +x
-aws ssm get-parameter --region us-east-1 --name ci.integrations-extras.github_deploy_key --with-decryption --query "Parameter.Value" --out text | ssh-add -
+GITHUB_TOKEN=$(dd-octo-sts token --scope DataDog/integrations-extras --policy self.gitlab.release.master)
+trap 'set +x; dd-octo-sts revoke -t "$GITHUB_TOKEN" 2>/dev/null || true' EXIT
+export GITHUB_TOKEN
+git remote set-url origin "https://x-access-token:${GITHUB_TOKEN}@github.com/DataDog/integrations-extras.git"
 set -x
 
-git remote set-url origin git@github.com:DataDog/integrations-extras.git
 git config --global user.email "$TAGGER_EMAIL"
 git config --global user.name "$TAGGER_NAME"
 git checkout master
