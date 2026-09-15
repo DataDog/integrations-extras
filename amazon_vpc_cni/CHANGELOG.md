3110fee8d8fd6bf50547965d301df8ee84c166fa
Willian Cesar Cincerre Da Silva <3329-willian.cincerre@users.noreply.code.ifoodcorp.com.br>
2026-09-15 06:13:26 -0300

    feat(amazon-vpc-cni): add new integration for Amazon VPC CNI

---
+++ amazon_vpc_cni/CHANGELOG.md
@@ -0,0 +1,7 @@
+# CHANGELOG - Amazon VPC CNI
+
+## 0.1.0 / 2026-09-15
+
+***Added***:
+
+* Initial release of the Amazon VPC CNI integration.
---
+++ amazon_vpc_cni/datadog_checks/amazon_vpc_cni/metrics.py
@@ -0,0 +1,33 @@
+METRIC_MAP = {
+    ## IPAMD
+    'awscni_ipamd_error_count': 'ipamd_error_count',
+    'awscni_ipamd_action_inprogress': 'ipamd_action_inprogress',
+    'awscni_reconcile_count': 'reconcile_count',
+    'awscni_add_ip_req_count': 'add_ip_req_count',
+    'awscni_del_ip_req_count': 'del_ip_req_count',
+    'awscni_pod_eni_error_count': 'pod_eni_error_count',
+    ## AWS API
+    'awscni_aws_api_latency_ms': 'aws_api_latency_ms',
+    'awscni_aws_api_error_count': 'aws_api_error_count',
+    'awscni_aws_utils_error_count': 'aws_utils_error_count',
+    'awscni_ec2api_req_count': 'ec2api_req_count',
+    'awscni_ec2api_error_count': 'ec2api_error_count',
+    ## ENI / IP allocation
+    'awscni_eni_max': 'eni_max',
+    'awscni_ip_max': 'ip_max',
+    'awscni_eni_allocated': 'eni_allocated',
+    'awscni_total_ip_addresses': 'total_ip_addresses',
+    'awscni_assigned_ip_addresses': 'assigned_ip_addresses',
+    'awscni_force_removed_enis': 'force_removed_enis',
+    'awscni_force_removed_ips': 'force_removed_ips',
+    'awscni_total_ipv4_prefixes': 'total_ipv4_prefixes',
+    'awscni_assigned_ip_per_cidr': 'assigned_ip_per_cidr',
+    'awscni_no_available_ip_addresses': 'no_available_ip_addresses',
+    'awscni_assigned_ip_per_eni': 'assigned_ip_per_eni',
+    ## Startup duration
+    'awscni_ipamd_startup_duration_seconds': 'ipamd_startup_duration_seconds',
+    'awscni_ipamd_node_initialization_duration_seconds': 'ipamd_node_initialization_duration_seconds',
+    ## Connmark
+    'awscni_connmark_backend': 'connmark_backend',
+    'awscni_connmark_reconcile_total': 'connmark_reconcile_total',
+}
